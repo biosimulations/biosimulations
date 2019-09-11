7161734016bf44cbaac5efb28190d9a8c08df4d7
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-09-11 02:28:23 -0400

    Added bootstrap material theme

--- CRBM-Viz/src/app/app.module.ts
+++ CRBM-Viz/src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MDBBootstrapModule } from "angular-bootstrap-md";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
