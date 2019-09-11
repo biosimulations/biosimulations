2cf115df4decc76e2debdf737cd2f2339a438114
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-09-11 13:28:27 -0400

    revert MDB theme

--- CRBM-Viz/src/app/app.module.ts
+++ CRBM-Viz/src/app/app.module.ts
@@ -1,13 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { MDBBootstrapModule } from "angular-bootstrap-md";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
