60060b7a25cd448c0d1dea56534fd62c45bdaa68
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-09-12 13:19:22 -0400

    add basic layout components

--- CRBM-Viz/src/app/app.module.ts
+++ CRBM-Viz/src/app/app.module.ts
@@ -7,8 +7,11 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { MaterialModule } from "./Modules/app-material.module";
 import { TopbarComponent } from './topbar/topbar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LogoComponent } from './logo/logo.component';
 @NgModule({
-  declarations: [AppComponent, TopbarComponent],
+  declarations: [AppComponent, TopbarComponent, SearchBarComponent, SidebarComponent, LogoComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ CRBM-Viz/src/app/logo/logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.sass']
+})
+export class LogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
