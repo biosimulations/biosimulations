67b9cabff5d766ff581359a50892ca14f6421986
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-09-25 23:02:50 -0400

    Remove login page

--- CRBM-Viz/src/app/Components/login/login.component.ts
+++ CRBM-Viz/src/app/Components/login/login.component.ts
@@ -2,15 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.sass'],
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass'],
 })
 export class LoginComponent implements OnInit {
-    test: string = 'just a test';
-    constructor(private auth: AuthService) {}
+  test: string = 'just a test';
+  constructor(private auth: AuthService) {}
 
-    ngOnInit() {
-        this.test = this.auth.test();
-    }
+  ngOnInit() {
+    this.test = this.auth.test();
+  }
 }
