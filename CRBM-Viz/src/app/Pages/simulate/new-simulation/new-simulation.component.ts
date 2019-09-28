5e60db06d271949a45512d75d81cd20350a2beac
gmarupilla <marupilla@uchc.edu>
2019-09-28 13:45:14 -0400

    Create Simulation working with auth

--- CRBM-Viz/src/app/Pages/simulate/new-simulation/new-simulation.component.ts
+++ CRBM-Viz/src/app/Pages/simulate/new-simulation/new-simulation.component.ts
@@ -16,6 +16,7 @@ export class NewSimulationComponent implements OnInit {
   omexFiles: Array<string> = null;
   solverFiles: Array<string> = null;
   sbatchFiles: Array<string> = null;
+  responseData: object = null;
 
   constructor(private http: HttpClient, private alertService: AlertService) { }
 
@@ -28,9 +29,10 @@ export class NewSimulationComponent implements OnInit {
           this.solverFiles = success['data']['omexSolvers']['solver']
           const sbatches = []
           for (const sbatch of success['data']['files']) {
-            sbatches.push(`${sbatch['createdBy']} - ${sbatch['filename']}`);
+            sbatches.push(`${sbatch['createdBy']}-${sbatch['filename']}`);
           }
           this.sbatchFiles = sbatches;
+          this.responseData = success['data'];
         },
         error => {
           this.alertService.openDialog('Error occured while fetching file info');
@@ -40,6 +42,33 @@ export class NewSimulationComponent implements OnInit {
 
   getFileInfo() {
     return this.http.get(`${CrbmConfig.CRBMAPI_URL}/simulation`);
+  }
+
+
+  createSimulation(){
+    const id = this.getFileId();
+    this.http.post(`${CrbmConfig.CRBMAPI_URL}/simulation`, {
+      omex: this.selectedOmex,
+      solver: this.selectedSolver,
+      fileId: id
+    }).subscribe(
+      success => {
+          this.alertService.openDialog('Simulation creation was successfull (Not catching HPC errors)');
+      },
+      error => {
+          this.alertService.openDialog('Simulation creation failed');
+      }
+    );
+  }
+
+  getFileId() {
+    const fileSplitted = this.selectedSbatch.split('-');
+    const user = fileSplitted[0];
+    const filename = fileSplitted[1];
+    const fileObj = this.responseData['files'].find(
+        file => file.createdBy === user && file.filename === filename
+      );
+    return fileObj['fileId'];
   }
 
 
