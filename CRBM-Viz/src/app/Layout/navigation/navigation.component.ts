4a0b2dfc492109d70f6c42057649af84981a9ff2
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-10-02 23:55:48 -0400

    Styling

--- CRBM-Viz/src/app/Layout/navigation/navigation.component.ts
+++ CRBM-Viz/src/app/Layout/navigation/navigation.component.ts
@@ -6,7 +6,7 @@ import { map, shareReplay } from 'rxjs/operators';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css'],
+  styleUrls: ['./navigation.component.sass'],
 })
 export class NavigationComponent {
   isHandset$: Observable<boolean> = this.breakpointObserver
