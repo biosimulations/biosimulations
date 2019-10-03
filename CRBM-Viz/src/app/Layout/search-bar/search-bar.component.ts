6a91e5b393146b082621a9623cd054c00cfc333a
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-10-02 21:08:34 -0400

    Move viz searchbar into topbar

--- CRBM-Viz/src/app/Layout/search-bar/search-bar.component.ts
+++ CRBM-Viz/src/app/Layout/search-bar/search-bar.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.sass']
+  styleUrls: ['./search-bar.component.sass'],
 })
 export class SearchBarComponent implements OnInit {
+  @Input() buttonTerm: string = 'Search';
+  @Input() placeholder: string = 'Search...';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
