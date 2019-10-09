33cb14b41c95a202af7edeb2af097f8599fc71e2
gmarupilla <marupilla@uchc.edu>
2019-10-09 15:49:51 -0400

    Fix merge markers in file table component

--- CRBM-Viz/src/app/Pages/files/file-table/file-table.component.ts
+++ CRBM-Viz/src/app/Pages/files/file-table/file-table.component.ts
@@ -2,14 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { HttpClient } from '@angular/common/http';
 import { CrbmConfig } from 'src/app/crbm-config';
-<<<<<<< HEAD
-import { SocialUser } from 'angularx-social-login';
-import { AuthService } from 'src/app/Services/auth0.service';
-
-=======
 import { AuthService } from 'src/app/Services/auth0.service';
 import { async } from 'q';
->>>>>>> master
 
 @Component({
   selector: 'app-file-table',
@@ -25,20 +19,12 @@ export class FileTableComponent implements OnInit {
     'accessType',
   ];
   dataSource: MatTableDataSource<object>;
-<<<<<<< HEAD
-  currentUser: string = null;
-=======
   currentUser = null;
->>>>>>> master
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-<<<<<<< HEAD
   constructor(private http: HttpClient, private auth: AuthService) {
-=======
-  constructor(private http: HttpClient, private crbmAuthService: AuthService) {
->>>>>>> master
     // Create 100 users
     // const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
     // Assign the data to the data source for the table to render
@@ -58,13 +44,9 @@ export class FileTableComponent implements OnInit {
       }
     );
 
-<<<<<<< HEAD
     this.auth.userProfile$.subscribe(
       profile => this.currentUser = JSON.stringify(profile, null, 2)
     );
-=======
-    this.currentUser = null;
->>>>>>> master
   }
 
   getFileData() {
