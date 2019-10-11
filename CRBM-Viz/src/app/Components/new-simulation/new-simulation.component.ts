6acead6308bc78b07e8ea756288461054a36b932
gmarupilla <marupilla@uchc.edu>
2019-10-11 15:45:34 -0400

    Add loading spinner to new simulation

--- CRBM-Viz/src/app/Components/new-simulation/new-simulation.component.ts
+++ CRBM-Viz/src/app/Components/new-simulation/new-simulation.component.ts
@@ -8,7 +8,7 @@ import { SimulationService } from 'src/app/Services/simulation.service';
   styleUrls: ['./new-simulation.component.sass']
 })
 export class NewSimulationComponent implements OnInit {
-
+  isLoading = true;
   selectedSbatch: string = null;
   selectedOmex: string = null;
   selectedSolver: string = null;
@@ -27,6 +27,7 @@ export class NewSimulationComponent implements OnInit {
         this.omexFiles = this.simulationService.omexFiles;
         this.solverFiles = this.simulationService.solverFiles;
         this.sbatchFiles = this.simulationService.sbatchFiles;
+        this.isLoading = false;
       },
       error => {
         this.alertService.openDialog(
@@ -38,6 +39,7 @@ export class NewSimulationComponent implements OnInit {
   }
 
   onSubmit() {
+    this.isLoading = true;
     this.simulationService.createSimulation(
       this.selectedSbatch,
       this.selectedOmex,
@@ -49,6 +51,7 @@ export class NewSimulationComponent implements OnInit {
           JSON.stringify(success)
         );
         this.simulationService.getSimulationAndJobFilesInfo();
+        this.isLoading = false;
       },
       error => {
         this.alertService.openDialog(
