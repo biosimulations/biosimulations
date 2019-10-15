2944dd44871b1fa5fbf3537b34abcd96035d8cfd
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-10-15 16:48:36 -0400

    Fixed bad imports

--- CRBM-Viz/src/app/Services/auth0.service.spec.ts
+++ CRBM-Viz/src/app/Services/auth0.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Auth0Service } from './auth0.service';
+import { AuthService } from './auth0.service';
 
 describe('Auth0Service', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: Auth0Service = TestBed.get(Auth0Service);
+    const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
   });
 });
