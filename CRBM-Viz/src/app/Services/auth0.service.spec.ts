0e0c8643d83c2b285630a5f3a496bc9c9e54b3b3
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-10-16 17:39:15 -0400

    Fixed Auth0 service tests

--- CRBM-Viz/src/app/Services/auth0.service.spec.ts
+++ CRBM-Viz/src/app/Services/auth0.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth0.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('Auth0Service', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [RouterTestingModule, HttpClientTestingModule],
+    })
+  );
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
