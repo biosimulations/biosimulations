df98019dcd0d8826365fc29277022d58ef168df7
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-10-16 18:15:57 -0400

    lint fix

--- CRBM-Viz/src/app/Components/callback/callback.component.spec.ts
+++ CRBM-Viz/src/app/Components/callback/callback.component.spec.ts
@@ -6,20 +6,21 @@ import { AuthService } from 'src/app/Services/auth0.service';
 import { RouterTestingModule } from '@angular/router/testing';
 //  TODO This test does nothing, fix
 describe('CallbackComponent', () => {
-  //let component: CallbackComponent;
-  //let fixture: ComponentFixture<CallbackComponent>;
+  // let component: CallbackComponent;
+  // let fixture: ComponentFixture<CallbackComponent>;
 
   beforeEach(async(() => {
-    //TestBed.configureTestingModule({ imports: [RouterTestingModule], providers: [RouterTestingModule, AuthService],declarations: [CallbackComponent],}).compileComponents();
+    // tslint:disable-next-line:max-line-length
+    // TestBed.configureTestingModule({ imports: [RouterTestingModule], providers: [RouterTestingModule, AuthService],declarations: [CallbackComponent],}).compileComponents();
   }));
 
   beforeEach(() => {
-    //fixture = TestBed.createComponent(CallbackComponent);
-    //component = fixture.componentInstance;
-    //fixture.detectChanges();
+    // fixture = TestBed.createComponent(CallbackComponent);
+    // component = fixture.componentInstance;
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
-    //expect(component.ngOnInit).toThrowError();
+    // expect(component.ngOnInit).toThrowError();
   });
 });
