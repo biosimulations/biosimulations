289b0661236b38585a5d319049590992a20aa27f
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-10-16 00:27:27 -0400

    Add todos

--- CRBM-Viz/src/app/Components/new-simulation/new-simulation.component.spec.ts
+++ CRBM-Viz/src/app/Components/new-simulation/new-simulation.component.spec.ts
@@ -3,10 +3,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NewSimulationComponent } from './new-simulation.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatTableModule, MatDialogModule } from '@angular/material';
+// TODO change this import to simply import the app-material module?
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AlertComponent } from 'src/app/Components/alert/alert.component';
-// TODO The alert component needs to be mocked!
+// TODO The alert component needs to be mocked !
 describe('NewSimulationComponent', () => {
   let component: NewSimulationComponent;
   let fixture: ComponentFixture<NewSimulationComponent>;
