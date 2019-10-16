3b2e7c44f6e51b4cc89458717f5679e913ee2783
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-10-16 17:45:29 -0400

    Fixed visualize components test

--- CRBM-Viz/src/app/Pages/visualize/visualize.component.spec.ts
+++ CRBM-Viz/src/app/Pages/visualize/visualize.component.spec.ts
@@ -2,7 +2,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VisualizeComponent } from './visualize.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+// TODO create mock vega view
+// TODO mock http client request for spec
 describe('VisualizeComponent', () => {
   let component: VisualizeComponent;
   let fixture: ComponentFixture<VisualizeComponent>;
@@ -10,6 +14,8 @@ describe('VisualizeComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [VisualizeComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [RouterTestingModule, HttpClientTestingModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
