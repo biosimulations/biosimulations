605cbf319b49a98cf097c4e209920cc3eece4479
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-10-16 15:42:45 -0400

    Fixed Alert component tests

--- CRBM-Viz/src/app/Components/alert/alert.component.spec.ts
+++ CRBM-Viz/src/app/Components/alert/alert.component.spec.ts
@@ -1,24 +1,56 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  inject,
+} from '@angular/core/testing';
 
 import { AlertComponent } from './alert.component';
-
+import { MatDialogModule, MatDialogRef, MatDialog } from '@angular/material';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import {
+  NoopAnimationsModule,
+  BrowserAnimationsModule,
+} from '@angular/platform-browser/animations';
+import { OverlayContainer } from '@angular/cdk/overlay';
 describe('AlertComponent', () => {
-  let component: AlertComponent;
-  let fixture: ComponentFixture<AlertComponent>;
-
+  let dialog: MatDialog;
+  let overlayContainer: OverlayContainer;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AlertComponent],
-    }).compileComponents();
+      imports: [MatDialogModule, BrowserAnimationsModule],
+      providers: [MatDialogModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    });
+    TestBed.overrideModule(BrowserDynamicTestingModule, {
+      set: {
+        entryComponents: [AlertComponent],
+      },
+    });
+
+    TestBed.compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AlertComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(inject(
+    [MatDialog, OverlayContainer],
+    (d: MatDialog, oc: OverlayContainer) => {
+      dialog = d;
+      overlayContainer = oc;
+    }
+  ));
+
+  afterEach(() => {
+    overlayContainer.ngOnDestroy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should open a dialog with a component', () => {
+    const dialogRef = dialog.open(AlertComponent, {
+      data: { param: '1' },
+    });
+
+    // verify
+    expect(dialogRef.componentInstance instanceof AlertComponent).toBe(true);
   });
 });
