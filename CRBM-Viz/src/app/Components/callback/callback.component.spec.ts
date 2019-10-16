f48c7743ffd264e427c219ebdfcbaa0808c85dfb
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-10-15 23:55:37 -0400

    Fixed link component test

--- CRBM-Viz/src/app/Components/callback/callback.component.spec.ts
+++ CRBM-Viz/src/app/Components/callback/callback.component.spec.ts
@@ -2,15 +2,18 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CallbackComponent } from './callback.component';
 
+import { AuthService } from 'src/app/Services/auth0.service';
+import { RouterModule } from '@angular/router';
+
 describe('CallbackComponent', () => {
   let component: CallbackComponent;
   let fixture: ComponentFixture<CallbackComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CallbackComponent ]
-    })
-    .compileComponents();
+      imports: [RouterModule],
+      declarations: [CallbackComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- CRBM-Viz/src/app/Layout/links/links.component.spec.ts
+++ CRBM-Viz/src/app/Layout/links/links.component.spec.ts
@@ -1,7 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LinksComponent } from './links.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
+import { RouterModule, Router, ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { from } from 'rxjs';
 describe('LinksComponent', () => {
   let component: LinksComponent;
   let fixture: ComponentFixture<LinksComponent>;
@@ -9,6 +11,11 @@ describe('LinksComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [LinksComponent],
+      imports: [RouterTestingModule],
+      providers: [
+        { provide: Router, useValue: Router },
+        { provide: ActivatedRoute, useValue: ActivatedRoute },
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
