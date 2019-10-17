9e7a6276700755f066b78402e679e7d033918d35
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-10-17 17:21:28 -0400

    SSR works with manual server.ts edit

--- CRBM-Viz/server.ts
+++ CRBM-Viz/server.ts
@@ -18,24 +18,43 @@
 import 'zone.js/dist/zone-node';
 
 import * as express from 'express';
-import {join} from 'path';
+import { join } from 'path';
 
 // Express server
+const domino = require('domino');
+const fs = require('fs');
+const path = require('path');
+const templateA = fs
+  .readFileSync(path.join('dist/browser', 'index.html'))
+  .toString();
+const win = domino.createWindow(templateA);
+win.Object = Object;
+win.Math = Math;
+global['window'] = win;
+global['document'] = win.document;
+global['branch'] = null;
+global['object'] = win.object;
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
+const {
+  AppServerModuleNgFactory,
+  LAZY_MODULE_MAP,
+  ngExpressEngine,
+  provideModuleMap,
+} = require('./dist/server/main');
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
+app.engine(
+  'html',
+  ngExpressEngine({
+    bootstrap: AppServerModuleNgFactory,
+    providers: [provideModuleMap(LAZY_MODULE_MAP)],
+  })
+);
 
 app.set('view engine', 'html');
 app.set('views', DIST_FOLDER);
@@ -43,9 +62,12 @@ app.set('views', DIST_FOLDER);
 // Example Express Rest API endpoints
 // app.get('/api/**', (req, res) => { });
 // Serve static files from /browser
-app.get('*.*', express.static(DIST_FOLDER, {
-  maxAge: '1y'
-}));
+app.get(
+  '*.*',
+  express.static(DIST_FOLDER, {
+    maxAge: '1y',
+  })
+);
 
 // All regular routes use the Universal engine
 app.get('*', (req, res) => {
--- CRBM-Viz/src/app/Pages/visualize/visualize.component.ts
+++ CRBM-Viz/src/app/Pages/visualize/visualize.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { Visualization } from 'src/app/Models/visualization';
 import { VisualizationsService } from 'src/app/Services/visualizations.service';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-visualize',
   templateUrl: './visualize.component.html',
   styleUrls: ['./visualize.component.sass'],
 })
 export class VisualizeComponent implements OnInit {
+  isBrowser = isPlatformBrowser(this.platformId);
   visualizations: Visualization[] = [];
   id: string;
 
   constructor(
+    @Inject(PLATFORM_ID) private platformId: any,
     private visService: VisualizationsService,
     private route: ActivatedRoute
   ) {}
