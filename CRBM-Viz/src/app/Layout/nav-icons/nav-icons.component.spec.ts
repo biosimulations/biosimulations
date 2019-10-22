1e371455c778b7d131b95251d4540ba4d06bf908
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-10-21 20:40:26 -0400

    Fixed nav-icons test

--- CRBM-Viz/src/app/Layout/nav-icons/nav-icons.component.spec.ts
+++ CRBM-Viz/src/app/Layout/nav-icons/nav-icons.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavIconsComponent } from './nav-icons.component';
+import { MaterialModule } from 'src/app/Modules/app-material.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NavIconsComponent', () => {
   let component: NavIconsComponent;
@@ -8,9 +10,10 @@ describe('NavIconsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavIconsComponent ]
-    })
-    .compileComponents();
+      imports: [MaterialModule, RouterTestingModule],
+      providers: [MaterialModule, RouterTestingModule],
+      declarations: [NavIconsComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
