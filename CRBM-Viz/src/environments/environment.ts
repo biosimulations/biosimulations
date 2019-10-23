bea43a482df138f75068e20e05a60ea61e26383d
gmarupilla <marupilla@uchc.edu>
2019-10-23 15:01:14 -0400

    Fix environments

--- CRBM-Viz/src/environments/environment.ts
+++ CRBM-Viz/src/environments/environment.ts
@@ -1,30 +1,13 @@
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: '/',
   auth0: {
-    domain: 'dev-t2vyo8fj.auth0.com',
-    clientId: '6euIYB0EPMSNjsPEjrNZAMmpiuVCRYNB'
-},
-  crbm: {
-    CRBMAPI_URL: 'http://localhost:5000',
-    ALLOWED_FILE_EXTENSIONS: [
-      'pdf',
-      'doc',
-      'ppt',
-      'txt',
-      'xlsx',
-      'sedx',
-      'sbex',
-      'cmex',
-      'sbox',
-      'neux',
-      'phex',
-      'zip',
-      'jpg',
-      'png',
-      'jpeg',
-      'raw',
-      'sh',
-    ],
+    domain: 'crbm.auth0.com',
+    clientId: '0NKMjbZuexkCgfWY3BG9C3808YsdLUrb'
   },
+  crbm: {
+      CRBMAPI_URL: 'http://localhost:5000',
+      ALLOWED_FILE_EXTENSIONS: ['pdf', 'doc', 'ppt', 'txt', 'xlsx', 'sedx', 'sbex',
+          'cmex', 'sbox', 'neux', 'phex', 'zip', 'jpg', 'png', 'jpeg', 'raw', 'sh']
+  }
 };
