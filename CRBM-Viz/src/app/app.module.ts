896df2abe3b2ba51129c415572852c83c94f419f
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-10-29 14:47:18 -0400

    Add navigation link to datatable

--- CRBM-Viz/src/app/app.module.ts
+++ CRBM-Viz/src/app/app.module.ts
@@ -1,49 +1,49 @@
 // ng Module
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 // Angular core components/modules/tools in imports
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { AgGridModule } from "ag-grid-angular";
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AgGridModule } from 'ag-grid-angular';
 // Defined Modules in Imports
 
 // Defined Modules in app
-import { AppRoutingModule } from "./Modules/app-routing.module";
-import { MaterialModule } from "./Modules/app-material.module";
+import { AppRoutingModule } from './Modules/app-routing.module';
+import { MaterialModule } from './Modules/app-material.module';
 
 // Defined components
-import { AppComponent } from "./app.component";
-import { SearchBarComponent } from "./Layout/search-bar/search-bar.component";
-import { LogoComponent } from "./Layout/logo/logo.component";
-import { AboutComponent } from "./Pages/about/about.component";
-import { VisualizeComponent } from "./Pages/visualize/visualize.component";
-import { SimulateComponent } from "./Pages/simulate/simulate.component";
-import { HomeComponent } from "./Pages/home/home.component";
-import { VegaViewerComponent } from "./Components/vega-viewer/vega-viewer.component";
-import { FourComponent } from "./Pages/four/four.component";
-import { UnderConstructionComponent } from "./Pages/under-construction/under-construction.component";
-import { NavigationComponent } from "./Layout/navigation/navigation.component";
-import { LayoutModule } from "@angular/cdk/layout";
-import { SidebarComponent } from "./Layout/sidebar/sidebar.component";
-import { CallbackComponent } from "./Components/callback/callback.component";
-import { ProfileComponent } from "./Components/profile/profile.component";
-import { AlertComponent } from "./Components/alert/alert.component";
-import { PastSimulationComponent } from "./Components/past-simulation/past-simulation.component";
-import { NewSimulationComponent } from "./Components/new-simulation/new-simulation.component";
-import { UploadComponent } from "./Pages/upload/upload.component";
-import { FileTableComponent } from "./Pages/files/file-table/file-table.component";
-import { FileEditComponent } from "./Pages/files/file-edit/file-edit.component";
-import { AuthInterceptorService } from "./Interceptors/auth-interceptor.service";
-import { FooterComponent } from "./Layout/footer/footer.component";
-import { DataTableComponent } from "./Components/data-table/data-table.component";
-import { DataComponent } from "./Pages/data/data.component";
-import { NavIconsComponent } from "./Layout/nav-icons/nav-icons.component";
-import { AccountMenuComponent } from "./Layout/account-menu/account-menu.component";
+import { AppComponent } from './app.component';
+import { SearchBarComponent } from './Layout/search-bar/search-bar.component';
+import { LogoComponent } from './Layout/logo/logo.component';
+import { AboutComponent } from './Pages/about/about.component';
+import { VisualizeComponent } from './Pages/visualize/visualize.component';
+import { SimulateComponent } from './Pages/simulate/simulate.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { VegaViewerComponent } from './Components/vega-viewer/vega-viewer.component';
+import { FourComponent } from './Pages/four/four.component';
+import { UnderConstructionComponent } from './Pages/under-construction/under-construction.component';
+import { NavigationComponent } from './Layout/navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { SidebarComponent } from './Layout/sidebar/sidebar.component';
+import { CallbackComponent } from './Components/callback/callback.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { AlertComponent } from './Components/alert/alert.component';
+import { PastSimulationComponent } from './Components/past-simulation/past-simulation.component';
+import { NewSimulationComponent } from './Components/new-simulation/new-simulation.component';
+import { UploadComponent } from './Pages/upload/upload.component';
+import { FileTableComponent } from './Pages/files/file-table/file-table.component';
+import { FileEditComponent } from './Pages/files/file-edit/file-edit.component';
+import { AuthInterceptorService } from './Interceptors/auth-interceptor.service';
+import { FooterComponent } from './Layout/footer/footer.component';
+import { DataTableComponent } from './Components/data-table/data-table.component';
+import { DataComponent } from './Pages/data/data.component';
+import { NavIconsComponent } from './Layout/nav-icons/nav-icons.component';
+import { AccountMenuComponent } from './Layout/account-menu/account-menu.component';
 
 // Pipes defined in app
-import { FilterPipe } from "./Pipes/filter.pipe";
+import { FilterPipe } from './Pipes/filter.pipe';
 
 // Services
 // import { VisualizationsService } from './Services/visualizations.service';
