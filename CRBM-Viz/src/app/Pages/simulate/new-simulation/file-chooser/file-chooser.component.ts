bcda14c01e925e565944eb9d1c03d4d731203460
gmarupilla <marupilla@uchc.edu>
2019-10-30 10:20:34 -0400

    Make file-chooser component functional with datatables and file service

--- CRBM-Viz/src/app/Pages/simulate/new-simulation/file-chooser/file-chooser.component.ts
+++ CRBM-Viz/src/app/Pages/simulate/new-simulation/file-chooser/file-chooser.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { FileService } from 'src/app/Services/file.service';
+import { AuthService } from 'src/app/Services/auth0.service';
+import { AlertService } from 'src/app/Services/alert.service';
 
 @Component({
   selector: 'app-file-chooser',
@@ -7,9 +12,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileChooserComponent implements OnInit {
 
-  constructor() { }
+  isLoading = false;
+  serverUrl = environment.crbm.CRBMAPI_URL; // Required in template
+  fileList: Array<object> = null;
+  displayedColumns: string[] = [
+    'fileId',
+    'filename',
+    'createdBy',
+    'accessType',
+  ];
+  dataSource: MatTableDataSource<object>;
+  currentUser = null;
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  constructor(
+    private fileService: FileService,
+    private auth: AuthService,
+    private alertService: AlertService,
+  ) { }
 
   ngOnInit() {
+    this.fileService.fileChangeSubject.subscribe(
+      success => {
+        this.fileList = this.fileService.fileList;
+        this.dataSource = new MatTableDataSource(this.fileList);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.isLoading = false;
+      },
+      error => {
+        this.alertService.openDialog(
+          'Error from FileTableComponent, can\'t fetch files: '+
+          JSON.stringify(error)
+        );
+      }
+    );
+
+    this.fileService.getFileData();
+
+    this.auth.userProfile$.subscribe(
+      profile => this.currentUser = profile
+    );
   }
 
+  
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  onFileDelete(fileId: number) {
+    this.fileService.deleteFile(fileId);
+  }
 }
