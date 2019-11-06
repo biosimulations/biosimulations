18d8c7fbe5764c4f5f48939d4b5f25d4fbf58d1d
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-11-05 20:53:12 -0500

    Remove unused imports

--- CRBM-Viz/src/app/app.module.ts
+++ CRBM-Viz/src/app/app.module.ts
@@ -15,23 +15,9 @@ import { MaterialModule } from './Modules/app-material.module';
 
 // Defined components
 import { AppComponent } from './app.component';
-
-import { HomeComponent } from './Shared/Components/home/home.component';
-
-import { FourComponent } from './Shared/Components/four/four.component';
-import { UnderConstructionComponent } from './Shared/Components/under-construction/under-construction.component';
-import { CallbackComponent } from './Shared/Components/callback/callback.component';
-
-import { UploadComponent } from './Modules/models/upload/upload.component';
-import { FileTableComponent } from './Modules/models/file-table/file-table.component';
-import { FileEditComponent } from './Modules/models/file-edit/file-edit.component';
 import { AuthInterceptorService } from './Shared/Interceptors/auth-interceptor.service';
 
 import { SharedModule } from './Shared/shared.module';
-import { AboutModule } from './Modules/about/about.module';
-import { AccountModule } from './Modules/account/account.module';
-import { DataComponent } from './Modules/simulate/data/data.component';
-import { ModelsModule } from './Modules/models/models.module';
 
 // Pipes defined in app
 
@@ -51,9 +37,6 @@ import { ModelsModule } from './Modules/models/models.module';
     HttpClientModule,
     MaterialModule,
     SharedModule,
-    AboutModule,
-    AccountModule,
-    ModelsModule,
   ],
   providers: [
     {
