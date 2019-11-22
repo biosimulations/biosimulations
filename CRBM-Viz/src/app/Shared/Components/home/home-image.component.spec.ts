62f175377426f66eaa1b886f8446c0ebced727c8
Jonathan Karr <jonrkarr@gmail.com>
2019-11-22 02:35:28 -0500

    debugging tests

--- CRBM-Viz/src/app/Shared/Components/home/home-image.component.spec.ts
+++ CRBM-Viz/src/app/Shared/Components/home/home-image.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HomeImageComponent } from './home-image.component';
 
 describe('HomeImageComponent', () => {
@@ -10,6 +11,7 @@ describe('HomeImageComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ HomeImageComponent ],
       imports: [ RouterTestingModule ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     })
     .compileComponents();
   }));
