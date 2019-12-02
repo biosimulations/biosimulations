4a432430e928f6e71839a1a833070185bc1c6777
Jonathan Karr <jonrkarr@gmail.com>
2019-12-02 13:10:16 -0500

    removing old model upload form

--- CRBM-Viz/src/app/Modules/models/models.module.ts
+++ CRBM-Viz/src/app/Modules/models/models.module.ts
@@ -9,14 +9,12 @@ import { ModelsRoutingModule } from './models-routing.module';
 
 import { BrowseComponent } from './browse/browse.component';
 import { EditComponent } from './edit/edit.component';
-import { UploadComponent } from './upload/upload.component';
 import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [
     BrowseComponent,
     EditComponent,
-    UploadComponent,
     ViewComponent,
   ],
   imports: [
