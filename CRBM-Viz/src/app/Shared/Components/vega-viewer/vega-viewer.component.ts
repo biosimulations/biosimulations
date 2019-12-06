4af9908e2a39f178a3d68998f75009f517c42b4c
Jonathan Karr <jonrkarr@gmail.com>
2019-12-06 15:07:34 -0500

    adding options input to vega viewer

--- CRBM-Viz/src/app/Shared/Components/vega-viewer/vega-viewer.component.ts
+++ CRBM-Viz/src/app/Shared/Components/vega-viewer/vega-viewer.component.ts
@@ -6,13 +6,20 @@ import vegaEmbed from 'vega-embed';
   templateUrl: './vega-viewer.component.html',
   styleUrls: ['./vega-viewer.component.sass'],
 })
-export class VegaViewerComponent implements AfterViewInit {
+export class VegaViewerComponent {
   private privateSpec: object | string;
-  @ViewChild("vegaContainer") vegaContainer: ElementRef;
+  private privateOptions: object;
+  @ViewChild("vegaContainer", { static: true }) vegaContainer: ElementRef;
 
   @Input()
   set spec(value: object | string) {
     this.privateSpec = value;
+    this.load();
+  }
+
+  @Input()
+  set options(value: object) {
+    this.privateOptions = value;
     this.load();
   }
 
@@ -21,7 +28,7 @@ export class VegaViewerComponent implements AfterViewInit {
   load() {
     if (this.vegaContainer && this.privateSpec) {
       // console.log(this.privateSpec);
-      vegaEmbed(this.vegaContainer.nativeElement, this.privateSpec)
+      vegaEmbed(this.vegaContainer.nativeElement, this.privateSpec, this.privateOptions)
         // result.view provides access to the Vega View API
         // .then(result => console.log(result))
         .catch(console.error);
