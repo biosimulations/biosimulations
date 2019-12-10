6997da56da3cc4450ab462faf419a6da5e76374e
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-12-10 13:33:11 -0500

    Add dev env to use local api

--- CRBM-Viz/src/environments/environment.dev.ts
+++ CRBM-Viz/src/environments/environment.dev.ts
@@ -2,40 +2,61 @@ export const environment = {
   production: false,
   baseUrl: '/',
   auth0: {
-    domain: 'dev-t2vyo8fj.auth0.com',
-    clientId: '6euIYB0EPMSNjsPEjrNZAMmpiuVCRYNB'
+    domain: 'crbm.auth0.com',
+    audience: 'api.biosimulations.org',
+    clientId: '0NKMjbZuexkCgfWY3BG9C3808YsdLUrb',
   },
   crbm: {
-    CRBMAPI_URL: 'https://crbm-api.herokuapp.com',
-    ALLOWED_FILE_EXTENSIONS: ['pdf', 'doc', 'ppt', 'txt', 'xlsx', 'sedx', 'sbex',
-      'cmex', 'sbox', 'neux', 'phex', 'zip', 'jpg', 'png', 'jpeg', 'raw', 'sh', 'omex', 'xml']
+    CRBMAPI_URL: 'http://localhost:5001',
+    ALLOWED_FILE_EXTENSIONS: [
+      'pdf',
+      'doc',
+      'ppt',
+      'txt',
+      'xlsx',
+      'sedx',
+      'sbex',
+      'cmex',
+      'sbox',
+      'neux',
+      'phex',
+      'zip',
+      'jpg',
+      'png',
+      'jpeg',
+      'raw',
+      'sh',
+      'omex',
+      'xml',
+    ],
   },
   bruitConfig: {
     apiKey: 'e6f43cf7-e04d-4581-9505-dbd7eddd9dbd',
     labels: {
-      title: 'Send us a feedback'
+      title: 'Send us a feedback',
     },
     form: [
       {
         label: 'Comments',
         type: 'textarea',
-        required: true
+        required: true,
       },
       {
         label: 'Your name',
-        type: 'text'
+        type: 'text',
       },
       {
         label: 'Email',
         type: 'text',
-        id: 'title'
+        id: 'title',
       },
       {
         id: 'agreement',
         type: 'checkbox',
-        label: 'I agree to send technical information in addition to my answers',
-        value: false
-      }
+        label:
+          'I agree to send technical information in addition to my answers',
+        value: false,
+      },
     ],
     colors: {
       header: '#2196f3',
