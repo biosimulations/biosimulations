24c004093730f47ea18a001c542b454ba41e8afc
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-01-25 15:04:35 -0500

    add chartype to project model

--- CRBM-Viz/src/app/Shared/Models/project-product.ts
+++ CRBM-Viz/src/app/Shared/Models/project-product.ts
@@ -3,15 +3,16 @@ import { JournalReference } from './journal-reference';
 import { Model } from './model';
 import { Simulation } from './simulation';
 import { Visualization } from './visualization';
+import { ChartType } from './chart-type';
 
 /* The product of a project, such as a figure, table, box, or supplementary item, and the resources that were used to create it.
 
 Synonyms: artifact, output, display item
 */
 export class ProjectProduct {
-    ref?: JournalReference;
-    type?: ProjectProductType;
-    label?: string;
-    description?: string;
-    resources?: (Model | Simulation | Visualization)[] = [];
+  ref?: JournalReference;
+  type?: ProjectProductType;
+  label?: string;
+  description?: string;
+  resources?: (Model | Simulation | Visualization | ChartType)[] = [];
 }
