d6282893f47393846af613e1cc4173c0f3b550ef
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-02-05 11:26:06 -0500

    feat(models): add query-options model definition

--- CRBM-Viz/src/app/Shared/Models/query-options.ts
+++ CRBM-Viz/src/app/Shared/Models/query-options.ts
@@ -1,8 +1,25 @@
 export class QueryOptions {
   user?: string;
   parent?: string;
+  embed?: string[] = [];
 
   toQueryString() {
-    return '';
+    let queryString = '';
+    if (this.user) {
+      queryString = queryString.concat('owner=' + (this.user as string));
+    }
+    if (this.parent) {
+      queryString = queryString.concat(('parent=' + this.parent) as string);
+    }
+
+    if (this.embed) {
+      this.embed.forEach(value => {
+        queryString = queryString.concat('embed=' + (value as string) + '&');
+      });
+    }
+    if (queryString.charAt(queryString.length - 1) === '&') {
+      queryString = queryString.substring(0, queryString.length - 1);
+    }
+    return queryString;
   }
 }
