78be1377afbf3877885744074fe8dccd0ffe1ca6
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-02-13 01:13:11 -0500

    fix(interceptor): API token, error handling
    Only send the Token in the request to the api.
    If not logged in, don't cause error

--- CRBM-Viz/src/app/Shared/Interceptors/auth-interceptor.service.ts
+++ CRBM-Viz/src/app/Shared/Interceptors/auth-interceptor.service.ts
@@ -3,31 +3,35 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { AuthService } from 'src/app/Shared/Services/auth0.service';
 import { Observable, throwError } from 'rxjs';
 import { mergeMap, catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthInterceptorService implements HttpInterceptor {
-
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService) {}
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    return this.auth.getTokenSilently$().pipe(
-      mergeMap(token => {
-        const tokenReq = req.clone({
-          setHeaders: { Authorization: `Bearer ${token}` }
-        });
-        return next.handle(tokenReq);
-      }),
-      catchError(err => throwError(err))
-    );
+    if (req.url.startsWith(environment.crbm.CRBMAPI_URL)) {
+      return this.auth.getTokenSilently$().pipe(
+        mergeMap(token => {
+          const tokenReq = req.clone({
+            setHeaders: { Authorization: `Bearer ${token}` },
+          });
+          return next.handle(tokenReq);
+        }),
+        catchError(err => next.handle(req))
+      );
+    } else {
+      return next.handle(req);
+    }
   }
 }
