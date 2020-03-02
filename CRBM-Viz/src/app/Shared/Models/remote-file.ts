b85ff9d7061a1e62d191b017078e680d5429c7b9
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-03-02 12:12:07 -0500

    feat(shared): add fields to remote file

--- CRBM-Viz/src/app/Shared/Models/remote-file.ts
+++ CRBM-Viz/src/app/Shared/Models/remote-file.ts
@@ -1,12 +1,29 @@
 export class RemoteFile {
-  constructor(name?: string, type?: string, url?: string, size?: number) {
+  constructor(
+    name?: string,
+    id?: string,
+    owner?: string,
+    isPrivate?: boolean,
+    type?: string,
+    url?: string,
+    size?: number,
+    metadata?: any
+  ) {
     this.name = name;
+    this.id = id;
+    this.owner = owner;
+    this.private = isPrivate;
     this.type = type;
     this.url = url;
     this.size = size;
+    this.metadata = metadata;
   }
   name?: string; // original filename
+  id?: string; // The id of the file on the server
+  owner?: string; // The username of the owner of the file
+  private?: boolean; // Whether the file is private to the owner
   type?: string; // MIME type
   url?: string;
   size?: number; // size in bytes
+  metadata: any; // other metadata stored in the file
 }
