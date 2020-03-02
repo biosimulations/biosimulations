053868605725bc0e5fad0944c53502737e46b950
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-03-02 12:11:13 -0500

    feat(services): add file service

---
+++ CRBM-Viz/src/app/Shared/Services/Resources/file.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, Injector } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { of, Observable } from 'rxjs';
+import { RemoteFile } from '../../Models/remote-file';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FileService {
+  constructor(private http: HttpClient, private injector: Injector) {}
+  public create(
+    file: File,
+    name?: string,
+    type?: string,
+    id?: string,
+    owner?: string,
+    isprivate?: boolean
+  ): Observable<string> {
+    return of('123456');
+  }
+  public read(id: string): Observable<RemoteFile> {
+    const file = new RemoteFile(
+      'model.xml',
+      '12345',
+      'bilalshaikh42',
+      false,
+      'xml',
+      'localhost:5001/5e571e1b7051aa2c8814ceeb/download',
+      200,
+      {}
+    );
+    return of(file);
+  }
+}
