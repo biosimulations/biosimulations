68efc1a43e0ff52250b16edc73b4351c0b6f647c
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-03-03 23:05:49 -0500

    feat(service): Breadcrumb service generates breadcrums
    Refactoring the creation of the navigation buttons and breadcrumbs to a central service rather than in each view

--- CRBM-Viz/src/app/Shared/Services/bread-crumbs.service.ts
+++ CRBM-Viz/src/app/Shared/Services/bread-crumbs.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
+import { NavItem } from '../Models/nav-item';
+import { NavItemDisplayLevel } from '../Enums/nav-item-display-level';
+import { AccessLevel } from '../Enums/access-level';
 
 @Injectable({
   providedIn: 'root',
@@ -20,6 +23,81 @@ export class BreadCrumbsService {
 
   set(crumbs: object[], buttons: object[], classes: string[] = []): void {
     this.dataSubject.next({ crumbs, buttons, classes });
+  }
+  setEditModel(model?, delFunct?: (model) => void) {
+    const crumbs: object[] = [{ label: 'Models', route: ['/models'] }];
+
+    if (model.id) {
+      crumbs.push({
+        label: 'Model ' + model.id,
+        route: ['/models', model.id],
+      });
+      crumbs.push({
+        label: 'Edit',
+      });
+    } else {
+      crumbs.push({
+        label: 'New',
+      });
+    }
+    const buttons: NavItem[] = [
+      {
+        iconType: 'mat',
+        icon: 'timeline',
+        label: 'Simulate',
+        route: ['/simulations', 'new', model?.id],
+        display: model?.id
+          ? NavItemDisplayLevel.always
+          : NavItemDisplayLevel.never,
+      },
+      {
+        iconType: 'fas',
+        icon: 'bars',
+        label: 'View',
+        route: ['/models', model?.id],
+        display: model?.id
+          ? NavItemDisplayLevel.always
+          : NavItemDisplayLevel.never,
+      },
+      {
+        iconType: 'fas',
+        icon: 'trash-alt',
+        label: 'Delete',
+        click: () => {
+          delFunct(model);
+        },
+        display:
+          model?.access === AccessLevel.public || delFunct == null
+            ? NavItemDisplayLevel.never
+            : NavItemDisplayLevel.user,
+        displayUser: model?.owner,
+      },
+      {
+        iconType: 'fas',
+        icon: 'plus',
+        label: 'New',
+        route: ['/models', 'new'],
+        display: model?.id
+          ? NavItemDisplayLevel.always
+          : NavItemDisplayLevel.never,
+      },
+      {
+        iconType: 'fas',
+        icon: 'user',
+        label: 'Your models',
+        route: ['/user', 'models'],
+        display: NavItemDisplayLevel.loggedIn,
+      },
+      {
+        iconType: 'fas',
+        icon: 'list',
+        label: 'Browse',
+        route: ['/models'],
+        display: NavItemDisplayLevel.always,
+      },
+    ];
+
+    this.set(crumbs, buttons);
   }
 
   clear(): void {
