88cace866e81e97def3dbbbedeacaf4daf24f435
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-03-10 11:17:29 -0400

    feat(forms): improve disable handling

--- CRBM-Viz/src/app/Shared/Forms/FormControlBase.ts
+++ CRBM-Viz/src/app/Shared/Forms/FormControlBase.ts
@@ -1,21 +1,46 @@
-import { OnDestroy } from "@angular/core";
+import { OnDestroy } from '@angular/core';
 import { ControlValueAccessor, FormGroup, FormControl } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
 export class FormControlBase implements OnDestroy, ControlValueAccessor {
     subscriptions: Subscription[] = []
     form: FormControl
-    isDisabled = false;
+
     get control() {
         return this.form
     }
+
+    /**
+     * The accessor for the value of the form. Override this in 
+     * subclasses to change or manipulate the value returned
+     * @type {string}
+     * @memberof FormControlBase
+     */
     get value(): string {
         return this.form.value
     }
+
+    /**
+     * Used to set the value. By default, takes in string and sets to string
+     * Should be overridden in subclasses with more complex data types
+     * @memberof FormControlBase
+     */
     set value(id: string) {
         this.form.setValue(id)
         this.onChange(id)
         this.onTouched()
+    }
+    get isDisabled() {
+        return this.isDisabled
+    }
+    set isDisabled(isDisabled: boolean) {
+        this.isDisabled = isDisabled
+        if (isDisabled) {
+            this.form.disable()
+        }
+        else {
+            this.form.enable()
+        }
     }
     ngOnDestroy(): void {
         this.subscriptions.forEach(s => s.unsubscribe())
@@ -33,12 +58,7 @@ export class FormControlBase implements OnDestroy, ControlValueAccessor {
     }
     setDisabledState?(isDisabled: boolean): void {
         this.isDisabled = isDisabled
-        if (isDisabled) {
-            this.form.disable()
-        }
-        else {
-            this.form.enable()
-        }
+
     }
 
     validate(_: FormControl) {
