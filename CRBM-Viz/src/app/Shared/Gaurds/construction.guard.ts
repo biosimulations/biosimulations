d931f6de41fc1adfa558355664b651ce4a160c6d
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-03-19 10:53:44 -0400

    feat(shared): add under constrcution gaurd

---
+++ CRBM-Viz/src/app/Shared/Gaurds/construction.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { ConfigurationService } from '../Services/configuration.service';
+import { pluck, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConstructionGuard implements CanActivate {
+  constructor(private config: ConfigurationService, private router: Router) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.config.showUnderConstructionPage$.pipe(
+      tap(redirect => {
+        if (redirect) {
+          this.router.navigateByUrl('/errors/underConstruction');
+        }
+      })
+    );
+  }
+}
