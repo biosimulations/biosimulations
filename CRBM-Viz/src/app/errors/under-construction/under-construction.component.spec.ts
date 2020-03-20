5d2fbe1405117f8acc52b7ae5cbc9c5b7d4f5974
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-03-19 20:11:03 -0400

    feat(errors): Create under construction component

--- CRBM-Viz/src/app/errors/under-construction/under-construction.component.spec.ts
+++ CRBM-Viz/src/app/errors/under-construction/under-construction.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import { UnderConstructionComponent } from './under-construction.component';
 
 describe('UnderConstructionComponent', () => {
@@ -8,8 +8,7 @@ describe('UnderConstructionComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UnderConstructionComponent ],
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      declarations: [ UnderConstructionComponent ]
     })
     .compileComponents();
   }));
--- CRBM-Viz/src/app/errors/under-construction/under-construction.component.ts
+++ CRBM-Viz/src/app/errors/under-construction/under-construction.component.ts
@@ -9,7 +9,7 @@ export class UnderConstructionComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
