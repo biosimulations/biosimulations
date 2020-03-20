e1c7d334ccde6684284ba1cab0ce38b2f109c3e9
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-03-19 20:10:15 -0400

    feat(errors): Create 404 component

---
+++ CRBM-Viz/src/app/errors/not-found/not-found.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { BreadCrumbsService } from 'src/app/Shared/Services/bread-crumbs.service';
+import { NavItem } from 'src/app/Shared/Models/nav-item';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.sass'],
+})
+export class NotFoundComponent implements OnInit {
+  constructor(
+    @Inject(BreadCrumbsService) private breadCrumbsService: BreadCrumbsService
+  ) {}
+
+  ngOnInit() {
+    const crumbs: object[] = [{ label: 'Error' }, { label: 'Page not found' }];
+    const buttons: NavItem[] = [];
+    this.breadCrumbsService.set(crumbs, buttons);
+  }
+}
