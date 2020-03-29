4ab5de7318027e31251ec4173801852a733da9d7
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-03-29 13:01:47 -0400

    fix typo

--- biosimulations/libs/datamodel/README.md
+++ biosimulations/libs/datamodel/README.md
@@ -1,4 +1,4 @@
-# datamodel
+#datamodel
 
 This library was generated with [Nx](https://nx.dev).
 
--- biosimulations/libs/datamodel/jest.config.js
+++ biosimulations/libs/datamodel/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: ' datamodel',
+  name: 'datamodel',
   preset: '../../jest.config.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/ datamodel',
+  coverageDirectory: '../../coverage/libs/datamodel',
 };
