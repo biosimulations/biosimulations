ec622848b0914bac04f3610e22b67f3f0b8fdbe5
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-03-29 14:17:22 -0400

    add api model library

---
+++ biosimulations/libs/datamodel/datamodel-api/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'datamodel-datamodel-api',
+  preset: '../../../jest.config.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../../coverage/libs/datamodel/datamodel-api',
+};
