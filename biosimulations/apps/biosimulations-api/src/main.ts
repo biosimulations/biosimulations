659f8b4a51cfb201aea00fc7d5b2bfac6c7a9c14
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-04-06 01:46:41 -0400

    feat(api): add open api spec generation

--- biosimulations/apps/biosimulations-api/src/main.ts
+++ biosimulations/apps/biosimulations-api/src/main.ts
@@ -4,17 +4,28 @@
  */
 
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+
+  setupOpenApi(app);
+
   const port = process.env.port || 3333;
   await app.listen(port, () => {
-    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    console.log('Listening at http://localhost:' + port + '/');
   });
 }
 
+function setupOpenApi(app) {
+  const options = new DocumentBuilder()
+    .setTitle('Biosimulations Resource API')
+    .setDescription('The API to interact with the Biosimulations Database')
+    .setVersion('0.1')
+    .addTag('Models')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
 bootstrap();
