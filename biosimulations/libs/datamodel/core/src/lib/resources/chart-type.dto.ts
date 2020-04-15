489d5ce13e42c5f33e45fd87c083c2c9572ba487
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-04-15 11:57:39 -0400

    feat(datamodel): redfine core resources as primary and secondary

--- biosimulations/libs/datamodel/core/src/lib/resources/chart-type.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ResourceDTO } from './resource.dto';
-import { ResourceType } from '../enums/resource-type';
-
-export class ChartTypeDTO extends ResourceDTO {
-  // TODO decide on the name for this. Chart or chart-type. Should be same name as cache key
-  type = ResourceType.chart;
-  spec: any;
-}
