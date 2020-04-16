217be14fcff929e4ce3f98ad4fd1790e9dd31aec
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-04-16 10:31:20 -0400

    tests(frontend): switch from karma to jest

---
+++ biosimulations/apps/biosimulations-frontend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'biosimulations-frontend',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/myapp',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
