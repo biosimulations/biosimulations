eb05030d658ff4570eac67c4fb08a91e38d70950
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-05-01 17:12:49 -0400

    feat(login): redirect works

--- biosimulations/apps/login-frontend/src/app/app.component.ts
+++ biosimulations/apps/login-frontend/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, pluck, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'biosimulations-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'login-frontend';
+export class AppComponent implements OnInit {
+  state: Observable<string>;
+  token: Observable<string>;
+  constructor(private route: ActivatedRoute, private router: Router) {}
+  ngOnInit(): void {
+    this.state = this.route.queryParams.pipe(pluck('state'));
+    this.token = this.route.queryParams.pipe(pluck('token'));
+  }
+  continueLogin() {
+    this.state.subscribe(
+      state =>
+        (window.location.href =
+          'https://auth.biosimulations.dev/continue?state=' + state),
+    );
+  }
 }
