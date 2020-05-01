5e896c2196cae4dc740b0ea3b1774bbba7cbb73a
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-05-01 14:45:01 -0400

    import theme into correct sccss

--- biosimulations/apps/login-frontend/src/app/app.module.ts
+++ biosimulations/apps/login-frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { UiMaterialModule } from '@biosimulations/ui/material';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -13,6 +14,7 @@ import { environment } from '../environments/environment';
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     UiMaterialModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
