35d7c23715fe1a4fd8a32164de68b28453472f75
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-05-06 11:51:53 -0400

    update page

--- biosimulations/apps/login-frontend/src/app/app.component.ts
+++ biosimulations/apps/login-frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { filter, pluck, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'biosimulations-root',
@@ -9,12 +10,28 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  userNameForm: FormGroup;
+  termsAndConditionsForm: FormGroup;
   state: Observable<string>;
   token: Observable<string>;
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  termsAndConditionsFormValue;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder,
+  ) {}
   ngOnInit(): void {
     this.state = this.route.queryParams.pipe(pluck('state'));
     this.token = this.route.queryParams.pipe(pluck('token'));
+
+    this.userNameForm = this.formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.termsAndConditionsForm = this.formBuilder.group({
+      tos: ['', Validators.requiredTrue],
+      coc: ['', Validators.requiredTrue],
+      ppo: ['', Validators.requiredTrue],
+    });
   }
   continueLogin() {
     this.state.subscribe(
--- biosimulations/apps/login-frontend/src/app/app.module.ts
+++ biosimulations/apps/login-frontend/src/app/app.module.ts
@@ -7,14 +7,26 @@ import { UiMaterialModule } from '@biosimulations/ui/material';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatStepperModule } from '@angular/material/stepper';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { DebugComponent } from './debug/debug.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DebugComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     UiMaterialModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
     BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatStepperModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
