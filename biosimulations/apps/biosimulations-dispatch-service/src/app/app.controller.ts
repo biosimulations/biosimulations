aa75b1662234289e2d7390259f01e63f89df5411
GMarupilla <marupilla@mail.com>
2020-05-06 16:20:43 -0400

    Add pseudocode and basic structure for dispatch app

--- biosimulations/apps/biosimulations-dispatch-service/src/app/app.controller.ts
+++ biosimulations/apps/biosimulations-dispatch-service/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Controller, Get, Logger, Post, Body } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
@@ -6,7 +6,12 @@ export class AppController {
   constructor() {}
   private logger = new Logger(AppController.name);
   @MessagePattern('dispatch')
+  // @Post('/dispatch')
   dispatch() {
+  // dispatch(@Body() simSpec: object) {
     this.logger.log('dispatching job');
+    // Get sim spec from message/request
+    // Call HPC manager
+    // Log at each step
   }
 }
---
+++ biosimulations/apps/biosimulations-dispatch-service/src/util/sbatch-generator.ts
@@ -0,0 +1,14 @@
+export class SbatchGenerator {
+    
+    // Template as string
+    private template = '';
+
+    constructor(simSpec) {
+        // Parse simspec for required params that will be used to generate SBATCH
+    }
+
+    generate() {
+        // fill out member variables in sbatch template
+        // Return the sbatch as string
+    }
+}
