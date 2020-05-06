db78ee8b9458eaf7d16a2f678134f10b07202490
GMarupilla <marupilla@mail.com>
2020-05-06 17:15:12 -0400

    Move util code to services, remove util

--- biosimulations/apps/biosimulations-dispatch-service/src/app/services/hpc/hpc.service.ts
+++ biosimulations/apps/biosimulations-dispatch-service/src/app/services/hpc/hpc.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class HpcService {}
+export class HpcService {
+    constructor(username,password, server, sftp_server) {
+        // Setup class variables in params
+        // Establish SSH and SFTP connection with HPC
+    }
+
+    dispatchJob(simSpec, omex) {
+        // get remote InDir and OutDir from config (ideally indir name should be simId)
+        // Expect omex as file
+        // Save omex file locally (so that it can be transferred over SSH session)
+        // Call SBATCH generator by passing sim spec
+        // Transfer omex and sbatch to inDir on HPC via SFTP session
+        // execute SBATCH via SSH session
+        // Frequent logging at every step
+    }
+
+    getOutput(simId) {
+        // pack all files (zip)
+        // Get them on local
+        // Unpack them and save to mongo
+    }
+}
--- biosimulations/apps/biosimulations-dispatch-service/src/app/services/sbatch/sbatch.service.ts
+++ biosimulations/apps/biosimulations-dispatch-service/src/app/services/sbatch/sbatch.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SbatchService {}
+export class SbatchService {
+     
+    // Template as string
+    private template = '';
+
+    constructor(simSpec) {
+        // Parse simspec for required params that will be used to generate SBATCH
+    }
+
+    generate() {
+        // fill out member variables in sbatch template
+        // Return the sbatch as string
+    }
+}
