559f86fda73a07cc0b71fd645d1b6160733f76d4
GMarupilla <marupilla@mail.com>
2020-05-07 11:20:12 -0400

    Remove App.module references of util references, complete stringCommand executing method

--- biosimulations/apps/biosimulations-dispatch-service/src/app/app.module.ts
+++ biosimulations/apps/biosimulations-dispatch-service/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
-import { HpcService } from './services/hpc/hpc.service';
-import { SbatchService } from './services/sbatch/sbatch.service';
-import { SshService } from './services/ssh/ssh.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [HpcService, SbatchService, SshService],
+  providers: [],
 })
 export class AppModule {}
--- biosimulations/apps/biosimulations-dispatch-service/src/app/utils/ssh/ssh.ts
+++ biosimulations/apps/biosimulations-dispatch-service/src/app/utils/ssh/ssh.ts
@@ -1,1 +1,43 @@
-export class Ssh {}
+import { Logger } from '@nestjs/common';
+import { Client } from 'ssh2';
+
+export class Ssh {
+    private logger = new Logger(Ssh.name);
+    private host = null;
+    private port = null;
+    private username = null;
+    private password = null;
+
+    constructor(host: string, port: number, username: string, password: string) {
+        this.host = host;
+        this.port = port;
+        this.username = username;
+        this.password = password;
+    }
+
+    stringCommand(cmd: string): Promise<{ stdout: string; stderr: string; }> {
+        return new Promise<{ stdout: string; stderr: string; }>((resolve, reject) => {
+            const conn = new Client();
+            conn.on('ready', () => {
+              this.logger.log('Connection ready');
+              let stdout = '';
+              let stderr = '';
+              conn.exec(cmd, (err, stream) => {
+                if(err){
+                    reject(err);  
+                } 
+                stream.on('close', (code, signal) => {
+                  resolve({ stdout, stderr });
+                  conn.end();
+                  this.logger.log('Connection closed')
+                }).on('data', (data) => {
+                  stdout += data.toString('utf8');
+                }).stderr.on('data', (data) => {
+                  stderr += data.toString('utf8');
+                });
+              });
+        
+            }).connect({ host: this.host, port: this.port, username: this.username, password: this.password });
+          });
+    }
+}
