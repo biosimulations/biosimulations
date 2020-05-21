0f1478f56c6fd9efa1b98d8d66d4afa266f7223f
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-05-21 18:06:47 -0400

    fix test

--- biosimulations/apps/login-frontend/src/app/app.component.spec.ts
+++ biosimulations/apps/login-frontend/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { UiMaterialModule } from '@biosimulations/ui/material';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, UiMaterialModule],
       declarations: [AppComponent],
     }).compileComponents();
   }));
