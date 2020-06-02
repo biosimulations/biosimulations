354f83c69b1c186dfbefd15ca6325d8a7262cfa1
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-06-02 13:37:27 -0400

    use custom config

--- biosimulations/apps/biosimulations-api/src/app/app.module.ts
+++ biosimulations/apps/biosimulations-api/src/app/app.module.ts
@@ -9,20 +9,18 @@ import { ProjectsModule } from './resources/projects/projects.module';
 import { SimulationsModule } from './resources/simulations/simulations.module';
 import { VisualizationsModule } from './resources/visualizations/visualizations.module';
 import { ChartsModule } from './resources/charts/charts.module';
-
+import { BiosimulationsConfigModule } from '@biosimulations/shared/biosimulations-config';
+import { BiosimulationsAuthModule } from '@biosimulations/shared/biosimulations-auth';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { ResourceRepository } from './resources/base/resource.repository';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [config],
-      isGlobal: true,
-      envFilePath: './config.env',
-    }),
+    BiosimulationsConfigModule,
+    BiosimulationsAuthModule,
     TypegooseModule.forRootAsync({
       // This line is not needed since config module is global. will be needed if used in another app after abstraction
-      imports: [ConfigModule],
+      imports: [BiosimulationsConfigModule],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get('database.uri'),
         useNewUrlParser: true,
--- biosimulations/apps/login-frontend/src/app/registration/registration.service.ts
+++ biosimulations/apps/login-frontend/src/app/registration/registration.service.ts
@@ -3,19 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { AsyncValidatorFn, AbstractControl } from '@angular/forms';
 import { map, catchError, switchMap, tap } from 'rxjs/operators';
 import { of, timer, ObservableInput, Observable, throwError } from 'rxjs';
-import { environment } from '../../environments/environment'
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class RegistrationService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   register(username: string, token: string | null, profile?: any) {
     return this.http
       .post(environment.api, { username, token, profile })
       .pipe(catchError(this.handleError));
-
-
   }
 
   // TODO make sure this handles the error properly
@@ -35,15 +33,11 @@ export class RegistrationService {
     // Browser will display console error for every bad call.Maybe include an actual endpoint to test this ?
     return timer(500).pipe(
       switchMap(_ =>
-        this.http.get<any>(
-          environment.api + 'valid/' + control.value,
-        ),
+        this.http.get<any>(environment.api + 'valid/' + control.value),
       ),
-      map(res => res.valid == true ? (null) : { server: res.message }),
+      map(res => (res.valid === true ? null : { server: res.message })),
       tap(_ => control.markAsTouched()),
-      catchError((err, caught) =>
-        of({ 'Network Error': err }),
-      ),
+      catchError((err, caught) => of({ 'Network Error': err })),
     );
   };
 }
