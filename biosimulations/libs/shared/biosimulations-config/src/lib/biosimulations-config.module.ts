beee07fffcbffaeffbc553e9fa98ed8805ca363f
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-06-10 19:48:16 -0400

    use config from mounted secret

--- biosimulations/libs/shared/biosimulations-config/src/lib/biosimulations-config.module.ts
+++ biosimulations/libs/shared/biosimulations-config/src/lib/biosimulations-config.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { nestConfig } from './biosimulations-secrets';
 @Module({
-    imports: [ConfigModule.forRoot({
-        isGlobal: true,
-        load: [nestConfig],
-        envFilePath: './config.env',
-    }),],
-    providers: [ConfigService],
-    exports: [ConfigService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [nestConfig],
+      envFilePath: ['./config/config.env', './config.env'],
+    }),
+  ],
+  providers: [ConfigService],
+  exports: [ConfigService],
 })
-export class BiosimulationsConfigModule { }
-
+export class BiosimulationsConfigModule {}
