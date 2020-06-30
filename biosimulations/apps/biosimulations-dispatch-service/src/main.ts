23ce62c87a23014e827fb158c4bb8b82b272b8f0
GMarupilla <marupilla@mail.com>
2020-06-30 16:16:16 -0400

    Dummy code to get host/port from config

--- biosimulations/apps/biosimulations-dispatch-service/src/main.ts
+++ biosimulations/apps/biosimulations-dispatch-service/src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 4444;
   const host = process.env.HOST || 'localhost';
+  // const port = app.get('ConfigService').get('dispatchService.port') || 4444;
+  // const host = app.get('ConfigService').get('dispatchService.host') || 'localhost';
   await app.listen(port, () => {
     logger.log(`Listening at http://${host}:${port}/${globalPrefix}`);
   });
