32b09bf22bbdc5af937c5f8c381ee49b71e33570
GMarupilla <marupilla@mail.com>
2020-06-30 15:05:13 -0400

    NATS config restructure

--- biosimulations/libs/shared/biosimulations-config/src/lib/biosimulations-nats-config.ts
+++ biosimulations/libs/shared/biosimulations-config/src/lib/biosimulations-nats-config.ts
@@ -2,13 +2,13 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('nats', () => {
   const config = {
-    host: process.env.NATS_HOST,
-    clientPort: process.env.NATS_CLIENT_PORT,
-    routePort: process.env.NATS_ROUTE_PORT,
-    monitoringPort: process.env.NATS_MONITOR_PORT,
-    username: process.env.NATS_USERNAME,
-    password: process.env.NATS_PASSWORD,
-    token: process.env.NATS_TOKEN
+    url: process.env.NATS_HOST + ':' + process.env.NATS_CLIENT_PORT,
+    user: process.env.NATS_USERNAME,
+    pass: process.env.NATS_PASSWORD,
+    // clientPort: process.env.NATS_CLIENT_PORT,
+    // routePort: process.env.NATS_ROUTE_PORT,
+    // monitoringPort: process.env.NATS_MONITOR_PORT,
+    // token: process.env.NATS_TOKEN
   };
 
   return config;
