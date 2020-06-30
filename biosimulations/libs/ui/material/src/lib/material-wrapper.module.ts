653e1ff75d5d47744bd9296e4454a81bc8aadd00
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-06-30 13:30:22 -0400

    add options to topbar

---
+++ biosimulations/libs/ui/material/src/lib/material-wrapper.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatNavList, MatListModule } from '@angular/material/list';
+
+const materialImports = [
+  FlexLayoutModule,
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatStepperModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+];
+
+@NgModule({
+  imports: [CommonModule, ...materialImports],
+  exports: [...materialImports],
+})
+export class MaterialWrapperModule {}
