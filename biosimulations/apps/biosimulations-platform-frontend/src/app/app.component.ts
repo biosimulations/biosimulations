1235a26e692895dfea73bf7b6d46df82595f6dfa
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-06-30 10:15:58 -0400

    create new frontend app

---
+++ biosimulations/apps/biosimulations-platform-frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'biosimulations-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'biosimulations-platform-frontend';
+}
