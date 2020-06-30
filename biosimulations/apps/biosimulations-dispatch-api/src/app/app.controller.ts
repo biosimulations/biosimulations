66a80a7ee78b4ecd77c581b424d986dbc55c1c18
GMarupilla <marupilla@mail.com>
2020-06-30 13:10:10 -0400

    NATS connection working

--- biosimulations/apps/biosimulations-dispatch-api/src/app/app.controller.ts
+++ biosimulations/apps/biosimulations-dispatch-api/src/app/app.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('DISPATCH_MQ') private messageClient: ClientProxy
+    ) {}
 
-  @Get()
+  @Get('/')
   getData() {
+    this.messageClient.emit('dispatch', 'Hello from DISPATCH API CLIENT!!!!!');
     return this.appService.getData();
   }
+
+  // async OnApplicationBootstrap() {
+  //   await this.messageClient.connect();
+  // }
 }
--- biosimulations/apps/biosimulations-dispatch-api/src/app/app.module.ts
+++ biosimulations/apps/biosimulations-dispatch-api/src/app/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
-
+import { ClientsModule, ClientProxy, Transport, ClientProxyFactory } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'DISPATCH_MQ',
+        transport: Transport.NATS
+      }
+    ])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    ConfigService,
+    // {
+    //   provide: 'DISPATCH_MQ',
+    //   useFactory: (configService: ConfigService) => {
+    //     const natsOptions = configService.get('nats');
+    //     return ClientProxyFactory.create(natsOptions);
+    //   },
+    //   inject: [ConfigService],
+    // }
+  ],
 })
 export class AppModule {}
