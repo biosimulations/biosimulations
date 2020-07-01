80c3ead42db9fa3a6bf79aa12695ce2577aa5ca1
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-07-01 10:35:23 -0400

    start working on model

--- biosimulations/apps/biosimulations-platform-frontend/src/app/models/services/model-http.service.ts
+++ biosimulations/apps/biosimulations-platform-frontend/src/app/models/services/model-http.service.ts
@@ -1,21 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ModelsDocument, ModelDocument } from '@biosimulations/datamodel/api';
+import {
+  ModelsDocument,
+  ModelDocument,
+  ModelResource,
+} from '@biosimulations/datamodel/api';
 import { Observable } from 'rxjs';
 import { map, pluck } from 'rxjs/operators';
-
+import { Model } from '../model';
 @Injectable({
   providedIn: 'root',
 })
 export class ModelHttpService {
-  url = 'https://api.biosimulations.org/models';
+  //url = 'https://api.biosimulations.dev/models';
+  url = 'http://localhost:3333/models';
   constructor(private http: HttpClient) {}
-
-  getAll() {
-    return this.http.get<ModelsDocument>(this.url).pipe(pluck('data'));
+  // TODO make this a behavior subject that updates as needed
+  models: Map<string, ModelResource> = new Map();
+  loadAll() {
+    return this.http.get<ModelsDocument>(this.url).pipe(
+      pluck('data'),
+      map((value: ModelResource[]) => {
+        value.map((resource: ModelResource) => {
+          this.models.set(resource.id, resource);
+        });
+      }),
+    );
   }
 
-  getOne(id: string) {
+  load(id: string) {
     this.http.get<ModelDocument>(this.url + '/' + id);
   }
 
