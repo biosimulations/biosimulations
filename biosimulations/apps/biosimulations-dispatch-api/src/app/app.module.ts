bcc731984f0e69720e3906937bed74f3b6d6997d
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-07-01 10:46:00 -0400

    fix nats config

--- biosimulations/apps/biosimulations-dispatch-api/src/app/app.module.ts
+++ biosimulations/apps/biosimulations-dispatch-api/src/app/app.module.ts
@@ -1,30 +1,35 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, ClientProxy, Transport, ClientProxyFactory } from '@nestjs/microservices';
+import {
+  ClientsModule,
+  ClientProxy,
+  Transport,
+  ClientProxyFactory,
+} from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
-
+import { BiosimulationsConfigModule } from '@biosimulations/shared/biosimulations-config';
 @Module({
   imports: [
+    BiosimulationsConfigModule,
     ClientsModule.register([
       {
         name: 'DISPATCH_MQ',
-        transport: Transport.NATS
-      }
-    ])
+        transport: Transport.NATS,
+      },
+    ]),
   ],
   controllers: [AppController],
   providers: [
     AppService,
-    ConfigService,
-    // {
-    //   provide: 'DISPATCH_MQ',
-    //   useFactory: (configService: ConfigService) => {
-    //     const natsOptions = configService.get('nats');
-    //     return ClientProxyFactory.create(natsOptions);
-    //   },
-    //   inject: [ConfigService],
-    // }
+    {
+      provide: 'DISPATCH_MQ',
+      useFactory: (configService: ConfigService) => {
+        const natsOptions = configService.get('nats');
+        return ClientProxyFactory.create(natsOptions);
+      },
+      inject: [ConfigService],
+    },
   ],
 })
 export class AppModule {}
