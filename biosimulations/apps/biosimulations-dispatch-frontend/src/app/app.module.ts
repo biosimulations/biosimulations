e9ac4c5e24eba975eac8986fa3f421b0ca17a96f
GMarupilla <marupilla@mail.com>
2020-07-08 17:39:23 -0400

    Add components for header, footer, main page, add basic navbar and footer code, add bootstrap,jquery and popper as dependencies

--- biosimulations/apps/biosimulations-dispatch-frontend/src/app/app.module.ts
+++ biosimulations/apps/biosimulations-dispatch-frontend/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainComponent
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
---
+++ biosimulations/apps/biosimulations-dispatch-frontend/src/app/components/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'biosimulations-dispatch-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent {
+  title = 'biosimulations-dispatch-frontend';
+}
