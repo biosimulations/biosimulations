05248b982ae7b429e23b106f13e2f9763191751a
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-07-08 21:54:17 -0400

    create shell

--- biosimulations/apps/biosimulations-platform-frontend/src/app/home/home.component.ts
+++ biosimulations/apps/biosimulations-platform-frontend/src/app/home/home.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { AuthService } from '@biosimulations/auth/frontend';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'biosimulations-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  loggedIn!: Observable<boolean>;
+  asyncLoggedIn = this.auth.isAuthenticated();
+  constructor(private auth: AuthService) {}
 
   ngOnInit(): void {
+    this.loggedIn = this.auth.isAuthenticated$;
+    this.asyncLoggedIn = this.auth.isAuthenticated();
   }
-
+  refresh() {
+    this.asyncLoggedIn = this.auth.isAuthenticated();
+  }
 }
