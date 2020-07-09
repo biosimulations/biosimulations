cc5cbf7c6038c6f77405a8d319776f83a1f5ea5c
Akhil Marupilla <marupilla@uchc.edu>
2020-07-09 12:47:31 -0400

    add OAS documentation

--- biosimulations/apps/biosimulations-dispatch-api/src/app/app.module.ts
+++ biosimulations/apps/biosimulations-dispatch-api/src/app/app.module.ts
@@ -1,11 +1,33 @@
 import { Module } from '@nestjs/common';
-
+import {
+  ClientsModule,
+  ClientProxy,
+  Transport,
+  ClientProxyFactory,
+  NatsOptions
+} from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigService } from '@nestjs/config';
+import { BiosimulationsConfigModule } from '@biosimulations/shared/biosimulations-config';
 @Module({
-  imports: [],
+  imports: [
+    BiosimulationsConfigModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'DISPATCH_MQ',
+      useFactory: (configService: ConfigService) => {
+        const natsServerConfig = configService.get('nats');
+        const natsOptions: NatsOptions = {};
+        natsOptions.transport = Transport.NATS;
+        natsOptions.options = natsServerConfig;
+        return ClientProxyFactory.create(natsOptions);
+      },
+      inject: [ConfigService],
+    },
+  ],
 })
 export class AppModule {}
--- biosimulations/apps/biosimulations-dispatch-api/src/main.ts
+++ biosimulations/apps/biosimulations-dispatch-api/src/main.ts
@@ -7,15 +7,30 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  // Swagger doc
+  const options = new DocumentBuilder()
+    .setTitle('Simulation dispatch example')
+    .setDescription('Dispatch API allows dispatching of simulation jobs to UConn HPC')
+    .setVersion('1.0')
+    .addTag('dispatch')
+    // .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('', app, document);
+
+
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
+  
 }
 
 bootstrap();
