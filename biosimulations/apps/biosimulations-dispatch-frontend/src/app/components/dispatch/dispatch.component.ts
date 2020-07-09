4da7c0dde8d9ebc3802e49d1d1ca32e347e19a70
GMarupilla <marupilla@mail.com>
2020-07-09 13:17:29 -0400

    Add form to dispatch simulation

--- biosimulations/apps/biosimulations-dispatch-frontend/src/app/components/dispatch/dispatch.component.ts
+++ biosimulations/apps/biosimulations-dispatch-frontend/src/app/components/dispatch/dispatch.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DispatchComponent implements OnInit {
 
+  simulators = ['COPASI', 'VCell'];
+
   constructor() { }
 
   ngOnInit(): void {
