35f5fb63beb5ce7cc8225420cff090730be881e0
GMarupilla <marupilla@mail.com>
2020-07-09 11:33:42 -0400

    Add component for dispatch route, comment search bar code, remove main component

--- biosimulations/apps/biosimulations-dispatch-frontend/src/app/app.module.ts
+++ biosimulations/apps/biosimulations-dispatch-frontend/src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { MainComponent } from './components/main/main.component';
 import { HomeComponent } from './components/home/home.component';
+import { DispatchComponent } from './components/dispatch/dispatch.component';
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'dispatch', component: DispatchComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    MainComponent,
-    HomeComponent
+    HomeComponent,
+    DispatchComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
