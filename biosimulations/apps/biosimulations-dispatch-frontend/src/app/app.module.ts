6d37a911bbe77dc11541f732ced49fa0a4a30620
GMarupilla <marupilla@mail.com>
2020-07-09 10:47:22 -0400

    Add component for home route

--- biosimulations/apps/biosimulations-dispatch-frontend/src/app/app.module.ts
+++ biosimulations/apps/biosimulations-dispatch-frontend/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
