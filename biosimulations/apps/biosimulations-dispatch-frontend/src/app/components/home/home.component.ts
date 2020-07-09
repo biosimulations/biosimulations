949ff3955eef674450463e6bde9a6bca11b37385
GMarupilla <marupilla@mail.com>
2020-07-09 10:47:22 -0400

    Add component for home route

---
+++ biosimulations/apps/biosimulations-dispatch-frontend/src/app/components/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'biosimulations-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
