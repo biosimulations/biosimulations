e0a40acb425f2066ce8096ac03a0e34bdcb0dfb0
GMarupilla <marupilla@mail.com>
2020-07-11 12:10:33 -0400

    Add configurations, log statements

--- biosimulations/apps/biosimulations-dispatch-frontend/src/app/services/dispatch/dispatch.service.ts
+++ biosimulations/apps/biosimulations-dispatch-frontend/src/app/services/dispatch/dispatch.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.dev';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DispatchService {
   submitJob(fileToUpload: File, selectedSimulator: string) {
-    const endpoint = 'http://localhost:3333/api/dispatch';
+    const endpoint = `${environment.crbm.DISPATCH_API_URL}/dispatch`;
+
+    // TODO: Create a datamodel to hold the schema for simulation spec for frontend
     const formData = new FormData();
     formData.append('file', fileToUpload, fileToUpload.name);
     formData.append('simulator', selectedSimulator);
