280d00f57999985f4c96f23642208ab3ff7610ff
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-07-15 00:47:25 -0400

    add framework viewmodel

---
+++ biosimulations/apps/biosimulations-platform-frontend/src/app/shared/viewmodels/framework.ts
@@ -0,0 +1,36 @@
+import { ViewModel } from './view';
+import { OntologyTerm as IFramework } from '@biosimulations/datamodel/core';
+export class Framework implements ViewModel, IFramework {
+  constructor(framework: IFramework) {
+    this.ontology = framework.ontology;
+    this.id = framework.id;
+    this.name = framework.name;
+    this.description = framework.description;
+    this.iri = framework.iri;
+  }
+  ontology: string;
+  id: string;
+  name: string;
+  description: string | null;
+  iri: string | null;
+  toString(): string {
+    return this.name;
+  }
+  icon():
+    | 'taxon'
+    | 'authors'
+    | 'home'
+    | 'file'
+    | 'question'
+    | 'author'
+    | 'model'
+    | null {
+    throw new Error('Method not implemented.');
+  }
+  link(): string | null {
+    return this.iri;
+  }
+  match(predicate: any): boolean {
+    throw new Error('Method not implemented.');
+  }
+}
