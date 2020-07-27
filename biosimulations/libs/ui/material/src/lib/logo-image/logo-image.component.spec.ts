a211dc9e39cfe818a14efd0307579c20514995be
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-07-27 11:58:30 -0400

    add components for logo

---
+++ biosimulations/libs/ui/material/src/lib/logo-image/logo-image.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogoImageComponent } from './logo-image.component';
+
+describe('LogoImageComponent', () => {
+  let component: LogoImageComponent;
+  let fixture: ComponentFixture<LogoImageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LogoImageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogoImageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ biosimulations/libs/ui/material/src/lib/logo-text/logo-text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'biosimulations-logo-text',
+  templateUrl: './logo-text.component.html',
+  styleUrls: ['./logo-text.component.scss']
+})
+export class LogoTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
