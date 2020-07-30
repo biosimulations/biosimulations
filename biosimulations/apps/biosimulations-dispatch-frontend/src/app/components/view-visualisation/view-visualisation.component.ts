867e2678461b9e3df33f797d375ed33280d6e9f6
GMarupilla <marupilla@mail.com>
2020-07-30 13:25:52 -0400

    Add static data from simulation result

--- biosimulations/apps/biosimulations-dispatch-frontend/src/app/components/view-visualisation/view-visualisation.component.ts
+++ biosimulations/apps/biosimulations-dispatch-frontend/src/app/components/view-visualisation/view-visualisation.component.ts
@@ -75,106 +75,296 @@ export class ViewVisualisationComponent implements OnInit {
 
   ngOnInit(): void {
     this.spec = {
-      "$schema": "https://vega.github.io/schema/vega/v5.json",
-      "description": "A basic line chart example.",
-      "width": 500,
-      "height": 200,
-      "padding": 5,
+      $schema: 'https://vega.github.io/schema/vega/v5.json',
+      description: 'A basic line chart example.',
+      width: 500,
+      height: 200,
+      padding: 5,
     
-      "signals": [
+      signals: [
         {
-          "name": "interpolate",
-          "value": "linear",
-          "bind": {
-            "input": "select",
-            "options": [
-              "basis",
-              "cardinal",
-              "catmull-rom",
-              "linear",
-              "monotone",
-              "natural",
-              "step",
-              "step-after",
-              "step-before"
+          name: 'interpolate',
+          value: 'linear',
+          bind: {
+            input: 'select',
+            options: [
+              'basis',
+              'cardinal',
+              'catmull-rom',
+              'linear',
+              'monotone',
+              'natural',
+              'step',
+              'step-after',
+              'step-before'
             ]
           }
         }
       ],
     
-      "data": [
+      data: [
         {
-          "name": "table",
-          "values": [
-            {"x": 0, "y": 28, "c":0}, {"x": 0, "y": 20, "c":1},
-            {"x": 1, "y": 43, "c":0}, {"x": 1, "y": 35, "c":1},
-            {"x": 2, "y": 81, "c":0}, {"x": 2, "y": 10, "c":1},
-            {"x": 3, "y": 19, "c":0}, {"x": 3, "y": 15, "c":1},
-            {"x": 4, "y": 52, "c":0}, {"x": 4, "y": 48, "c":1},
-            {"x": 5, "y": 24, "c":0}, {"x": 5, "y": 28, "c":1},
-            {"x": 6, "y": 87, "c":0}, {"x": 6, "y": 66, "c":1},
-            {"x": 7, "y": 17, "c":0}, {"x": 7, "y": 27, "c":1},
-            {"x": 8, "y": 68, "c":0}, {"x": 8, "y": 16, "c":1},
-            {"x": 9, "y": 49, "c":0}, {"x": 9, "y": 25, "c":1}
+          name: 'table',
+          values: [
+            {
+              "x": 0,
+              "y": 50,
+              "c": 0
+            },
+            {
+              "x": 3.6,
+              "y": 48.8285,
+              "c": 0
+            },
+            {
+              "x": 7.2,
+              "y": 47.7821,
+              "c": 0
+            },
+            {
+              "x": 10.8,
+              "y": 46.8427,
+              "c": 0
+            },
+            {
+              "x": 14.4,
+              "y": 45.9957,
+              "c": 0
+            },
+            {
+              "x": 18,
+              "y": 45.2295,
+              "c": 0
+            },
+            {
+              "x": 21.6,
+              "y": 44.5341,
+              "c": 0
+            },
+            {
+              "x": 25.2,
+              "y": 43.9015,
+              "c": 0
+            },
+            {
+              "x": 28.8,
+              "y": 43.3248,
+              "c": 0
+            },
+            {
+              "x": 32.4,
+              "y": 42.7978,
+              "c": 0
+            },
+            {
+              "x": 36,
+              "y": 42.3157,
+              "c": 0
+            },
+            {
+              "x": 39.6,
+              "y": 41.8739,
+              "c": 0
+            },
+            {
+              "x": 43.2,
+              "y": 41.4685,
+              "c": 0
+            },
+            {
+              "x": 46.8,
+              "y": 41.096,
+              "c": 0
+            },
+            {
+              "x": 50.4,
+              "y": 40.7536,
+              "c": 0
+            },
+            {
+              "x": 54,
+              "y": 40.4384,
+              "c": 0
+            },
+            {
+              "x": 57.6,
+              "y": 40.148,
+              "c": 0
+            },
+            {
+              "x": 61.2,
+              "y": 39.8803,
+              "c": 0
+            },
+            {
+              "x": 64.8,
+              "y": 39.6334,
+              "c": 0
+            },
+            {
+              "x": 68.4,
+              "y": 39.4055,
+              "c": 0
+            },
+            {
+              "x": 3531.6,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3535.2,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3538.8,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3542.4,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3546,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3549.6,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3553.2,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3556.8,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3560.4,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3564,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3567.6,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3571.2,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3574.8,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3578.4,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3582,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3585.6,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3589.2,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3592.8,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3596.4,
+              "y": 36.5385,
+              "c": 1
+            },
+            {
+              "x": 3600,
+              "y": 36.5385,
+              "c": 1
+            }
           ]
         }
       ],
     
-      "scales": [
+      scales: [
         {
-          "name": "x",
-          "type": "point",
-          "range": "width",
-          "domain": {"data": "table", "field": "x"}
+          name: 'x',
+          type: 'point',
+          range: 'width',
+          domain: {data: 'table', field: 'x'}
         },
         {
-          "name": "y",
-          "type": "linear",
-          "range": "height",
-          "nice": true,
-          "zero": true,
-          "domain": {"data": "table", "field": "y"}
+          name: 'y',
+          type: 'linear',
+          range: 'height',
+          nice: true,
+          zero: true,
+          domain: {data: 'table', field: 'y'}
         },
         {
-          "name": "color",
-          "type": "ordinal",
-          "range": "category",
-          "domain": {"data": "table", "field": "c"}
+          name: 'color',
+          type: 'ordinal',
+          range: 'category',
+          domain: {data: 'table', field: 'c'}
         }
       ],
     
-      "axes": [
-        {"orient": "bottom", "scale": "x"},
-        {"orient": "left", "scale": "y"}
+      axes: [
+        {orient: 'bottom', scale: 'x'},
+        {orient: 'left', scale: 'y'}
       ],
     
-      "marks": [
+      marks: [
         {
-          "type": "group",
-          "from": {
-            "facet": {
-              "name": "series",
-              "data": "table",
-              "groupby": "c"
+          type: 'group',
+          from: {
+            facet: {
+              name: 'series',
+              data: 'table',
+              groupby: 'c'
             }
           },
-          "marks": [
+          marks: [
             {
-              "type": "line",
-              "from": {"data": "series"},
-              "encode": {
-                "enter": {
-                  "x": {"scale": "x", "field": "x"},
-                  "y": {"scale": "y", "field": "y"},
-                  "stroke": {"scale": "color", "field": "c"},
-                  "strokeWidth": {"value": 2}
+              type: 'line',
+              from: {data: 'series'},
+              encode: {
+                enter: {
+                  x: {scale: 'x', field: 'x'},
+                  y: {scale: 'y', field: 'y'},
+                  stroke: {scale: 'color', field: 'c'},
+                  strokeWidth: {value: 2}
                 },
-                "update": {
-                  "interpolate": {"signal": "interpolate"},
-                  "strokeOpacity": {"value": 1}
+                update: {
+                  interpolate: {signal: 'interpolate'},
+                  strokeOpacity: {value: 1}
                 },
-                "hover": {
-                  "strokeOpacity": {"value": 0.5}
+                hover: {
+                  strokeOpacity: {value: 0.5}
                 }
               }
             }
