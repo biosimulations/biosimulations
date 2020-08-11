ef53582990145787ae5a8f5d6a17eab7d0b0260f
Jonathan Karr <jonrkarr@gmail.com>
2020-08-11 12:10:47 -0400

    adding icons to models module

--- biosimulations/apps/biosimulations-platform-frontend/src/app/models/models.module.ts
+++ biosimulations/apps/biosimulations-platform-frontend/src/app/models/models.module.ts
@@ -19,8 +19,11 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { DebugModule } from '@biosimulations/shared/debug';
 import { ResourceViewModule } from '@biosimulations/resource/view';
+import { BiosimulationsIconsModule } from '@biosimulations/ui/icons';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+
+
 const routes: Routes = [{ path: '', component: ModelsComponent }];
 
 @NgModule({
@@ -46,6 +49,7 @@ const routes: Routes = [{ path: '', component: ModelsComponent }];
     MatCheckboxModule,
     DragDropModule,
     ResourceViewModule,
+    BiosimulationsIconsModule,
   ],
   providers: [],
 })
