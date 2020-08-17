77f4d8b39973d2645f0cea1eca0494a27b85cf71
GMarupilla <marupilla@mail.com>
2020-08-17 16:49:43 -0400

    Fix the test for api including simulator version

--- biosimulations/apps/biosimulations-dispatch-api/src/app/app.controller.spec.ts
+++ biosimulations/apps/biosimulations-dispatch-api/src/app/app.controller.spec.ts
@@ -38,6 +38,7 @@ describe('AppController', () => {
       }, {
         filepathOnDataStore: '',
         simulator: '',
+        simulatorVersion: '',
         filename: '',
         uniqueFilename: ''
         
