9841326ad6a8da6057d5a6f62c4bd6f8da445f1f
Jonathan Karr <jonrkarr@gmail.com>
2020-08-21 22:51:51 -0400

    adding scroll restoration to apps

--- biosimulations/apps/simulators/src/app/app.module.ts
+++ biosimulations/apps/simulators/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { RouterModule } from '@angular/router';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot([], { initialNavigation: 'enabled', scrollPositionRestoration: 'enabled' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
