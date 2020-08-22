b0d33a10d6b903ff9bf3fdd2d2bd0e23a0bd6aa0
Jonathan Karr <jonrkarr@gmail.com>
2020-08-22 01:44:10 -0400

    removing unused import

--- biosimulations/libs/shared/ui/src/lib/privacy-notice/privacy-notice.component.spec.ts
+++ biosimulations/libs/shared/ui/src/lib/privacy-notice/privacy-notice.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { PrivacyNoticeComponent } from './privacy-notice.component';
 import { RouterTestingModule } from '@angular/router/testing';
