8795ec486c1f61ea790ab1e38aead4bbb8bfa6be
GMarupilla <marupilla@mail.com>
2020-08-25 15:39:34 -0400

    Beautify the JSON format

--- biosimulations/apps/dispatch-service/src/app/app.controller.spec.ts
+++ biosimulations/apps/dispatch-service/src/app/app.controller.spec.ts
@@ -67,12 +67,63 @@ describe('AppController', () => {
       expect(
         appController.convertJsonDataToChartData(
           JSON.parse(
-            `[{"time":"0.0","UnInfected_Tumour_Cells_Xu":"500000.0"},{"time":"0.1","UnInfected_Tumour_Cells_Xu":"548268.0"},{"time":"0.2","UnInfected_Tumour_Cells_Xu":"600386.0"},{"time":"0.3","UnInfected_Tumour_Cells_Xu":"655801.0"},{"time":"0.4","UnInfected_Tumour_Cells_Xu":"713931.0"},{"time":"0.5","UnInfected_Tumour_Cells_Xu":"774275.0"},{"time":"0.6","UnInfected_Tumour_Cells_Xu":"836486.0"}]`
+            `[
+              {
+                "time": "0.0",
+                "UnInfected_Tumour_Cells_Xu": "500000.0"
+              },
+              {
+                "time": "0.1",
+                "UnInfected_Tumour_Cells_Xu": "548268.0"
+              },
+              {
+                "time": "0.2",
+                "UnInfected_Tumour_Cells_Xu": "600386.0"
+              },
+              {
+                "time": "0.3",
+                "UnInfected_Tumour_Cells_Xu": "655801.0"
+              },
+              {
+                "time": "0.4",
+                "UnInfected_Tumour_Cells_Xu": "713931.0"
+              },
+              {
+                "time": "0.5",
+                "UnInfected_Tumour_Cells_Xu": "774275.0"
+              },
+              {
+                "time": "0.6",
+                "UnInfected_Tumour_Cells_Xu": "836486.0"
+              }
+            ]`
           )
         )
       ).toEqual(
         JSON.parse(
-          `{"UnInfected_Tumour_Cells_Xu":{"x":["0.0","0.1","0.2","0.3","0.4","0.5","0.6"],"y":["500000.0","548268.0","600386.0","655801.0","713931.0","774275.0","836486.0"],"type":"scatter"}}`
+          `{
+            "UnInfected_Tumour_Cells_Xu": {
+              "x": [
+                "0.0",
+                "0.1",
+                "0.2",
+                "0.3",
+                "0.4",
+                "0.5",
+                "0.6"
+              ],
+              "y": [
+                "500000.0",
+                "548268.0",
+                "600386.0",
+                "655801.0",
+                "713931.0",
+                "774275.0",
+                "836486.0"
+              ],
+              "type": "scatter"
+            }
+          }`
         )
       );
     });
