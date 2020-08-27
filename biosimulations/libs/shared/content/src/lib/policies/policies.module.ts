57a2bfb53aca604cccf67285b4270837dcc68906
Jonathan Karr <jonrkarr@gmail.com>
2020-08-27 09:59:18 -0400

    renaming privacy policy to make room for app-specific policies

--- biosimulations/libs/shared/content/src/lib/policies/policies.module.ts
+++ biosimulations/libs/shared/content/src/lib/policies/policies.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedUiModule } from '@biosimulations/shared/ui';
 import { BiosimulationsIconsModule } from '@biosimulations/shared/icons';
-import { PrivacyPolicyComponent } from './privacy-policy.component';
+import { PlatformPrivacyPolicyComponent } from './platform-privacy-policy.component';
+// import { SimulatorsPrivacyPolicyComponent } from './simulators-privacy-policy.component';
 @NgModule({
   imports: [CommonModule, SharedUiModule, BiosimulationsIconsModule],
   exports: [
-    PrivacyPolicyComponent,
+    PlatformPrivacyPolicyComponent,
+    // SimulatorsPrivacyPolicyComponent,
   ],
   declarations: [
-    PrivacyPolicyComponent,
+    PlatformPrivacyPolicyComponent,
+    // SimulatorsPrivacyPolicyComponent,
   ],
 })
 export class PoliciesModule { }
