22fae878f858fc7f7791df6328c9947bdbb2fc7c
GMarupilla <marupilla@mail.com>
2020-09-04 13:27:08 -0400

    Add tests for simulationIdMap controller

--- biosimulations/apps/dispatch-api/src/app/SimulationIdMap/controller/simulation-id-map.controller.spec.ts
+++ biosimulations/apps/dispatch-api/src/app/SimulationIdMap/controller/simulation-id-map.controller.spec.ts
@@ -3,10 +3,17 @@ import { SimulationIdMapController } from './simulation-id-map.controller';
 
 describe('SimulationIdMapController', () => {
   let app: TestingModule;
+  let controller: SimulationIdMapController;
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
       controllers: [SimulationIdMapController],
     }).compile();
+
+    controller = app.get<SimulationIdMapController>(SimulationIdMapController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
   });
 });
