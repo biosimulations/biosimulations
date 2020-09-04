14fbd108163b625c18ee9b1b856b9c158ebd3e89
GMarupilla <marupilla@mail.com>
2020-09-04 13:29:09 -0400

    Fix test for simulationIdMap schema

--- biosimulations/apps/dispatch-api/src/app/SimulationIdMap/schemas/simulation-id-map.schema.spec.ts
+++ biosimulations/apps/dispatch-api/src/app/SimulationIdMap/schemas/simulation-id-map.schema.spec.ts
@@ -2,6 +2,8 @@ import { SimulationIdMap } from './simulation-id-map.schema';
 
 describe('SimulationIdMap', () => {
   it('should create an instance', () => {
-    expect(new SimulationIdMap()).toBeTruthy();
+    expect(
+      new SimulationIdMap('9dnwjxns93-wsnxjswjx92jnz-98nxz', 'Vilar')
+    ).toBeTruthy();
   });
 });
