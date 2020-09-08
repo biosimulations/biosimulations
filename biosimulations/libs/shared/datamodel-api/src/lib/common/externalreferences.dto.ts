a6e954849a743dcd46a8d4f02b10f88eb72e0551
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-09-08 13:53:34 -0400

    add shared api datamodels

---
+++ biosimulations/libs/shared/datamodel-api/src/lib/common/externalreferences.dto.ts
@@ -0,0 +1,13 @@
+import { ExternalReferences as IExternalReferences } from '@biosimulations/shared/datamodel';
+import { Identifier } from './ontology.dto';
+import { JournalReference } from './journalreference';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ExternalReferences implements IExternalReferences {
+  @ApiProperty({ type: [Identifier] })
+  identifiers!: Identifier[];
+  @ApiProperty({ type: [JournalReference] })
+  citations!: JournalReference[];
+  @ApiProperty({ type: String, nullable: true })
+  doi!: string | null;
+}
---
+++ biosimulations/libs/shared/datamodel-api/src/lib/common/ontology.dto.ts
@@ -0,0 +1,28 @@
+import {
+  OntologyTerm as IOntologyTerm,
+  Ontologies,
+  Identifier as IIdentifier,
+} from '@biosimulations/shared/datamodel';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class OntologyTerm implements OntologyTerm {
+  @ApiProperty({ enum: Ontologies })
+  ontology!: string;
+  @ApiProperty()
+  id!: string;
+  @ApiProperty()
+  name!: string;
+  @ApiProperty({ type: String, nullable: true })
+  description!: string | null;
+  @ApiProperty({ type: String, nullable: true })
+  iri!: string | null;
+}
+
+export class Identifier implements IIdentifier {
+  @ApiProperty()
+  namespace!: string;
+  @ApiProperty()
+  id!: string;
+  @ApiProperty({ type: String, nullable: true })
+  url!: string | null;
+}
