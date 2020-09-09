1d7ce2fbe87736008a93597c18b39ad31d9bd955
Jonathan Karr <jonrkarr@gmail.com>
2020-09-09 19:32:48 -0400

    styling simulator view

--- biosimulations/libs/shared/ui/src/lib/text-page/text-page.component.ts
+++ biosimulations/libs/shared/ui/src/lib/text-page/text-page.component.ts
@@ -1,8 +1,8 @@
 import {
   Component,
-  OnInit,
   Input,
   ViewChild,
+  ElementRef,
 } from '@angular/core';
 
 interface TocItem {
@@ -15,30 +15,62 @@ interface TocItem {
   templateUrl: './text-page.component.html',
   styleUrls: ['./text-page.component.scss'],
 })
-export class TextPageComponent implements OnInit {
+export class TextPageComponent {
   @Input()
   heading = '';
+
+  @Input()
+  contentsHeading = 'Contents';
+
+  @Input()
+  padded = true;
+
+  @Input()
+  alwaysFixed: string | null = null;
 
   fixed = false;
 
   tocSections: TocItem[] = [];
+  tocSectionObservers: MutationObserver[] = [];
 
-  /* TODO (low priority): switch to monitoring ContentChildren so that the TOC is dynamically updated with the section or their headings change.
-       low priority because none of the instances of TextPageComponent currently need this.
-  */
-  @ViewChild('sectionsContainer')
-  set sectionsContainer(container: any) {
+  @ViewChild('sectionsContainer', {read: ElementRef})
+  set sectionsContainer(container: ElementRef) {
+    while(this.tocSections.length) {
+      this.tocSections.pop();
+    }
+    while(this.tocSectionObservers.length) {
+      const observer = this.tocSectionObservers.pop();
+      if (observer !== undefined) {
+        observer.disconnect();
+      }
+    }
     this.getTocSections(container.nativeElement);
   }
 
   getTocSections(container: any) {
     for (const section of container.children) {
-      const heading = section.getAttribute('shortHeading') || section.getAttribute('heading');
-      if (heading) {
-        this.tocSections.push({
+      if (section.localName === 'biosimulations-text-page-content-section') {
+        const heading = section.getAttribute('shortHeading') || section.getAttribute('ng-reflect-shortHeading') || section.getAttribute('heading') || section.getAttribute('ng-reflect-heading');
+        const tocSection = {
           heading: heading,
           target: section,
+        };
+        this.tocSections.push(tocSection);
+
+        const observer = new MutationObserver((mutations) => {
+          mutations.forEach((mutation) => {
+            const heading = section.getAttribute('shortHeading')
+              || section.getAttribute('ng-reflect-shortHeading')
+              || section.getAttribute('heading')
+              || section.getAttribute('ng-reflect-heading');
+            tocSection.heading = heading;
+          });
         });
+
+        observer.observe(section, {
+          attributeFilter: ['shortHeading', 'ng-reflect-shortHeading', 'heading', 'ng-reflect-heading'],
+        });
+        this.tocSectionObservers.push(observer);
       } else {
         this.getTocSections(section);
       }
@@ -49,9 +81,13 @@ export class TextPageComponent implements OnInit {
     window.addEventListener('scroll', this.scroll, true);
   }
 
-  ngOnInit(): void {}
-
   ngOnDestroy() {
+    while(this.tocSectionObservers.length) {
+      const observer = this.tocSectionObservers.pop();
+      if (observer !== undefined) {
+        observer.disconnect();
+      }
+    }
     window.removeEventListener('scroll', this.scroll, true);
   }
 
