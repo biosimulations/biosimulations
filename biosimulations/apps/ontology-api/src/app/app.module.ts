08ca898694118a4bc529e12ea1d213c51d1102ea
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-09-28 15:01:42 -0400

    add ontology module to api

--- biosimulations/apps/ontology-api/src/app/app.module.ts
+++ biosimulations/apps/ontology-api/src/app/app.module.ts
@@ -1,11 +1,12 @@
+import { Ontologies } from '@biosimulations/shared/datamodel';
 import { Module } from '@nestjs/common';
-
+import { OntologiesModule } from '@biosimulations/ontology/ontologies'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [OntologiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
