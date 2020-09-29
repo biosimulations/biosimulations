0425be64e0716fc87c31a74f5661c2fe439b8d49
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-09-29 10:17:30 -0400

    add missing icon

--- biosimulations/libs/shared/icons/src/lib/shared-icons.module.ts
+++ biosimulations/libs/shared/icons/src/lib/shared-icons.module.ts
@@ -63,7 +63,7 @@ import {
 
 } from '@fortawesome/free-solid-svg-icons';
 import { fab, faGitAlt, faGithub, faDocker, faLinkedin, faOrcid, faCreativeCommons, faCreativeCommonsBy, faCreativeCommonsNc, faCreativeCommonsZero, faCreativeCommonsSa, faCreativeCommonsShare } from '@fortawesome/free-brands-svg-icons';
-import { far, faFile, } from '@fortawesome/free-regular-svg-icons';
+import { far, faFile, faFileAlt as farFileAlt } from '@fortawesome/free-regular-svg-icons';
 import { MatIconComponent } from './mat-icon/mat-icon.component';
 import { CCIconComponent } from './cc-icon/cc-icon.component';
 export { biosimulationsIcon } from './icon/icon.component'
@@ -143,6 +143,7 @@ export class BiosimulationsIconsModule {
       faCreativeCommonsZero,
       faCreativeCommonsSa,
       faCreativeCommonsShare,
+      farFileAlt
     )
 
   }
