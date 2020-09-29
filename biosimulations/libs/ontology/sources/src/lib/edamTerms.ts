59a7220704691422301a69971c5f44a61cfc5622
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-09-28 20:58:49 -0400

    parse ontologies

---
+++ biosimulations/libs/ontology/sources/src/lib/edamTerms.ts
@@ -0,0 +1,42 @@
+import { Ontologies, EDAMTerm } from '@biosimulations/shared/datamodel'
+import edamJson from './edam.json'
+function getEdamTerms(input: any): { [id: string]: EDAMTerm } {
+    let edamTerms: { [id: string]: EDAMTerm } = {};
+
+    // Drop context
+    let edamJsonParse = input["@graph"]
+
+    edamJsonParse.forEach(
+        (jsonTerm: any) => {
+
+            if (jsonTerm["@id"].startsWith("http://edamontology.org/") && !(jsonTerm?.["owl:deprecated"])) {
+
+                const termIRI = jsonTerm["@id"];
+                const termNameSpace = Ontologies.EDAM
+                const termId = jsonTerm["@id"].replace("http://edamontology.org/", "")
+                const termDescription = jsonTerm["http://www.geneontology.org/formats/oboInOwl#hasDefinition"]
+                const termName = jsonTerm["rdf:label"]
+                const termUrl = jsonTerm?.["http://edamontology.org/documentation"]?.["@id"] || jsonTerm?.["http://www.geneontology.org/formats/oboInOwl#hasDbXref"]?.["@id"] || encodeURI("http://bioportal.bioontology.org/ontologies/EDAM/?p=classes&conceptid=" + termIRI)
+                let term: EDAMTerm = {
+                    id: termId,
+                    name: termName,
+                    description: termDescription,
+                    namespace: termNameSpace,
+                    iri: termIRI,
+                    url: termUrl
+                }
+
+
+                edamTerms[termId] = term
+            }
+            else {
+                return;
+            }
+
+
+        }
+    );
+    return edamTerms;
+
+}
+export const edamTerms = getEdamTerms(edamJson);
