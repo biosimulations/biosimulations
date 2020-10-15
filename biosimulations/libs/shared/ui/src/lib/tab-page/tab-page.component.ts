bc21f565f60ad0d47b959eaf6c899d67da96692b
Jonathan Karr <jonrkarr@gmail.com>
2020-10-15 00:20:40 -0400

    switching to first tab on route changes -- closes #1234

--- biosimulations/libs/shared/ui/src/lib/tab-page/tab-page.component.ts
+++ biosimulations/libs/shared/ui/src/lib/tab-page/tab-page.component.ts
@@ -1,22 +1,32 @@
-import { Component, AfterViewInit, AfterViewChecked, Input, Output, EventEmitter, ViewChild, ContentChildren, QueryList } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterViewChecked, Input, Output, EventEmitter, ViewChild, ContentChildren, QueryList } from '@angular/core';
 import { MatTabGroup, MatTab } from '@angular/material/tabs';
 import { TabPageTabComponent } from './tab-page-tab.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'biosimulations-tab-page',
   templateUrl: './tab-page.component.html',
   styleUrls: ['./tab-page.component.scss'],
 })
-export class TabPageComponent implements AfterViewInit {
+export class TabPageComponent implements OnInit, AfterViewInit, AfterViewChecked {
   @Input()
   loading = false;
 
   @Output() selectedTabChange: EventEmitter<any> = new EventEmitter();
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
   @ViewChild(MatTabGroup) matTabGroup!: MatTabGroup;
   @ContentChildren(TabPageTabComponent, {descendants: true}) tabs!: QueryList<TabPageTabComponent>;
+
+  selectedTabIndex = 0;
+
+  ngOnInit(): void {
+    this.route.params.subscribe(() => {
+      this.selectedTabIndex = 0;
+      console.log('here')
+    });
+  }
 
   ngAfterViewInit(): void {
     const baseTabs: MatTab[] = [];
