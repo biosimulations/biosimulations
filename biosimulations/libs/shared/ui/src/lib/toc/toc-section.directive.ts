69ba4e0a720be0f0d5fd934bf73bee2f72488dc8
Jonathan Karr <jonrkarr@gmail.com>
2020-10-20 14:07:26 -0400

    removing trailing white space

--- biosimulations/libs/shared/ui/src/lib/toc/toc-section.directive.ts
+++ biosimulations/libs/shared/ui/src/lib/toc/toc-section.directive.ts
@@ -20,7 +20,7 @@ export class TocSectionDirective {
   constructor(
     @Host() private sectionsContainer: TocSectionsContainerDirective,
     elementRef: ElementRef
-  ) {    
+  ) {
     this.section = {
       heading: this.heading.asObservable(),
       target: elementRef.nativeElement,
