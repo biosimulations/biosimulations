5e2ce66ffb8e6113bc71f39ea2cc2bc98272b623
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-10-20 14:32:33 -0400

    add error handler, and exception type

--- biosimulations/libs/shared/exceptions/src/lib/biosimulations-exception.filter.ts
+++ biosimulations/libs/shared/exceptions/src/lib/biosimulations-exception.filter.ts
@@ -7,32 +7,40 @@ import {
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { ErrorObject } from '@biosimulations/shared/datamodel-api';
+import {
+  BiosimulationsException,
+  isBiosimulationsException,
+} from './exception';
+import { stat } from 'fs';
 @Catch()
 export class BiosimulationsExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(
+    exception: HttpException | BiosimulationsException | any,
+    host: ArgumentsHost
+  ) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
-    const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
-    let errors: ErrorObject[] = [];
-    response.status(status).json({
-      errors: [
-        {
-          status: status,
-          code: '',
-          title: '',
-          detail: '',
-          source: '',
-          meta: {
-            timestamp: new Date().toISOString(),
-            path: request.url,
-          },
-        },
-      ],
-    });
+    let status: number;
+    let resbody: ErrorObject;
+    if (isBiosimulationsException(exception)) {
+      status = exception.getStatus();
+      resbody = exception.getError();
+    } else if (exception instanceof HttpException) {
+      exception = BiosimulationsException.fromHTTP(exception);
+      status = exception.getStatus();
+      resbody = exception.getError();
+      response.status(exception.getStatus()).json(exception.getError());
+    } else {
+      status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+      resbody = {
+        status: HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+        title: 'Internal Server Error',
+        detail: exception,
+      };
+    }
+    response.status(status).json(resbody);
   }
 }
---
+++ biosimulations/libs/shared/exceptions/src/lib/exception.ts
@@ -0,0 +1,87 @@
+import {
+  AboutLinksObject,
+  ErrorObject,
+  ErrorSourceObject,
+} from '@biosimulations/shared/datamodel-api';
+import { HttpException } from '@nestjs/common';
+import { strict, throws } from 'assert';
+
+export class BiosimulationsException extends Error {
+  private errorObject: ErrorObject;
+  constructor(
+    private status: number,
+    private title: string,
+    private detail?: string,
+    private code?: string,
+    private link?: string,
+    private sourcePointer?: string,
+    private sourceParameter?: string,
+    private meta?: { [key: string]: any }
+  ) {
+    super(title);
+    this.name = 'BiosimulationsError';
+    this.initFields();
+    this.errorObject = this.createError();
+  }
+
+  initFields() {}
+  createError(): ErrorObject {
+    const error: ErrorObject = {
+      status: this.status.toString(),
+      title: this.title,
+    };
+    if (this.detail) {
+      error.detail = this.detail;
+    }
+    if (this.code) {
+      error.code = this.code;
+    }
+    if (this.link) {
+      let linkObject: AboutLinksObject = {
+        about: this.link,
+      };
+      error.links = linkObject;
+    }
+    if (this.sourceParameter || this.sourcePointer) {
+      let source: ErrorSourceObject = {};
+      if (this.sourcePointer) {
+        source.pointer = this.sourcePointer;
+      }
+      if (this.sourceParameter) {
+        source.parameter = this.sourceParameter;
+      }
+      error.source = source;
+    }
+    if (this.meta) {
+      error.meta = { meta: this.meta };
+    }
+    return error;
+  }
+  getStatus(): number {
+    return this.status;
+  }
+  getError(): ErrorObject {
+    return this.errorObject;
+  }
+
+  // TODO write test
+  static fromHTTP(exception: HttpException): BiosimulationsException {
+    let response = exception.getResponse();
+    if (typeof response === 'string') {
+      return new BiosimulationsException(exception.getStatus(), response);
+    } else {
+      return new BiosimulationsException(
+        exception.getStatus(),
+        exception.message
+      );
+    }
+  }
+}
+export function isBiosimulationsException(
+  exception: BiosimulationsException | HttpException
+): exception is BiosimulationsException {
+  return (
+    (exception as BiosimulationsException).getError !== undefined &&
+    (exception as BiosimulationsException).getStatus !== undefined
+  );
+}
