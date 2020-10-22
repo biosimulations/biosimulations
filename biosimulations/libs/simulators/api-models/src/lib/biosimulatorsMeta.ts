85261ceacfe732e52ff91e5568328a1f94ea574d
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-10-22 11:13:03 -0400

    add some documentation to meta object

--- biosimulations/libs/simulators/api-models/src/lib/biosimulatorsMeta.ts
+++ biosimulations/libs/simulators/api-models/src/lib/biosimulatorsMeta.ts
@@ -1,12 +1,26 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class BiosimulatorsMeta {
-  @ApiProperty()
+  @ApiProperty({
+    default: '1.0.0',
+    description:
+      ' The version of the API schema that the properties of the simulators conforms to',
+    examples: ['1.0.0', '1.2.3'],
+    enum: ['1.0.0'],
+  })
   schemaVersion!: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    default: '1.0.0',
+    description:
+      ' The version of the docker image interface and features supported by the image',
+    examples: ['1.0.0', '1.2.3'],
+    enum: ['1.0.0'],
+  })
   imageVersion!: string;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description: 'True if the simulator is supported by the BioSimulators team',
+  })
   internal?: boolean;
 }
