4440e264b649b91af1a3bf83ed143388758f6c21
Jonathan Karr <jonrkarr@gmail.com>
2020-10-30 11:13:17 -0400

    debugging test

--- biosimulations/apps/simulators/src/app/help/help/help.component.spec.ts
+++ biosimulations/apps/simulators/src/app/help/help/help.component.spec.ts
@@ -7,6 +7,7 @@ import { SharedUiModule } from '@biosimulations/shared/ui';
 import { BiosimulationsIconsModule } from '@biosimulations/shared/icons';
 import { SharedContentModule } from '@biosimulations/shared/content';
 import { ConfigService, ScrollService } from '@biosimulations/shared/services';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 describe('HelpComponent', () => {
   let component: HelpComponent;
@@ -14,8 +15,18 @@ describe('HelpComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, SharedUiModule, BiosimulationsIconsModule, SharedContentModule],
-      providers: [RouterTestingModule, ConfigService, ScrollService],
+      imports: [RouterTestingModule, SharedUiModule, BiosimulationsIconsModule, SharedContentModule, HighlightModule],
+      providers: [
+        RouterTestingModule,
+        ConfigService,
+        ScrollService,
+        {
+          provide: HIGHLIGHT_OPTIONS,
+          useValue: {
+            fullLibraryLoader: () => import('highlight.js'),
+          }
+        }
+      ],
       declarations: [HelpComponent],
     }).compileComponents();
   }));
