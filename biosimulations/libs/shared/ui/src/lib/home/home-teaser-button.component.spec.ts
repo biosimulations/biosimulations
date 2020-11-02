7547f18a1db108815ee615bcc42b8bc1b35d5f20
Jonathan Karr <jonrkarr@gmail.com>
2020-11-02 15:08:10 -0500

    debugging tests

--- biosimulations/libs/shared/ui/src/lib/home/home-teaser-button.component.spec.ts
+++ biosimulations/libs/shared/ui/src/lib/home/home-teaser-button.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HomeTeaserButtonComponent } from './home-teaser-button.component';
 import { MatButtonModule } from '@angular/material/button';
@@ -10,10 +11,12 @@ describe('HomeTeaserButtonComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        MatButtonModule,
         HomeTeaserButtonComponent,
       ],
-      imports: [],
+      imports: [
+        RouterTestingModule,
+        MatButtonModule,
+      ],
     }).compileComponents();
   }));
 
--- biosimulations/libs/shared/ui/src/lib/home/home-teaser.component.spec.ts
+++ biosimulations/libs/shared/ui/src/lib/home/home-teaser.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HomeTeaserComponent } from './home-teaser.component';
 import { HomeTeaserButtonComponent } from './home-teaser-button.component';
@@ -11,11 +12,13 @@ describe('HomeTeaserComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        MatButtonModule,
         HomeTeaserComponent,
         HomeTeaserButtonComponent,
       ],
-      imports: [],
+      imports: [
+        RouterTestingModule,
+        MatButtonModule,
+      ],
     }).compileComponents();
   }));
 
