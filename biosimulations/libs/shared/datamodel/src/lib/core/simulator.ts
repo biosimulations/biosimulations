2c070ed73322b12412fdd1b3dd5e9c2ea5f86afa
Jonathan Karr <jonrkarr@gmail.com>
2020-11-03 16:17:09 -0500

    debugging

--- biosimulations/libs/shared/datamodel/src/lib/core/simulator.ts
+++ biosimulations/libs/shared/datamodel/src/lib/core/simulator.ts
@@ -1,8 +1,8 @@
 import { IAlgorithm } from './algorithm';
 import { SecondaryResourceMetaData } from '../resources';
 
-export simulatorSpecificationsVersions = ['1.0.0'];
-export SimulatorImageVersions = ['1.0.0'];
+export const simulatorSpecificationsVersions: string[] = ['1.0.0'];
+export const SimulatorImageVersions: string[] = ['1.0.0'];
 
 export interface SimulatorAttributes {
   name: string;
