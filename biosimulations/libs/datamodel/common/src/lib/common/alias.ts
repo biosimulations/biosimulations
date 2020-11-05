dd5524256a178046d9ce928095b8b69161328ecd
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-11-05 11:53:49 -0500

    clean up datamodel

--- biosimulations/libs/datamodel/common/src/lib/common/alias.ts
+++ biosimulations/libs/datamodel/common/src/lib/common/alias.ts
@@ -1,13 +1,5 @@
 // Mostly for documentation purposes. Also allows for all common references to be in one place incase they need more detail later
 
-// String that represent an id in some ontonlogy/resource
-export type KisaoId = string;
-export type EdamId = string;
-export type SBOId = string;
-export type DOI = string;
-
-// Todo perhaps use class validator or see if type can be limited using regex
-
 // Strings with some sort of constraint
 
 // Seconds from epoch
