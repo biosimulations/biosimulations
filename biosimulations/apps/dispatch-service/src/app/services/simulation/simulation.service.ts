99c03973dd181a54b931593cd4571f5ece4bb952
GMarupilla <marupilla@mail.com>
2020-11-07 13:44:58 -0500

    Resizing plotly chart, working on job cancelling..

--- biosimulations/apps/dispatch-service/src/app/services/simulation/simulation.service.ts
+++ biosimulations/apps/dispatch-service/src/app/services/simulation/simulation.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { HpcService } from '../hpc/hpc.service';
+import { MQDispatch } from '@biosimulations/messages';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Injectable()
 export class SimulationService {
@@ -11,4 +13,12 @@ export class SimulationService {
     const jobStatus = await this.hpcService.saactJobStatus(jobId);
     return jobStatus;
   }
+<<<<<<< HEAD
+=======
+
+  @MessagePattern(MQDispatch.SIM_HPC_CANCEL)
+  async cancelRunningSimulation(jobId: string) {
+    return await this.hpcService.scancelJob(jobId);
+  }
+>>>>>>> 5caf5092 (Resizing plotly chart, working on job cancelling..)
 }
--- biosimulations/libs/messages/messages/src/lib/messages.ts
+++ biosimulations/libs/messages/messages/src/lib/messages.ts
@@ -3,4 +3,5 @@ export enum MQDispatch {
   SIM_HPC_FINISH = 'sim_hpc_finish',
   SIM_DISPATCH_FINISH = 'sim_dispatch_finish',
   SIM_RESULT_FINISH = 'sim_result_finish',
+  SIM_HPC_CANCEL = 'sim_hpc_cancel',
 }
