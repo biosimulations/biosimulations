e4c71b6daca3be004d0bc793e096fc873b5d22d1
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-11-10 12:37:15 -0500

    begin skeleton for mail service

--- biosimulations/apps/mail-service/src/app/app.controller.ts
+++ biosimulations/apps/mail-service/src/app/app.controller.ts
@@ -1,13 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import {
+  ClientProxy,
+  Ctx,
+  EventPattern,
+  JsonSocket,
+  MessagePattern,
+  NatsContext,
+  Payload,
+} from '@nestjs/microservices';
 
 import { AppService } from './app.service';
-
+class dataInput {
+  hello!: string;
+}
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('Nats_Client') private client: ClientProxy
+  ) {}
 
-  @Get()
-  getData() {
+  @EventPattern('test.ping')
+  async getData(@Payload() payload: any, @Ctx() context: NatsContext) {
+    console.log('Staring Routine 1. Sending Message');
+    this.client
+      .send('test.echo', { hello: 'world' })
+      .subscribe((reply) => console.log('Got reply'));
+
     return this.appService.getData();
+  }
+
+  @MessagePattern('test.echo')
+  echoData(@Payload() data: dataInput, @Ctx() context: NatsContext) {
+    console.log('Starting Routine 2');
+
+    return data;
+  }
+  @MessagePattern('test.context')
+  echoContext(@Payload() data: any, @Ctx() context: NatsContext) {
+    return context;
+  }
+
+  @EventPattern('>')
+  log(@Payload() data: dataInput, @Ctx() context: NatsContext) {
+    console.log(data);
+    console.log(context);
   }
 }
--- biosimulations/apps/mail-service/src/app/app.module.ts
+++ biosimulations/apps/mail-service/src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'Nats_Client',
+        transport: Transport.NATS,
+        options: {
+          url: 'nats://localhost:4222',
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
