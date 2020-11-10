33d74452855b2469220759cce1e534f0cc4ac064
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-11-10 18:31:24 -0500

    implement get and post

--- biosimulations/apps/dispatch-api/src/simulation-run/simulation-run.service.ts
+++ biosimulations/apps/dispatch-api/src/simulation-run/simulation-run.service.ts
@@ -1,13 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { SimulationRun } from './simulation-run.dto';
 import { SimulationFile } from './file.model';
-import { Model } from 'mongoose';
-import { SimulationRunModel } from './simulation-run.model';
+import { Model, Mongoose } from 'mongoose';
+import {
+  SimulationRunModel,
+  SimulationRunModelSchema,
+} from './simulation-run.model';
+const toApi = (obj: any) => {
+  delete obj.__v;
+  delete obj._id;
+  return obj;
+};
 @Injectable()
 export class SimulationRunService {
-  async createRun(run: SimulationRun, file: any) {
-    console.log(this.fileModel.schema);
+  update(id: string, run: SimulationRun) {
+    throw new Error('Method not implemented.');
+  }
+  async getAll() {
+    return (await this.simulationRunModel.find().lean().exec()).map(toApi);
+  }
+  get(id: string) {
+    throw new Error('Method not implemented.');
+  }
+  async createRun(run: SimulationRun, file: any): Promise<SimulationRunModel> {
     const fileParsed = {
       originalname: file.originalname,
       encoding: file.encoding,
@@ -15,11 +31,26 @@ export class SimulationRunService {
       buffer: file.buffer,
       size: file.size,
     };
-    const newFile = new this.fileModel(fileParsed);
 
-    const res = await newFile.save();
-    console.log(res);
-    return res;
+    let newFile = new this.fileModel(fileParsed);
+    const newSimulationRun = new this.simulationRunModel(run);
+    newSimulationRun.id = newSimulationRun._id;
+    newSimulationRun.file = newFile;
+    newSimulationRun.depopulate('file');
+
+    const filePromise = newFile.save();
+    const modelPromise = newSimulationRun.save();
+
+    // TODO determine if there is a better way to do this. Need to ensure that file is saved properly, if not delete the model entry
+    // Unclear if I should use a promise.all here. Since promise is created before await, this should be efficient either way.
+
+    filePromise.catch((reason) => {
+      newSimulationRun.remove();
+    });
+
+    const modelRes = await modelPromise;
+    const fileRes = await filePromise;
+    return modelRes;
   }
   constructor(
     @InjectModel(SimulationFile.name) private fileModel: Model<SimulationFile>,
