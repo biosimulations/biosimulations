acaf837bf4718d5d7bd6e4aa136edaeef0a1ab74
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-11-10 18:31:40 -0500

    improve schema definitions

--- biosimulations/apps/dispatch-api/src/simulation-run/simulation-run.model.ts
+++ biosimulations/apps/dispatch-api/src/simulation-run/simulation-run.model.ts
@@ -1,25 +1,52 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { Types } from 'mongoose';
+import * as EmailValidator from 'email-validator';
+import { SimulationFile, SimulationFileSchema } from './file.model';
 
+// TODO move this to common utils
+const omitPrivate = (doc: any, obj: any) => {
+  delete obj.__v;
+  delete obj._id;
+
+  return obj;
+};
+
+// TODO combine with already defined enum
 export enum SimulationRunStatus {
+  // The api has created the entry
+  CREATED = 'CREATED',
+  // The api has submitted the run and service has accepted
   QUEUED = 'QUEUED',
+  // The service has starting the run
   RUNNING = 'RUNNING',
+  // The run has finished
   SUCCEEDED = 'SUCCEEDED',
+  // The run has failed
   FAILED = 'FAILED',
 }
 
-@Schema({ collection: 'Simulation Runs' })
+@Schema({ collection: 'Simulation Runs', id: false })
 export class SimulationRunModel extends Document {
-  @Prop({ index: true })
+  @Prop({ required: true, unique: true, index: true })
   id!: string;
 
-  @Prop()
+  @Prop({ type: Types.ObjectId, ref: SimulationFile.name })
+  file!: SimulationFile;
+  @Prop({ type: String, required: true })
   name!: string;
 
-  @Prop()
+  @Prop({ type: String, required: false, validate: EmailValidator.validate })
   email!: string;
 
-  @Prop()
+  @Prop({
+    type: String,
+    enum: Object.keys(SimulationRunStatus).map(
+      (key) => SimulationRunStatus[key as SimulationRunStatus]
+    ),
+
+    default: SimulationRunStatus.CREATED,
+  })
   status!: SimulationRunStatus;
 
   @Prop()
@@ -31,11 +58,11 @@ export class SimulationRunModel extends Document {
   @Prop()
   resultsSize!: number;
 
-  @Prop()
+  @Prop({ type: String, required: true })
   simulator!: string;
 
-  @Prop()
-  simulationVersion!: string;
+  @Prop({ type: String, required: true })
+  simulatorVersion!: string;
 
   @Prop()
   submitted!: Date;
@@ -54,3 +81,5 @@ SimulationRunModelSchema.set('timestamps', {
   createdAt: 'submitted',
   updatedAt: 'updated',
 });
+SimulationRunModelSchema.set('toObject', { transform: omitPrivate });
+SimulationRunModelSchema.set('toJSON', { transform: omitPrivate });
