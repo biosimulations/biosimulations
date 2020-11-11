b60931a40940911a040c6f48891b19b7a72222b0
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-11-11 12:03:35 -0500

    add controller skeleton

--- biosimulations/apps/dispatch-api/src/results/results.controller.ts
+++ biosimulations/apps/dispatch-api/src/results/results.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { ResultsService } from './results.service';
 
 @Controller('results')
-export class ResultsController {}
+@ApiTags('Results')
+export class ResultsController {
+  constructor(private service: ResultsService) {}
+  @Get()
+  getResults() {
+    return this.service.getResults();
+  }
+
+  @Get(':id')
+  getResult(@Param('') id: string) {
+    return this.service.getResult(id);
+  }
+
+  @Get(':id/download')
+  downloadResult(@Param('id') id: string) {
+    return this.service.download(id);
+  }
+
+  @Post()
+  addResult(@Body() results: any) {
+    return this.service.addResults(results);
+  }
+  @Put(':id')
+  editResults(
+    @Param('id')
+    id: string,
+    @Body() results: any
+  ) {
+    return this.service.editResults(id, results);
+  }
+
+  @Delete()
+  deleteResults() {
+    return this.service.deleteAll();
+  }
+  @Delete(':id')
+  deleteResult(@Param('id') id: string) {
+    return this.service.delete(id);
+  }
+}
--- biosimulations/apps/dispatch-api/src/results/results.service.ts
+++ biosimulations/apps/dispatch-api/src/results/results.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ResultsService {}
+export class ResultsService {
+  getResults() {
+    throw new Error('Method not implemented.');
+  }
+  getResult(id: string) {
+    throw new Error('Method not implemented.');
+  }
+  download(id: string) {
+    throw new Error('Method not implemented.');
+  }
+  addResults(results: any) {
+    throw new Error('Method not implemented.');
+  }
+  editResults(id: string, results: any) {
+    throw new Error('Method not implemented.');
+  }
+  deleteAll() {
+    throw new Error('Method not implemented.');
+  }
+  delete(id: string) {
+    throw new Error('Method not implemented.');
+  }
+}
