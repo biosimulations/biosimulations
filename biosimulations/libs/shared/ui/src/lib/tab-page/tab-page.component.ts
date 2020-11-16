d36954eb33d26149fc403eca283d2a8c162e6109
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-11-16 14:58:34 -0500

    fix broken tab-page component

--- biosimulations/libs/shared/ui/src/lib/tab-page/tab-page.component.ts
+++ biosimulations/libs/shared/ui/src/lib/tab-page/tab-page.component.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit, AfterViewInit, AfterViewChecked, Input, Output, EventEmitter, ViewChild, ContentChildren, QueryList } from '@angular/core';
-import { MatTabGroup, MatTab } from '@angular/material/tabs';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  AfterViewChecked,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ContentChildren,
+  QueryList,
+} from '@angular/core';
+import { MatTab, MatTabGroup, MAT_TAB_GROUP } from '@angular/material/tabs';
 import { TabPageTabComponent } from './tab-page-tab.component';
 import { ActivatedRoute } from '@angular/router';
 
@@ -7,8 +18,20 @@ import { ActivatedRoute } from '@angular/router';
   selector: 'biosimulations-tab-page',
   templateUrl: './tab-page.component.html',
   styleUrls: ['./tab-page.component.scss'],
+  /*
+   *Not sure why this is needed.
+   * The page-tab-tab component has <mat-tab> elements that fail to find the mat tab group to inject.
+   * Providing the injection token here somehow fixes the problem. The mat tab group component used here should be the same one that is being provided
+   */
+  providers: [
+    {
+      provide: MAT_TAB_GROUP,
+      useClass: MatTabGroup,
+    },
+  ],
 })
-export class TabPageComponent implements OnInit, AfterViewInit, AfterViewChecked {
+export class TabPageComponent
+  implements OnInit, AfterViewInit, AfterViewChecked {
   @Input()
   loading = false;
 
@@ -17,7 +40,9 @@ export class TabPageComponent implements OnInit, AfterViewInit, AfterViewChecked
   constructor(private route: ActivatedRoute) {}
 
   @ViewChild(MatTabGroup) matTabGroup!: MatTabGroup;
-  @ContentChildren(TabPageTabComponent, {descendants: true}) tabs!: QueryList<TabPageTabComponent>;
+  @ContentChildren(TabPageTabComponent, { descendants: true }) tabs!: QueryList<
+    TabPageTabComponent
+  >;
 
   selectedTabIndex = 0;
 
