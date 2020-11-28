8e7494bb4e2d90eced8faead25ad477e7d6e7f3d
Jonathan Karr <jonrkarr@gmail.com>
2020-11-27 23:42:29 -0500

    debugging lint, test, build workflow; pretty-printing, removing empty functions to trigger affected apps

--- biosimulations/libs/shared/ui/src/lib/spinner/spinner.component.ts
+++ biosimulations/libs/shared/ui/src/lib/spinner/spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'biosimulations-spinner',
@@ -6,11 +6,5 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./spinner.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SpinnerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SpinnerComponent {
 }
