d8d6989b0da24053032a0a25bbd10f29afa7db96
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-12-10 14:46:34 -0500

    remove old models

--- biosimulations/apps/dispatch-service/src/app/services/archiver/archiver.service.ts
+++ biosimulations/apps/dispatch-service/src/app/services/archiver/archiver.service.ts
@@ -1,4 +1,3 @@
-import { ModelsService } from './../../resources/models/models.service';
 import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@angular/core';
 import { Logger } from '@nestjs/common';
@@ -10,12 +9,11 @@ import { AppService } from '../../app.service';
 @Injectable()
 export class ArchiverService {
   constructor(
-    private modelsService: ModelsService,
     private configService: ConfigService,
-    private appService: AppService) { }
+    private appService: AppService
+  ) {}
   private logger = new Logger('ArchiverService');
-  private fileStorage: string = this.configService.get(
-    'hpc.fileStorage', '');
+  private fileStorage: string = this.configService.get('hpc.fileStorage', '');
 
   async createResultArchive(uuid: string) {
     const resultPath = path.join(this.fileStorage, 'simulations', uuid, 'out');
@@ -25,7 +23,9 @@ export class ArchiverService {
 
     output.on('close', () => {
       const size = archive.pointer().toString();
-      this.appService.updateSimulationInDb(uuid, {resultsSize: parseInt(size)})
+      this.appService.updateSimulationInDb(uuid, {
+        resultsSize: parseInt(size),
+      });
       this.logger.verbose(`The resulting archive holds ${size} bytes in size`);
       this.logger.log(
         'Archiver has been finalized and the output file descriptor has closed.'
