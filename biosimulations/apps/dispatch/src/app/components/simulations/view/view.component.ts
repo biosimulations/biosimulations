a07be21b7e1a6d12eb5291a68985215141110cf5
Jonathan Karr <jonrkarr@gmail.com>
2020-12-12 01:29:15 -0500

    adding spinner to view component; organizing variables into observables

--- biosimulations/apps/dispatch/src/app/components/simulations/view/view.component.ts
+++ biosimulations/apps/dispatch/src/app/components/simulations/view/view.component.ts
@@ -16,6 +16,25 @@ import { DispatchService } from '../../../services/dispatch/dispatch.service';
 import { Simulation } from '../../../datamodel';
 import { urls } from '@biosimulations/config/common';
 import { ConfigService } from '@biosimulations/shared/services';
+import { BehaviorSubject } from 'rxjs';
+
+interface FormattedSimulation {
+  id: string;
+  name: string;
+  simulator: string;
+  simulatorVersion: string;
+  simulatorUrl: string;
+  statusRunning: boolean;
+  statusSucceeded: boolean;
+  statusLabel: string;
+  submitted: string;
+  updated: string;
+  runtime: string;
+  projectUrl: string;
+  projectSize: string;
+  resultsUrl: string;
+  resultsSize: string;
+}
 
 interface VizApiResponse {
   message: string;
@@ -26,36 +45,43 @@ interface CombineArchive {
   [id: string]: string[];
 }
 
+interface Logs {
+  out: string;
+  err: string;
+}
+
 @Component({
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.scss'],
 })
 export class ViewComponent implements OnInit {
-  uuid = '';
-  name = '';
-  simulator = '';
-  simulatorVersion = '';
-  simulatorUrl = '';
-  statusRunning = false;
-  statusSucceeded = false;
-  statusLabel = '';
-  submitted = '';
-  updated = '';
-  runtime = '';
-  projectUrl = '';
-  projectSize = '';
-  resultsUrl = '';
-  resultsSize = '';
+  private uuid = '';
+  
+  private formattedSimulation = new BehaviorSubject<FormattedSimulation | undefined>(undefined);
+  formattedSimulation$ = this.formattedSimulation.asObservable();  
 
-  outLog = '';
-  errLog = '';
+  private statusRunning = new BehaviorSubject<boolean>(true);
+  statusRunning$ = this.statusRunning.asObservable();
+
+  private logs = new BehaviorSubject<Logs>({out: '', err: ''});
+  logs$ = this.logs.asObservable();
 
   formGroup: FormGroup;
-  combineArchive?: CombineArchive;
-  sedmlLocations?: string[];
-  selectedSedmlLocation?: string;
-  reportIds?: string[];
-  selectedReportId?: string;
+  private combineArchive: CombineArchive | undefined;
+  
+  private _sedmlLocations: string[] | undefined;
+  private sedmlLocations = new BehaviorSubject<string[] | undefined>(undefined);
+  sedmlLocations$ = this.sedmlLocations.asObservable()
+  
+  private _reportIds: string[] | undefined;
+  private reportIds = new BehaviorSubject<string[] | undefined>(undefined);
+  reportIds$ = this.reportIds.asObservable();
+
+  private selectedSedmlLocation: string | undefined;  
+  
+  private _selectedReportId: string | undefined;
+  private selectedReportId = new BehaviorSubject<string | undefined>(undefined);
+  selectedReportId$ = this.selectedReportId.asObservable()
 
   @ViewChild('visualization') visualization!: VisualisationComponent;
 
@@ -77,44 +103,57 @@ export class ViewComponent implements OnInit {
     this.uuid = this.route.snapshot.params['uuid'];
 
     this.simulationService.getSimulationByUuid(this.uuid).subscribe((simulation: Simulation): void => {
-      this.name = simulation.name;
-      this.simulator = simulation.simulator;
-      this.simulatorVersion = simulation.simulatorVersion;
-      this.statusRunning = SimulationStatusService.isSimulationStatusRunning(simulation.status);
-      this.statusSucceeded = SimulationStatusService.isSimulationStatusSucceeded(simulation.status);
-      this.statusLabel = SimulationStatusService.getSimulationStatusMessage(simulation.status, true);
-      this.runtime = simulation.runtime !== undefined ? Math.round((simulation.runtime)/1000).toString() + ' s' : 'N/A';
-      this.submitted = new Date(simulation.submitted).toLocaleString();
-      this.updated = new Date(simulation.updated).toLocaleString();
-      this.projectSize = simulation.projectSize !== undefined ? ((simulation.projectSize) / 1024).toFixed(2) + ' KB' : '';
-      this.resultsSize = simulation.resultsSize !== undefined ? (simulation.resultsSize / 1024).toFixed(2) + ' KB' : 'N/A';
-      this.projectUrl = `${urls.dispatchApi}run/${simulation.id}/download`;
-      this.simulatorUrl = `${this.config.simulatorsAppUrl}simulators/${simulation.simulator}/${simulation.simulatorVersion}`;
-      this.resultsUrl = `${urls.dispatchApi}download/result/${simulation.id}`;
+      const statusRunning = SimulationStatusService.isSimulationStatusRunning(simulation.status);
+      const statusSucceeded = SimulationStatusService.isSimulationStatusSucceeded(simulation.status);
+      this.formattedSimulation.next({
+        id: simulation.id,
+        name: simulation.name,
+        simulator: simulation.simulator,
+        simulatorVersion: simulation.simulatorVersion,
+        statusRunning: statusRunning,
+        statusSucceeded: statusSucceeded,
+        statusLabel: SimulationStatusService.getSimulationStatusMessage(simulation.status, true),
+        runtime: simulation.runtime !== undefined ? Math.round((simulation.runtime)/1000).toString() + ' s' : 'N/A',
+        submitted: new Date(simulation.submitted).toLocaleString(),
+        updated: new Date(simulation.updated).toLocaleString(),
+        projectSize: simulation.projectSize !== undefined ? ((simulation.projectSize) / 1024).toFixed(2) + ' KB' : '',
+        resultsSize: simulation.resultsSize !== undefined ? (simulation.resultsSize / 1024).toFixed(2) + ' KB' : 'N/A',
+        projectUrl: `${urls.dispatchApi}run/${simulation.id}/download`,
+        simulatorUrl: `${this.config.simulatorsAppUrl}simulators/${simulation.simulator}/${simulation.simulatorVersion}`,
+        resultsUrl: `${urls.dispatchApi}download/result/${simulation.id}`,
+      });
+      this.statusRunning.next(statusRunning);
 
       this.combineArchive = undefined;
-      this.sedmlLocations = undefined;
+      this._sedmlLocations = undefined;
+      this.sedmlLocations.next(this._sedmlLocations);
       this.selectedSedmlLocation = undefined;
-      this.reportIds = undefined;
-      this.selectedReportId = undefined;
+      this._reportIds = undefined;
+      this.reportIds.next(this._reportIds);
+      this._selectedReportId = undefined;
+      this.selectedReportId.next(this._selectedReportId);
       this.formGroup.controls.sedmlLocation.disable();
       this.formGroup.controls.reportId.disable();
 
-      if (!this.statusRunning) {
+      if (!statusRunning) {
         this.dispatchService.getSimulationLogs(this.uuid)
           .subscribe((data: any) => {
             if (data.data === undefined) {
               // TODO: should this be interpreted as an error message?
-              this.outLog = data.message;
-              this.errLog = '';
+              this.logs.next({
+                out: data.message,
+                err: '',
+              });
             } else {
-              this.outLog = data.data.output;
-              this.errLog = data.data.error;
+              this.logs.next({
+                out: data.data.output,
+                err: data.data.error,
+              });
             }
           });
       }
 
-      if (this.statusSucceeded) {
+      if (statusSucceeded) {
         this.visualisationService
           .getResultStructure(this.uuid)
           .subscribe((response: any): void => {
@@ -129,14 +168,15 @@ export class ViewComponent implements OnInit {
   private setProjectOutputs(combineArchive: CombineArchive): void {
     this.combineArchive = combineArchive;
 
-    this.sedmlLocations = Object.keys(combineArchive);
-    if (this.sedmlLocations?.length) {
+    this._sedmlLocations = Object.keys(combineArchive);
+    this.sedmlLocations.next(this._sedmlLocations);
+    if (this._sedmlLocations?.length) {
       this.formGroup.controls.sedmlLocation.enable();
     } else {
       this.formGroup.controls.sedmlLocation.disable();
     }
 
-    const selectedSedmlLocation = this.sedmlLocations?.[0];
+    const selectedSedmlLocation = this._sedmlLocations?.[0];
     this.formGroup.controls.sedmlLocation.setValue(selectedSedmlLocation);
     this.selectSedmlLocation(selectedSedmlLocation);
   }
@@ -144,20 +184,22 @@ export class ViewComponent implements OnInit {
   selectSedmlLocation(selectedSedmlLocation?: string): void {
     this.selectedSedmlLocation = selectedSedmlLocation;
 
-    this.reportIds = this.combineArchive && selectedSedmlLocation ? this.combineArchive[selectedSedmlLocation] : undefined;
-    if (this.reportIds?.length) {
+    this._reportIds = this.combineArchive && selectedSedmlLocation ? this.combineArchive[selectedSedmlLocation] : undefined;
+    this.reportIds.next(this._reportIds);
+    if (this._reportIds?.length) {
       this.formGroup.controls.reportId.enable();
     } else {
       this.formGroup.controls.reportId.disable();
     }
 
-    const selectedReportId = this.reportIds?.[0];
+    const selectedReportId = this._reportIds?.[0];
     this.formGroup.controls.reportId.setValue(selectedReportId);
     this.selectReportId(selectedReportId);
   }
 
   selectReportId(selectedReportId?: string): void {
-    this.selectedReportId = selectedReportId;
+    this._selectedReportId = selectedReportId;
+    this.selectedReportId.next(this._selectedReportId);
 
     this.visualisationService.updateDataEvent.next(undefined);
 
