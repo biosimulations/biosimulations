ed9ec90d810513305f59400b37f9103ee9942ec3
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-12-14 18:08:19 -0500

    fix tests

--- biosimulations/apps/dispatch-service/src/app/results/results.controller.spec.ts
+++ biosimulations/apps/dispatch-service/src/app/results/results.controller.spec.ts
@@ -2,13 +2,22 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ResultsController } from './results.controller';
 import { ConfigService } from '@nestjs/config';
 import { ResultsService } from './results.service';
+import { SimulationRunService } from '../simulation-run/simulation-run.service';
+
+class MockSimulationsRunService {
+  async sendReport() {}
+}
 describe('ResultsController', () => {
   let controller: ResultsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ResultsController],
-      providers: [ResultsService, ConfigService],
+      providers: [
+        ResultsService,
+        ConfigService,
+        { provide: SimulationRunService, useClass: MockSimulationsRunService }
+      ]
     }).compile();
 
     controller = module.get<ResultsController>(ResultsController);
