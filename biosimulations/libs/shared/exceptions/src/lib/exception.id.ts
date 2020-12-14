2c8ea0007a62d3daa9326ebf32adf92783149ad9
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-12-14 12:58:08 -0500

    add handling for mongo key error

--- biosimulations/libs/shared/exceptions/src/lib/exception.id.ts
+++ biosimulations/libs/shared/exceptions/src/lib/exception.id.ts
@@ -1,4 +1,5 @@
 import { HttpStatus } from '@nestjs/common';
+
 import { StrictModeError } from './filters/Mongo/strict-mode-exception';
 
 // use this as a list of all the  errors that are defined/caught. This will provide the source of information for creating the error responses.
@@ -7,6 +8,12 @@ const errors = {
     id: 1,
     title: 'Extra Fields Error',
     detail: 'The input object contains fields that are not in the schema',
-    status: HttpStatus.BAD_REQUEST,
+    status: HttpStatus.BAD_REQUEST
   },
+  KeyConflictError: {
+    id: 2,
+    title: 'Key Conflict',
+    detail: 'The value for a unique key is already present in the database',
+    status: HttpStatus.CONFLICT
+  }
 };
--- biosimulations/libs/shared/exceptions/src/lib/shared-exceptions.module.ts
+++ biosimulations/libs/shared/exceptions/src/lib/shared-exceptions.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { BiosimulationsExceptionFilter } from './biosimulations-exception.filter';
 import { DefaultFilter } from './default.filter';
+import { MongoErrorFilter } from './filters/Mongo/MongoError';
 import { MongooseErrorInterceptor } from './filters/Mongo/mongose-exception.filter';
 
 import { StrictModeExceptionFilter } from './filters/Mongo/strict-mode-exception.filter';
@@ -14,31 +15,36 @@ import { HttpExceptionFilter } from './http-exception.filter';
     // ENSURE that the defualt filter is provided first! Filters catch from the bottom up
     {
       provide: APP_FILTER,
-      useClass: DefaultFilter,
+      useClass: DefaultFilter
     },
     // Keep mongose filter above other mongo errors
     {
       provide: APP_INTERCEPTOR,
-      useClass: MongooseErrorInterceptor,
+      useClass: MongooseErrorInterceptor
     },
     {
       provide: APP_FILTER,
-      useClass: BiosimulationsExceptionFilter,
-    },
-    {
-      provide: APP_FILTER,
-      useClass: StrictModeExceptionFilter,
+      useClass: MongoErrorFilter
     },
 
     {
       provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
+      useClass: BiosimulationsExceptionFilter
     },
     {
       provide: APP_FILTER,
-      useClass: ValidationExceptionFilter,
+      useClass: StrictModeExceptionFilter
     },
+
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ValidationExceptionFilter
+    }
   ],
-  exports: [],
+  exports: []
 })
 export class SharedExceptionsModule {}
