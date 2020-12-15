652e29eb371664493c7041988518df126762396c
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-12-14 20:34:44 -0500

    implement getting reports

--- biosimulations/apps/dispatch/src/app/services/visualization/visualization.service.ts
+++ biosimulations/apps/dispatch/src/app/services/visualization/visualization.service.ts
@@ -1,14 +1,14 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@biosimulations/shared/environments';
-import { Subject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { urls } from '@biosimulations/config/common';
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class VisualizationService {
-  private resultsEndpoint = `${urls.dispatchApi}result`;
+  private resultsEndpoint = `${urls.dispatchApi}results`;
   constructor(private http: HttpClient) {}
 
   getReport(uuid: string, sedml: string, report: string) {
@@ -18,7 +18,20 @@ export class VisualizationService {
     );
   }
 
+  /* @TODO The api does not keep track of higher level sedml file
+   *  @jonrkarr The api is setting an ID for each report produced, witouth keeeping track of the sedml file that it is from. The design chart form is expecting a top level sedml file name, and then names for each task under that. Should I change the api? *
+   */
   getResultStructure(uuid: string) {
-    return this.http.get(`${this.resultsEndpoint}/structure/${uuid}`);
+    return this.http.get(`${this.resultsEndpoint}/${uuid}?sparse=true`).pipe(
+      //tap((x) => console.log(x)),
+      map((result: any) => result.reports),
+      map((array: { [key: string]: string }[]) => {
+        const taskNames: string[] = [];
+        array.forEach((item) => {
+          taskNames.push(item.reportId);
+        });
+        return { simulation: taskNames };
+      })
+    );
   }
 }
