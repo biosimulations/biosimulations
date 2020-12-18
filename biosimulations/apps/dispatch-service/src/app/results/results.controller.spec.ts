f58b72dc130f8ecd11e892b52000b071827e24ca
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-12-18 16:48:20 -0500

    fix test

--- biosimulations/apps/dispatch-service/src/app/results/results.controller.spec.ts
+++ biosimulations/apps/dispatch-service/src/app/results/results.controller.spec.ts
@@ -3,6 +3,7 @@ import { ResultsController } from './results.controller';
 import { ConfigService } from '@nestjs/config';
 import { ResultsService } from './results.service';
 import { SimulationRunService } from '../simulation-run/simulation-run.service';
+import { ArchiverService } from '../services/archiver/archiver.service';
 
 class MockSimulationsRunService {
   async sendReport() {}
@@ -10,6 +11,10 @@ class MockSimulationsRunService {
 class MockResultsService {
   async method() {}
 }
+class mockArchiverService {
+  async method() {}
+}
+
 describe('ResultsController', () => {
   let controller: ResultsController;
 
@@ -18,6 +23,7 @@ describe('ResultsController', () => {
       controllers: [ResultsController],
       providers: [
         { provide: ResultsService, useClass: MockResultsService },
+        { provide: ArchiverService, useClass: mockArchiverService },
         ConfigService,
         { provide: SimulationRunService, useClass: MockSimulationsRunService }
       ]
