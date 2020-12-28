a093597c51c1eef2d02d698a4c15b83da4695adc
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-12-28 00:23:32 -0500

    fix mail service init

--- biosimulations/apps/mail-service/src/main.ts
+++ biosimulations/apps/mail-service/src/main.ts
@@ -2,18 +2,23 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { Transport, MicroserviceOptions, NatsOptions } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.NATS,
-      options: {
-        url: 'nats://localhost:4222',
-      },
+  const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+  const natsUrl = configService.get("nats.url")
+  const natsQueue = configService.get("nats.queue")
+  const natsConfig: NatsOptions = {
+    transport: Transport.NATS,
+    options: {
+      url: natsUrl,
+      queue: natsQueue,
+      reconnect: true
     }
-  );
-  app.listen(() => Logger.log('Running'));
+  }
+  app.connectMicroservice(natsConfig)
+  await app.startAllMicroservicesAsync()
 }
 
 bootstrap();
