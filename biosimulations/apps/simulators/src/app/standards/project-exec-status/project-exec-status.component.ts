13cef5ac486b0babee16ffdd7119b507ca8dd96f
Jonathan Karr <jonrkarr@gmail.com>
2020-12-29 17:00:21 -0500

    adding documentation for format for the execution status of COMBINE/OMEX archives

---
+++ biosimulations/apps/simulators/src/app/standards/project-exec-status/project-exec-status.component.ts
@@ -0,0 +1,25 @@
+import { Component, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+  TocSection,
+  TocSectionsContainerDirective,
+} from '@biosimulations/shared/ui';
+import { ConfigService } from '@biosimulations/shared/services';
+
+@Component({
+  templateUrl: './project-exec-status.component.html',
+  styleUrls: ['./project-exec-status.component.sass'],
+})
+export class ProjectExecStatusComponent {
+  tocSections!: Observable<TocSection[]>;
+
+  @ViewChild(TocSectionsContainerDirective)
+  set tocSectionsContainer(container: TocSectionsContainerDirective) {
+    setTimeout(() => {
+      this.tocSections = container.sections$;
+    });
+  }
+
+  constructor(public config: ConfigService) {
+  }
+}
