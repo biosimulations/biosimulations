4b789b0f83e573d2e354809a46ccac463cdac6cd
Jonathan Karr <jonrkarr@gmail.com>
2020-12-31 19:04:38 -0500

    importing highlighting libraries for additional languages

--- biosimulations/apps/simulators/src/highlight.d.ts
+++ biosimulations/apps/simulators/src/highlight.d.ts
@@ -1,3 +1,5 @@
-declare module 'highlight.js/lib/languages/json';
 declare module 'highlight.js/lib/languages/dockerfile';
+declare module 'highlight.js/lib/languages/json';
+declare module 'highlight.js/lib/languages/typescript';
+declare module 'highlight.js/lib/languages/yaml';
 declare module 'highlight.js/lib/core';
