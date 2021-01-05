4729f09e31746bc80a9e0a9131392de6699bcc7a
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-01-05 00:08:27 -0500

    use mail templates (#1876)
    * use mail templates
    closes #1842
    
    * include group id in emails
    
    * restore email form
    closes #1841

--- biosimulations/libs/mail-service/client/src/lib/mail-service-client.service.ts
+++ biosimulations/libs/mail-service/client/src/lib/mail-service-client.service.ts
@@ -5,14 +5,64 @@ import SendGrid from "@sendgrid/mail";
 export class MailClientService {
     constructor(private config: ConfigService) { }
     logger = new Logger(MailClientService.name)
+
+    successTemplate = this.config.get("email").successTemplate as string
+    failureTemplate = this.config.get("email").failureTemplate as string
     private sendEmail(message: SendGrid.MailDataRequired) {
         SendGrid.setApiKey(this.config.get('email').token as string)
-        SendGrid.send(message).then(() => this.logger.log(`Send email to ${message.to} with subject ${message.subject}`))
+        SendGrid.send(message).then(() => this.logger.log(`Sent email to ${message.to}`))
     }
+    /**
+     * Sends a notification email to the submitter of a simulation when the simulation has succeeded. Uses a sendgrid template
+     * @param to The email address of the user
+     * @param id The id of the simulation run
+     * @param name The name of the simulation run
+     * @param date  The date that the simulation run was submitted
+     */
+    public sendSuccessEmail(to: string, id: string, name: string, date: Date) {
 
-    public sendNotificationEmail(to: string, subject: string, text: string, html: string) {
-
-        const message = { to, subject, text, html, from: "notifications@biosimulations.org" }
+        const message: MailData = {
+            to, templateId: this.successTemplate, dynamicTemplateData: {
+                id, name, date:
+                    date.toLocaleString()
+            }, from: "notifications@biosimulations.org", asm: {
+                groupId: 14634
+            }
+        }
         this.sendEmail(message)
     }
+
+    /**
+     *  Sends a notifcation email to the submitter of the a simulation when the simulation has failed. Uses a sendgrid template
+     * @param to The email address of the user
+     * @param id The id of the simulation run
+     * @param name The name of the simulation run
+     * @param date The date the simulation run was submitted
+     */
+    public sendFailureEmail(to: string, id: string, name: string, date: Date) {
+
+        const message: MailData = {
+            to, templateId: this.failureTemplate, dynamicTemplateData: {
+                id, name, date:
+                    date.toLocaleString()
+            }, from: "notifications@biosimulations.org",
+            asm: {
+                groupId: 14634
+            }
+        }
+        this.sendEmail(message)
+    }
+}
+
+interface DynamicTemplateData {
+    id: string,
+    name: string,
+    date: string
+}
+type MailData = SendGrid.MailDataRequired & {
+    to: string,
+    templateId: string,
+    dynamicTemplateData: DynamicTemplateData,
+    from: string,
+    asm: { groupId: number }
 }
