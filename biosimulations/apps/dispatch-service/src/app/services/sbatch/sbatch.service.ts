b9bf645f97af50fc7e9e3d5b72c4f0db5dad8f61
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-01-07 17:00:01 -0500

    fix path closes #1901

--- biosimulations/apps/dispatch-service/src/app/services/sbatch/sbatch.service.ts
+++ biosimulations/apps/dispatch-service/src/app/services/sbatch/sbatch.service.ts
@@ -25,12 +25,15 @@ export class SbatchService {
 #SBATCH --mem-per-cpu=1000
 #SBATCH --partition=crbm
 #SBATCH --qos=general\n
+export MODULEPATH=/isg/shared/modulefiles:/tgcapps/modulefiles
+source /usr/share/Modules/init/bash
 module load singularity
 export XDG_RUNTIME_DIR=${homeDir}/singularityXDG/
 date
 \`wget ${apiDomain}run/${simId}/download -O \"${tempSimDir}/in/${omexName}\" 1>\"${tempSimDir}/out/job.output\" 2>&1\`
 command=\\"singularity run -B ${tempSimDir}/in:/root/in -B ${tempSimDir}/out:/root/out ${simulator} -i \'/root/in/${omexName}\' -o /root/out\\"
 eval \\$command;`;
+    console.log(template)
     return template;
   }
 }
