d62e1ac2a0a345353de44baee25b86d02c207144
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-01-07 23:47:53 -0500

    use force option on pull

--- biosimulations/apps/dispatch-service/src/app/services/sbatch/sbatch.service.ts
+++ biosimulations/apps/dispatch-service/src/app/services/sbatch/sbatch.service.ts
@@ -31,7 +31,7 @@ module load singularity/3.1.1
 export XDG_RUNTIME_DIR=${homeDir}/singularityXDG/
 date
 \`wget ${apiDomain}run/${simId}/download -O "${tempSimDir}/in/${omexName}" 1>"${tempSimDir}/out/job.output" 2>&1\`
-command=\\" singularity pull ${simulator} && singularity run -B ${tempSimDir}/in:/root/in -B ${tempSimDir}/out:/root/out ${simulator} -i '/root/in/${omexName}' -o /root/out\\"
+command=\\" singularity pull --force ${simulator} && singularity run -B ${tempSimDir}/in:/root/in -B ${tempSimDir}/out:/root/out ${simulator} -i '/root/in/${omexName}' -o /root/out\\"
 eval \\$command;`;
     console.log(template)
     return template;
