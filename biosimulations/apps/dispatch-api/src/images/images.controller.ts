505ee3c60150bd74ba220725e8602204b4603afa
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-01-12 15:48:26 -0500

    implement api method

--- biosimulations/apps/dispatch-api/src/images/images.controller.ts
+++ biosimulations/apps/dispatch-api/src/images/images.controller.ts
@@ -1,19 +1,35 @@
 import { permissions } from '@biosimulations/auth/nest';
-import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
-import { Client } from '@nestjs/microservices/external/nats-client.interface';
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ImageMessage, ImageMessagePayload, ImageMessageResponse } from '@biosimulations/messages/messages';
+import { Body, Controller, Get, HttpCode, HttpStatus, Inject, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { map, pluck } from 'rxjs/operators';
 import { refreshImageBody } from './image.dto';
 
 @Controller('images')
+@ApiResponse({})
 @ApiTags('Images', 'Internal')
+
 export class ImagesController {
-    constructor(@Inject("NATS_CLIENT") private client: Client) { }
-    @ApiOperation({})
+    constructor(@Inject("NATS_CLIENT") private client: ClientProxy) { }
+    @ApiOperation({ summary: "Refresh Container Image", description: "Trigger a rebuild of the singulairty image of a particular container" })
     @ApiBody({ type: refreshImageBody })
-    @permissions("refresh:images")
+    @permissions("refresh:Images")
     @Post('refresh')
-    refreshImage(@Body() data: refreshImageBody) {
-        this.client.emit("test")
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async refreshImage(@Body() data: refreshImageBody) {
+        const message = new ImageMessagePayload(data.simulator, data.simulator)
+        // !Replace with wrapper to allow typing 
+        const success = await this.client.send(ImageMessage.refresh, message).pipe<boolean>(
+            map((data: ImageMessageResponse) => data.okay)
+        ).toPromise()
+        if (success) {
+            return
+        } else {
+            throw Error()
+        }
+
+
     }
 
 
--- biosimulations/libs/messages/messages/src/index.ts
+++ biosimulations/libs/messages/messages/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/dispatch';
+export * from './lib/images';
