96cf808a42e7722290834f613756b95e2df109ea
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-01-12 14:57:33 -0500

    fix controller tests

--- biosimulations/apps/dispatch-api/src/images/images.controller.spec.ts
+++ biosimulations/apps/dispatch-api/src/images/images.controller.spec.ts
@@ -1,3 +1,6 @@
+import { BiosimulationsAuthModule } from '@biosimulations/auth/nest';
+import { BiosimulationsConfigModule } from '@biosimulations/config/nest';
+import { SharedNatsClientModule } from '@biosimulations/shared/nats-client';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ImagesController } from './images.controller';
 
@@ -7,6 +10,7 @@ describe('ImagesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ImagesController],
+      imports: [BiosimulationsAuthModule, BiosimulationsConfigModule, SharedNatsClientModule],
     }).compile();
 
     controller = module.get<ImagesController>(ImagesController);
