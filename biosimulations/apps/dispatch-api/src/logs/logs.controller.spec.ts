2bab023136718e202bae5e9b76dfcd3faeae21fd
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-01-21 16:48:25 -0500

    add tests

--- biosimulations/apps/dispatch-api/src/logs/logs.controller.spec.ts
+++ biosimulations/apps/dispatch-api/src/logs/logs.controller.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LogsController } from './logs.controller';
+import { LogsService } from './logs.service';
 
 describe('LogsController', () => {
   let controller: LogsController;
-
+  const mockService = {
+    getLog(id: string) {
+      return {
+        message: `Sample log for ${id}`,
+        output: null
+      };
+    },
+    getOldLogs(id: string) {
+      return {
+        output: 'oldOut',
+        error: 'oldErr'
+      };
+    }
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LogsController],
+      providers: [{ provide: LogsService, useValue: mockService }]
     }).compile();
 
     controller = module.get<LogsController>(LogsController);
@@ -14,5 +29,16 @@ describe('LogsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it(' should get logs from service', async () => {
+    const log = await controller.getLogs('testId');
+    //@ts-ignore
+    expect(log.message).toBe('Sample log for testId');
+  });
+
+  it(' should get older logs from service', async () => {
+    const log = await controller.getLogs('testId');
+    //@ts-ignore
+    expect(log.output).toBe('oldOutoldErr');
   });
 });
