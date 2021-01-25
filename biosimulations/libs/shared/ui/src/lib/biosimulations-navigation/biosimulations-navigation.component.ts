0a651669a8b585de90bee7c761ecb15b37849eac
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-01-25 12:22:47 -0500

    add support for dark mode (#2030)
    Not enabled until issues in #2033 are resolved

--- biosimulations/libs/shared/ui/src/lib/biosimulations-navigation/biosimulations-navigation.component.ts
+++ biosimulations/libs/shared/ui/src/lib/biosimulations-navigation/biosimulations-navigation.component.ts
@@ -6,7 +6,7 @@ import { map, shareReplay } from 'rxjs/operators';
 @Component({
   selector: 'biosimulations-navigation',
   templateUrl: './biosimulations-navigation.component.html',
-  styleUrls: ['./biosimulations-navigation.component.scss'],
+  styleUrls: ['./biosimulations-navigation.component.scss']
 })
 export class BiosimulationsNavigationComponent {
   @Input()
@@ -27,11 +27,14 @@ export class BiosimulationsNavigationComponent {
   @Input()
   showNews!: boolean;
 
+  @Input()
+  darkMode = false;
+
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
       map((result) => result.matches),
-      shareReplay(),
+      shareReplay()
     );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
