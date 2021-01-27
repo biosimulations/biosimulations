beb10236fc390fe4da435c57e2754532783fc5d2
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-01-26 19:16:37 -0500

    read stdoutput into log

--- biosimulations/apps/dispatch-service/src/app/results/log.service.ts
+++ biosimulations/apps/dispatch-service/src/app/results/log.service.ts
@@ -3,6 +3,7 @@ import { FileService } from './file.service';
 import { promises as fsPromises } from 'fs';
 import YAML from 'yaml';
 import { SimulationRunService } from '@biosimulations/dispatch/nest-client';
+import { CombineArchiveLog } from '@biosimulations/dispatch/api-models';
 Injectable();
 export class LogService {
   public constructor(
@@ -11,19 +12,30 @@ export class LogService {
   ) {}
 
   public async createLog(id: string): Promise<void> {
-    const path = `${this.fileService.getResultsDirectory(id)}/log.yml`;
-    return this.uploadLog(id, path);
+    const path = this.fileService.getResultsDirectory(id);
+    return this.readLog(path).then((value) => this.uploadLog(id, value));
   }
 
-  private uploadLog(id: string, path: string): Promise<void> {
-    return fsPromises.readFile(path, 'utf8').then((file) => {
-      const log = YAML.parse(file);
-      return this.submit
-        .sendLog(id, log)
-        .toPromise()
-        .then((_) => {
-          return;
-        });
+  private async readLog(path: string): Promise<CombineArchiveLog> {
+    const yamlFile = `${path}/log.yml`;
+    const logFile = `${path}/job.out`;
+    return fsPromises.readFile(yamlFile, 'utf8').then((file) => {
+      const log = YAML.parse(file) as CombineArchiveLog;
+      return fsPromises.readFile(logFile, 'utf8').then((stdout) => {
+        if (log) {
+          log.output = stdout;
+        }
+        return log;
+      });
     });
+  }
+
+  private uploadLog(id: string, log: CombineArchiveLog): Promise<void> {
+    return this.submit
+      .sendLog(id, log)
+      .toPromise()
+      .then((_) => {
+        return;
+      });
   }
 }
