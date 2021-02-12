96c597974dad1eff9378e8de7c5887270416a723
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-02-11 21:22:10 -0500

    dispatch lint

--- biosimulations/apps/dispatch/src/app/services/simulation/simulation.service.ts
+++ biosimulations/apps/dispatch/src/app/services/simulation/simulation.service.ts
@@ -8,7 +8,7 @@ import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
 import { urls } from '@biosimulations/config/common';
 import { ConfigService } from '@biosimulations/shared/services';
 import { map } from 'rxjs/internal/operators/map';
-import { concatAll, debounceTime, share, shareReplay } from 'rxjs/operators';
+import { concatAll, debounceTime, shareReplay } from 'rxjs/operators';
 import { SimulationRun } from '@biosimulations/dispatch/api-models';
 
 @Injectable({
