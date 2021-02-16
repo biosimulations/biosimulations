d473fefbf4ec066de9b9436c449faa6a1a8c8976
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-02-16 15:41:40 -0500

    move errors to new library

--- biosimulations/libs/shared/error-handler/src/index.ts
+++ biosimulations/libs/shared/error-handler/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/shared-error-handler.module';
+export * from './lib/biosimulations-error';
+export * from './lib/error-handler';
+export * from './lib//error-404.component';
--- biosimulations/libs/shared/error-handler/src/lib/shared-error-handler.module.ts
+++ biosimulations/libs/shared/error-handler/src/lib/shared-error-handler.module.ts
@@ -1,7 +1,50 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { Error404Component } from './error-404.component';
+import { Error500Component } from './error-500.component';
+import { UnderConstructionComponent } from './under-construction.component';
+import { UnderMaintainenceComponent } from './under-maintainence.component';
+import { ErrorComponent } from './error.component';
+import { BiosimulationsIconsModule } from '@biosimulations/shared/icons';
+import { SharedUiModule } from '@biosimulations/shared/ui';
+import { ErrorHandler as BiosimulationsErrorHandler } from './error-handler';
+
+export const errorRoutes: Routes = [
+  {
+    path: '404',
+    component: Error404Component,
+  },
+  {
+    path: '500',
+    component: Error500Component,
+  },
+  {
+    path: 'construction',
+    component: UnderConstructionComponent,
+  },
+  {
+    path: 'maintainence',
+    component: UnderMaintainenceComponent,
+  },
+];
 
 @NgModule({
-  imports: [CommonModule],
+  declarations: [
+    ErrorComponent,
+    Error404Component,
+    Error500Component,
+    UnderConstructionComponent,
+    UnderMaintainenceComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(errorRoutes),
+    BiosimulationsIconsModule,
+    SharedUiModule,
+  ],
+  exports: [RouterModule],
+  providers: [{ provide: ErrorHandler, useClass: BiosimulationsErrorHandler }],
 })
 export class SharedErrorHandlerModule {}
