0ecfcb20f935035db5ebf5379b9ecd5ae0731e7c
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-02-24 14:46:00 -0500

    Merge pull request #2241 from biosimulations/jobmonitoring
    Jobmonitoring

--- biosimulations/apps/dispatch-service/src/app/services/sbatch/sbatch.service.ts
+++ biosimulations/apps/dispatch-service/src/app/services/sbatch/sbatch.service.ts
@@ -21,19 +21,20 @@ export class SbatchService {
 #SBATCH --time=20:00
 #SBATCH --output=${tempSimDir}/out/job.output
 #SBATCH --error=${tempSimDir}/out/job.output
-#SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=1000
+#SBATCH --chdir=${tempSimDir}
 #SBATCH --partition=crbm
 #SBATCH --qos=general\n
 
 export MODULEPATH=/isg/shared/modulefiles:/tgcapps/modulefiles
 source /usr/share/Modules/init/bash
-module load singularity/3.1.1
-export XDG_RUNTIME_DIR=${homeDir}/singularityXDG/
-export SINGULARITY_CACHEDIR=${homeDir}/singularityCache/
+module load singularity/3.7.1
+export XDG_RUNTIME_DIR=${homeDir}/singularity/XDG/
+export SINGULARITY_CACHEDIR=${homeDir}/singularity/cache/
+export SINGULARITY_LOCALCACHEDIR=${homeDir}/singularity/localCache/
+export SINGULARITY_TMPDIR=${homeDir}/singularity/tmp/
+export SINGULARITY_PULLFOLDER=${homeDir}/singularity/images/
 wget ${apiDomain}run/${simId}/download -O '${tempSimDir}/in/${omexName}' 1>'${tempSimDir}/out/job.output' 2>&1
-singularity run -B ${tempSimDir}/in:/root/in -B ${tempSimDir}/out:/root/out ${simulator} -i '/root/in/${omexName}' -o '/root/out'
-date`;
+singularity run -B ${tempSimDir}/in:/root/in -B ${tempSimDir}/out:/root/out ${simulator} -i '/root/in/${omexName}' -o '/root/out'`;
     return template;
   }
 
@@ -42,16 +43,18 @@ date`;
     const template = `#!/bin/bash    
 #SBATCH --job-name=BioSimulations_Image_Update
 #SBATCH --time=10:00
-#SBATCH --output=${homeDir}/singularityImages/job.output
-#SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=1000
+#SBATH --chdir${homeDir}/singularity/images/
 #SBATCH --partition=crbm
 #SBATCH --qos=general\n
+
 export MODULEPATH=/isg/shared/modulefiles:/tgcapps/modulefiles
 source /usr/share/Modules/init/bash
-module load singularity/3.1.1
-export XDG_RUNTIME_DIR=${homeDir}/singularityXDG/
-export SINGULARITY_CACHEDIR=${homeDir}/singularityCache/
+module load singularity/3.7.1
+export XDG_RUNTIME_DIR=${homeDir}/singularity/XDG/
+export SINGULARITY_CACHEDIR=${homeDir}/singularity/cache/
+export SINGULARITY_LOCALCACHEDIR=${homeDir}/singularity/localCache/
+export SINGULARITY_TMPDIR=${homeDir}/singularity/tmp/
+export SINGULARITY_PULLFOLDER=${homeDir}/singularity/images/
 command=\\"cd singularityImages && singularity pull ${force} ${url}\\"
     eval \\$command; `;
     return template;
