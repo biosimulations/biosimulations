09b4c86d7114283fa81ab53fa667490e2446d009
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-03-01 10:41:18 -0500

    add logging to api error handling

--- biosimulations/libs/shared/exceptions/filters/src/lib/filters/Mongo/mongose-exception.filter.ts
+++ biosimulations/libs/shared/exceptions/filters/src/lib/filters/Mongo/mongose-exception.filter.ts
@@ -2,6 +2,7 @@ import {
   CallHandler,
   ExecutionContext,
   Injectable,
+  Logger,
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
@@ -10,11 +11,17 @@ import { StrictModeError } from './strict-mode-exception';
 
 @Injectable()
 export class MongooseErrorInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+  private logger = new Logger(MongooseErrorInterceptor.name);
+
+  public intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> {
     // next.handle() is an Observable of the controller's result value
     return next.handle().pipe(
       catchError((error) => {
         if (error.name == 'StrictModeError') {
+          this.logger.error(error);
           throw new StrictModeError(error.path, error.message, error.immutable);
         } else {
           throw error;
