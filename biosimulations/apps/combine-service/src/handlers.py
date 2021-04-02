43d6859071446a4278c0142e3450794425f94b3f
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-04-02 01:19:30 -0400

    complete skeleton

--- biosimulations/apps/combine-service/src/handlers.py
+++ biosimulations/apps/combine-service/src/handlers.py
@@ -1,6 +1,13 @@
-def get_sed():
-    return "true"
-
-def create_combine():
+"""
+Return json formated information based on the url of a combine archive 
+"""
+def get_sed(archiveUrl):  
+    # TODO Implement the processing of combine archive 
+    return archiveUrl
+"""
+Create a combine archive given a model file and simulation file. File objects are class werkzeug.FileStorage
+"""
+def create_combine(modelFile, simulationFile):
+    # TODO Implement the creation of combine archive 
     return "true"
     
