b4a53c6f7ac13d00a3711150642c0fa5689a8f25
Jonathan Karr <jonrkarr@gmail.com>
2021-04-09 10:00:43 -0400

    allowing some cross origin requests to combine-service

--- biosimulations/apps/combine-service/src/app.py
+++ biosimulations/apps/combine-service/src/app.py
@@ -1,6 +1,6 @@
 from . import exceptions
+from flask_cors import CORS
 import connexion
-
 
 # Instantiate app from specs
 app = connexion.App(__name__, specification_dir='spec')
@@ -19,3 +19,20 @@ app.add_api('combine-service.yml',
 
 app.add_error_handler(500, exceptions._render_exception)
 app.add_error_handler(exceptions.BadRequestException, exceptions._render_exception)
+
+# cross-origin resource sharing
+CORS(app.app,
+     origins=[
+         'http://127.0.0.1:4200',
+         'http://127.0.0.1:4201',
+         'http://127.0.0.1:4202',
+         'http://localhost:4200',
+         'http://localhost:4201',
+         'http://localhost:4202',
+         'https://biosimulators.org',
+         'https://www.biosimulators.org',
+         'https://biosimulators.dev',
+         'https://www.biosimulators.dev',
+         'https://run.biosimulations.dev',
+         'https://run.biosimulations.org',
+     ])
