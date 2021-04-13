eed1c8dea04fa4dc4a1ba93ff6a27342aec4a36c
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-04-13 11:39:32 -0400

    lint fixes

--- biosimulations/libs/messages/messages/src/lib/images.ts
+++ biosimulations/libs/messages/messages/src/lib/images.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
 export enum ImageMessage {
   refresh = 'image.refresh',
 }
--- biosimulations/libs/simulators/database-models/src/lib/algorithmParameter.ts
+++ biosimulations/libs/simulators/database-models/src/lib/algorithmParameter.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
 import {
   ValueType,
   AlgorithmParameter as IAlgorithmParameter,
