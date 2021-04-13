6bb60b9beef323fb7759c73df10e9fd2ab02e9c1
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-04-13 11:01:16 -0400

    generate datamodel utils library

---
+++ biosimulations/libs/datamodel/utils/src/lib/datamodel-utils.spec.ts
@@ -0,0 +1,7 @@
+import { datamodelUtils } from './datamodel-utils';
+
+describe('datamodelUtils', () => {
+  it('should work', () => {
+    expect(datamodelUtils()).toEqual('datamodel-utils');
+  });
+});
---
+++ biosimulations/libs/datamodel/utils/src/lib/datamodel-utils.ts
@@ -0,0 +1,3 @@
+export function datamodelUtils(): string {
+  return 'datamodel-utils';
+}
