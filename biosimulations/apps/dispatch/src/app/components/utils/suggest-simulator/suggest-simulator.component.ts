454c26d60feaf1f530c2bbe6c9d052bb19123490
Jonathan Karr <jonrkarr@gmail.com>
2021-05-02 18:37:53 -0400

    displaying all known alternate algorithms in suggest simulator form; incorporating algorithm substition into submit simulation form

--- biosimulations/apps/dispatch/src/app/components/utils/suggest-simulator/suggest-simulator.component.ts
+++ biosimulations/apps/dispatch/src/app/components/utils/suggest-simulator/suggest-simulator.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { DispatchService, SimulatorsData, SimulatorSpecsMap, OntologyTerm, OntologyTermsMap } from '../../../services/dispatch/dispatch.service';
+import { DispatchService, SimulatorsData } from '../../../services/dispatch/dispatch.service';
 import { CombineService } from '../../../services/combine/combine.service';
-import { AlgorithmSubstitution, AlgorithmSubstitutionPolicy } from '../../../kisao.interface';
-import { Observable, Subscription, combineLatest } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { AlgorithmSubstitution, AlgorithmSubstitutionPolicy, AlgorithmSubstitutionPolicyLevels, Algorithm as KisoaAlgorithm } from '../../../kisao.interface';
+import { Observable } from 'rxjs';
+import { map, withLatestFrom, concatAll } from 'rxjs/operators';
 import { ActivatedRoute, Params } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 interface Algorithm {
   id: string;
   name: string;
-  url?: string;
+  url: string;
 }
 
-interface AltAlgPolicy {
-  policy: AlgorithmSubstitutionPolicy;
+interface AlgorithmPolicy {
   algorithms: Algorithm[];
+  maxPolicy: AlgorithmSubstitutionPolicy;
 }
 
 interface Simulator {
@@ -26,32 +26,32 @@ interface Simulator {
   algorithms: Algorithm[];
 }
 
-interface SimulatorAlgPolicy {
-  policy: AlgorithmSubstitutionPolicy;
+interface SimulatorPolicy {
   simulators: Simulator[];
+  maxPolicy: AlgorithmSubstitutionPolicy;
 }
 
-interface Suggestions {
-  altAlgsByPolicies: AltAlgPolicy[];
-  simulatorsByPolicies: SimulatorAlgPolicy[];
+interface AlgorithmData {
+  algorithm: Algorithm;
+  altAlgorithms: AlgorithmPolicy[];
+  simulators: SimulatorPolicy[];
 }
+
+const EBI_KISAO_BASE_URL = 'https://www.ebi.ac.uk/ols/ontologies/kisao/terms?iri=http%3A%2F%2Fwww.biomodels.net%2Fkisao%2FKISAO%23';
 
 @Component({
   selector: 'biosimulations-suggest-simulator',
   templateUrl: './suggest-simulator.component.html',
   styleUrls: ['./suggest-simulator.component.scss'],
 })
-export class SuggestSimulatorComponent implements OnInit, OnDestroy {
-  algorithms: Observable<Algorithm[]> | undefined = undefined;
-  private algorithmsMap: OntologyTermsMap | undefined = undefined;
-  private simulatorSpecsMap: SimulatorSpecsMap | undefined = undefined;
+export class SuggestSimulatorComponent implements OnInit {
+  algorithms: Observable<AlgorithmData[]> | undefined = undefined;
+  private algorithmsMap: {[id: string]: AlgorithmData} | undefined = undefined;
 
   formGroup: FormGroup;
   selectedAlgorithm: Algorithm | undefined = undefined;
 
-  suggestions: Observable<Suggestions | undefined> | undefined = undefined;
-
-  private subscriptions: Subscription[] = [];
+  suggestions: AlgorithmData | undefined = undefined;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -70,188 +70,25 @@ export class SuggestSimulatorComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     const simulatorsDataObs = this.dispatchService.getSimulatorsFromDb();
-    const algorithmsMapObs = simulatorsDataObs.pipe(
-      map((simulatorsData: SimulatorsData): OntologyTermsMap => {
-        return simulatorsData.simulationAlgorithms;
-      }));
-    const simulatorSpecsMapObs = simulatorsDataObs.pipe(
-      map((simulatorsData: SimulatorsData): SimulatorSpecsMap => {
-        return simulatorsData.simulatorSpecs;
-      }));
+    const algSubObs = simulatorsDataObs.pipe(
+      map((simulatorsData: SimulatorsData): Observable<AlgorithmSubstitution[] | undefined> => {
+        return this.combineService.getSimilarAlgorithms(Object.keys(simulatorsData.simulationAlgorithms));
+      }),
+      concatAll(),
+      withLatestFrom(simulatorsDataObs, this.activatedRoute.queryParams),
+    )
 
-    this.algorithms = combineLatest([algorithmsMapObs, this.activatedRoute.queryParams]).pipe(
-      map((algorithmsMapParams: [OntologyTermsMap, Params]): Algorithm[] => {
-        this.algorithmsMap = algorithmsMapParams[0];
-        const params = algorithmsMapParams[1];
+    this.algorithms = algSubObs.pipe(
+      map((args: [AlgorithmSubstitution[] | undefined, SimulatorsData, Params]): AlgorithmData[] => {
+        const algSubstitutions = args[0];
+        const simulatorsData = args[1];
+        const simulatorSpecsMap = simulatorsData.simulatorSpecs;
+        const simulationAlgorithmsMap = simulatorsData.simulationAlgorithms;
+        const params = args[2];
 
-        const algorithms = Object.values(this.algorithmsMap)
-          .map((alg: OntologyTerm): Algorithm => {
-            return {
-              id: alg.id,
-              name: alg.name,
-            };
-          });
-        algorithms.sort((a: Algorithm, b: Algorithm): number => {
-          return a.name.localeCompare(b.name, undefined, { numeric: true });
-        });
-
-        const simulationAlgorithm = params?.simulationAlgorithm;
-        if (simulationAlgorithm) {
-          this.formGroup.controls.algorithm.setValue(simulationAlgorithm)
-        }
-
-        this.formGroup.controls.algorithm.enable();
-        return algorithms;
-      })
-    );
-
-    const simulatorSpecsSub = simulatorSpecsMapObs.subscribe(
-      (simulatorSpecsMap: SimulatorSpecsMap): void => {
-        this.simulatorSpecsMap = simulatorSpecsMap;
-      });
-    this.subscriptions.push(simulatorSpecsSub);
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.forEach((subscription: Subscription) =>
-      subscription.unsubscribe(),
-    );
-  }
-
-  getAlgorithmSimulatorSuggestions(): void {
-    const selectedAlgorithmId = this.formGroup.controls.algorithm.value;
-    this.selectedAlgorithm = {
-      id: selectedAlgorithmId,
-      name: this.algorithmsMap?.[selectedAlgorithmId]?.name as string,
-      url: 'https://www.ebi.ac.uk/ols/ontologies/kisao/terms?iri=http%3A%2F%2Fwww.biomodels.net%2Fkisao%2FKISAO%23' + selectedAlgorithmId,
-    }
-    this.suggestions = undefined;
-
-    this.suggestions = this.combineService.getSimilarAlgorithms([selectedAlgorithmId]).pipe(
-      map((altAlgs: AlgorithmSubstitution[] | undefined): Suggestions | undefined => {
-        if (altAlgs) {
-          const altAlgsByPolicies: any = {};
-          const simulatorAlgPolicies: any = {};
-          altAlgs
-            .filter((altAlg: AlgorithmSubstitution): boolean => {
-              return altAlg.algorithms[1].id in (this.algorithmsMap as OntologyTermsMap);
-            })
-            .forEach((altAlg: AlgorithmSubstitution): void => {
-              if (!(altAlg.maxPolicy.id in altAlgsByPolicies)) {
-                altAlgsByPolicies[altAlg.maxPolicy.id] = {
-                  policy: altAlg.maxPolicy,
-                  algorithms: [],
-                };
-              }
-              altAlgsByPolicies[altAlg.maxPolicy.id].algorithms.push({
-                id: altAlg.algorithms[1].id,
-                name: altAlg.algorithms[1].name,
-                url: 'https://www.ebi.ac.uk/ols/ontologies/kisao/terms?iri=http%3A%2F%2Fwww.biomodels.net%2Fkisao%2FKISAO%23' + altAlg.algorithms[1].id,
-              });
-
-              this.algorithmsMap?.[altAlg.algorithms[1].id]?.simulators?.forEach((simulator: string): void => {
-                if (!(simulator in simulatorAlgPolicies)) {
-                  simulatorAlgPolicies[simulator] = {
-                    id: simulator,
-                    algPolicies: [],
-                  };
-                }
-                simulatorAlgPolicies[simulator].algPolicies.push(altAlg)
-              });
-            });
-
-          const simulatorAlgMaxPolicies = Object.values(simulatorAlgPolicies)
-            .map((simulatorAlgPolicies: any): any => {
-              let maxPolicy = simulatorAlgPolicies.algPolicies[0].maxPolicy;
-              simulatorAlgPolicies.algPolicies.forEach((altAlg: AlgorithmSubstitution): void => {
-                if (altAlg.maxPolicy.level < maxPolicy.level) {
-                  maxPolicy = altAlg.maxPolicy;
-                }
-              });
-
-              const algorithms = simulatorAlgPolicies.algPolicies
-                .filter((altAlg: AlgorithmSubstitution): boolean => {
-                  return altAlg.maxPolicy.level <= maxPolicy.level;
-                })
-                .map((altAlg: AlgorithmSubstitution): Algorithm => {
-                  return {
-                    id: altAlg.algorithms[1].id,
-                    name: altAlg.algorithms[1].name,
-                    url: 'https://www.ebi.ac.uk/ols/ontologies/kisao/terms?iri=http%3A%2F%2Fwww.biomodels.net%2Fkisao%2FKISAO%23' + altAlg.algorithms[1].id,
-                  }
-                });
-              algorithms.sort((a: Algorithm, b: Algorithm): number => {
-                return a.name.localeCompare(b.name, undefined, { numeric: true });
-              });
-
-              return {
-                id: simulatorAlgPolicies.id,
-                maxPolicy,
-                algorithms,
-              };
-            });
-
-          const simulatorsByPolicies: {[id: string]: SimulatorAlgPolicy} = {};
-          simulatorAlgMaxPolicies.forEach((simulatorAlgMaxPolicy: any): void => {
-            if (!(simulatorAlgMaxPolicy.maxPolicy.id in simulatorsByPolicies)) {
-              simulatorsByPolicies[simulatorAlgMaxPolicy.maxPolicy.id] = {
-                policy: simulatorAlgMaxPolicy.maxPolicy,
-                simulators: [],
-              }
-            }
-
-            simulatorsByPolicies[simulatorAlgMaxPolicy.maxPolicy.id].simulators.push({
-              id: simulatorAlgMaxPolicy.id,
-              name: this.simulatorSpecsMap?.[simulatorAlgMaxPolicy.id]?.name as string,
-              url: 'https://biosimulators.org/simulators/' + simulatorAlgMaxPolicy.id,
-              algorithms: simulatorAlgMaxPolicy.algorithms,
-            });
-          });
-
-          const altAlgsByPoliciesList = (Object.values(altAlgsByPolicies) as AltAlgPolicy[])
-            .filter((altAlgPolicy: AltAlgPolicy): boolean => {
-              return altAlgPolicy.policy.level > 1;
-            });
-          altAlgsByPoliciesList.sort((a: AltAlgPolicy, b: AltAlgPolicy): number => {
-            if (a.policy.level < b.policy.level) {
-              return -1;
-            } else if (a.policy.level > b.policy.level) {
-              return 1;
-            } else {
-              return 0;
-            }
-          });
-
-          altAlgsByPoliciesList.forEach((altAlgsByPoly: AltAlgPolicy): void => {
-            altAlgsByPoly.algorithms.sort((a: Algorithm, b: Algorithm): number => {
-              return a.name.localeCompare(b.name, undefined, { numeric: true });
-            });
-          });
-
-          const simulatorsByPoliciesList = (Object.values(simulatorsByPolicies) as SimulatorAlgPolicy[]);
-          simulatorsByPoliciesList.sort((a: SimulatorAlgPolicy, b: SimulatorAlgPolicy): number => {
-            if (a.policy.level < b.policy.level) {
-              return -1;
-            } else if (a.policy.level > b.policy.level) {
-              return 1;
-            } else {
-              return 0;
-            }
-          });
-
-          simulatorsByPoliciesList.forEach((simulatorsAlgPolicy: SimulatorAlgPolicy): void => {
-            simulatorsAlgPolicy.simulators.sort((a: Simulator, b: Simulator): number => {
-              return a.name.localeCompare(b.name, undefined, { numeric: true });
-            });
-          });
-
-          return {
-            altAlgsByPolicies: altAlgsByPoliciesList,
-            simulatorsByPolicies: simulatorsByPoliciesList,
-          };
-        } else {
+        if (!algSubstitutions) {
           this.snackBar.open(
-            'Sorry! We were unable to determine similar algorithms.',
+            'Sorry! We were unable to load information about the simularity among algorithms.',
             undefined,
             {
               duration: 5000,
@@ -259,9 +96,183 @@ export class SuggestSimulatorComponent implements OnInit, OnDestroy {
               verticalPosition: 'bottom',
             },
           );
-          return undefined;
+          return [];
         }
+
+        const algorithmsMap: {[id: string]: any} = {};
+        algSubstitutions.forEach((algSubstitution: AlgorithmSubstitution): void => {
+          algSubstitution.algorithms.forEach((algorithm: KisoaAlgorithm): void => {
+            if (!(algorithm.id in algorithmsMap)) {
+              algorithmsMap[algorithm.id] = {
+                algorithm: {
+                  id: algorithm.id,
+                  name: algorithm.name,
+                  url: EBI_KISAO_BASE_URL + algorithm.id,
+                },
+                altAlgorithms: {},
+                simulators: {},
+              }
+            }
+          });
+
+          const mainAlg = algSubstitution.algorithms[0];
+          const altAlg = algSubstitution.algorithms[1]
+          const subPolicy = algSubstitution.maxPolicy;
+
+          // algorithm substitution
+          if (!(subPolicy.level in algorithmsMap[mainAlg.id].altAlgorithms)) {
+            algorithmsMap[mainAlg.id].altAlgorithms[subPolicy.level] = {
+              maxPolicy: subPolicy,
+              algorithms: {},
+            }
+          }
+          if (!(subPolicy.level in algorithmsMap[altAlg.id].altAlgorithms)) {
+            algorithmsMap[altAlg.id].altAlgorithms[subPolicy.level] = {
+              maxPolicy: subPolicy,
+              algorithms: {},
+            }
+          }
+
+          algorithmsMap[mainAlg.id].altAlgorithms[subPolicy.level].algorithms[altAlg.id] = {
+            id: altAlg.id,
+            name: altAlg.name,
+            url: EBI_KISAO_BASE_URL + altAlg.id,
+          }
+          algorithmsMap[altAlg.id].altAlgorithms[subPolicy.level].algorithms[mainAlg.id] = {
+            id: mainAlg.id,
+            name: mainAlg.name,
+            url: EBI_KISAO_BASE_URL + mainAlg.id,
+          }
+
+          // simulators
+          const mainAlgSimulators = simulationAlgorithmsMap[mainAlg.id].simulators;
+
+          mainAlgSimulators.forEach((simulator: string): void => {
+            // implementation of main algorithm
+            algorithmsMap[mainAlg.id].simulators[simulator] = {
+              maxPolicy: {
+                id: 'SAME_METHOD',
+                name: 'Same method',
+                level: AlgorithmSubstitutionPolicyLevels.SAME_METHOD,
+              },
+              simulatorAlgorithms: {
+                id: simulator,
+                name: simulatorSpecsMap[simulator].name,
+                url: 'https://biosimulators.org/simulators/' + simulator,
+                algorithms: {},
+              }
+            };
+            algorithmsMap[mainAlg.id].simulators[simulator].simulatorAlgorithms.algorithms[mainAlg.id] = {
+              id: mainAlg.id,
+              name: mainAlg.name,
+              url: EBI_KISAO_BASE_URL + mainAlg.id,
+            };
+
+            // alt implementations
+            if (!(simulator in algorithmsMap[altAlg.id].simulators)) {
+              algorithmsMap[altAlg.id].simulators[simulator] = {
+                maxPolicy: subPolicy,
+                simulatorAlgorithms: {
+                  id: simulator,
+                  name: simulatorSpecsMap[simulator].name,
+                  url: 'https://biosimulators.org/simulators/' + simulator,
+                  algorithms: {},
+                }
+              };
+            }
+
+            if (subPolicy.level < algorithmsMap[altAlg.id].simulators[simulator].maxPolicy.level) {
+              algorithmsMap[altAlg.id].simulators[simulator].maxPolicy = subPolicy;
+              algorithmsMap[altAlg.id].simulators[simulator].simulatorAlgorithms.algorithms = {};
+            }
+            if (subPolicy.level <= algorithmsMap[altAlg.id].simulators[simulator].maxPolicy.level) {
+              algorithmsMap[altAlg.id].simulators[simulator].simulatorAlgorithms.algorithms[mainAlg.id]  = {
+                id: mainAlg.id,
+                name: mainAlg.name,
+                url: EBI_KISAO_BASE_URL + mainAlg.id,
+              };
+            }
+          });
+        });
+
+        const algorithms = Object.values(algorithmsMap)
+        algorithms.sort((a: AlgorithmData, b: AlgorithmData): number => {
+          return a.algorithm.name.localeCompare(b.algorithm.name, undefined, { numeric: true });
+        });
+
+        algorithms.forEach((algorithmData: AlgorithmData): void => {
+          // algorithms
+          algorithmData.altAlgorithms = Object.values(algorithmData.altAlgorithms)
+            .filter((algPolicy: AlgorithmPolicy): boolean => {
+              return algPolicy.maxPolicy.level > 1;
+            });
+          algorithmData.altAlgorithms.sort((a: AlgorithmPolicy, b: AlgorithmPolicy): number => {
+            if (a.maxPolicy.level < b.maxPolicy.level) {
+              return -1;
+            } else if (a.maxPolicy.level > b.maxPolicy.level) {
+              return 1;
+            } else {
+              return 0;
+            }
+          });
+          algorithmData.altAlgorithms.forEach((algPolicy: AlgorithmPolicy): void => {
+            algPolicy.algorithms = Object.values(algPolicy.algorithms);
+            algPolicy.algorithms.sort((a: Algorithm, b: Algorithm): number => {
+              return a.name.localeCompare(b.name, undefined, { numeric: true });
+            });
+          })
+
+          // implementations
+          const simulatorPolicices: any = {};
+          Object.values(algorithmData.simulators).forEach((simulatorPolicyAlgs: any): void => {
+            if (!(simulatorPolicyAlgs.maxPolicy.level in simulatorPolicices)) {
+              simulatorPolicices[simulatorPolicyAlgs.maxPolicy.level] = {
+                maxPolicy: simulatorPolicyAlgs.maxPolicy,
+                simulators: [],
+              }
+            }
+            simulatorPolicices[simulatorPolicyAlgs.maxPolicy.level].simulators.push(simulatorPolicyAlgs.simulatorAlgorithms);
+            simulatorPolicyAlgs.simulatorAlgorithms.algorithms = Object.values(simulatorPolicyAlgs.simulatorAlgorithms.algorithms)
+          });
+
+          algorithmData.simulators = Object.values(simulatorPolicices);
+          algorithmData.simulators.sort((a: SimulatorPolicy, b: SimulatorPolicy): number => {
+            if (a.maxPolicy.level < b.maxPolicy.level) {
+              return -1;
+            } else if (a.maxPolicy.level > b.maxPolicy.level) {
+              return 1;
+            } else {
+              return 0;
+            }
+          });
+
+          algorithmData.simulators.forEach((simulatorPolicy: SimulatorPolicy): void => {
+            simulatorPolicy.simulators.sort((a: Simulator, b: Simulator): number => {
+              return a.name.localeCompare(b.name, undefined, { numeric: true });
+            });
+            simulatorPolicy.simulators.forEach((simulator: Simulator): void => {
+              simulator.algorithms.sort((a: Algorithm, b: Algorithm): number => {
+                return a.name.localeCompare(b.name, undefined, { numeric: true });
+              });
+            })
+          })
+        })
+
+        this.algorithmsMap = algorithmsMap;
+
+        const simulationAlgorithm = params?.simulationAlgorithm;
+        if (simulationAlgorithm && simulationAlgorithm in algorithmsMap) {
+          this.formGroup.controls.algorithm.setValue(simulationAlgorithm)
+        }
+
+        this.formGroup.controls.algorithm.enable();
+        return algorithms;
       })
     );
+  }
+
+  getAlgorithmSimulatorSuggestions(): void {
+    const selectedAlgorithmId = this.formGroup.controls.algorithm.value;
+    this.suggestions = this.algorithmsMap?.[selectedAlgorithmId];
   }
 }
