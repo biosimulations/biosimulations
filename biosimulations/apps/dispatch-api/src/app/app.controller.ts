785ad27f1477ac6122c2a735cb46201928a0f754
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-05-11 15:16:51 -0400

    feat (dispatch-api): remove download endpoint
    BREAKING CHANGE: The /download endpoint has been removed. Should be replaced by /results/download

--- biosimulations/apps/dispatch-api/src/app/app.controller.ts
+++ biosimulations/apps/dispatch-api/src/app/app.controller.ts
@@ -1,37 +1,10 @@
-import { ConfigService } from '@nestjs/config';
-import { AppService } from './app.service';
 import { ClientProxy } from '@nestjs/microservices';
-import {
-  Controller,
-  Inject,
-  OnApplicationBootstrap,
-  Get,
-  Param,
-  Res,
-} from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-
+import { Controller, Inject, OnApplicationBootstrap } from '@nestjs/common';
 @Controller()
 export class AppController implements OnApplicationBootstrap {
   public constructor(
     @Inject('NATS_CLIENT') private messageClient: ClientProxy,
-    private appService: AppService,
-
-    private configService: ConfigService,
   ) {}
-
-  @ApiTags('Downloads')
-  @Get('download/result/:uuid')
-  @ApiOperation({ deprecated: false, summary: 'Downloads result files' })
-  @ApiResponse({
-    status: 200,
-    description: 'Download all results as zip archive',
-    type: Object,
-  })
-  public resultArchive(@Param('uuid') uId: string, @Res() res: any): void {
-    const path = this.appService.downloadResultArchive(uId);
-    res.download(path);
-  }
 
   public async onApplicationBootstrap(): Promise<void> {
     await this.messageClient.connect();
