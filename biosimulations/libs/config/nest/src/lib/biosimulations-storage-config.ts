7484e06c8394adc8c57ebec2f8596d5f0155ce61
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-05-11 15:15:29 -0400

    feat (config): include public endpoint for storage

--- biosimulations/libs/config/nest/src/lib/biosimulations-storage-config.ts
+++ biosimulations/libs/config/nest/src/lib/biosimulations-storage-config.ts
@@ -6,6 +6,7 @@ export default registerAs('storage', () => {
     accessKey: process.env.STORAGE_ACCESS_KEY,
     secret: process.env.STORAGE_SECRET,
     bucket: process.env.STORAGE_BUCKET || 'biosimdev',
+    publicEndpoint: process.env.STORAGE_PUBLIC_ENDPOINT || "https://files-dev.biosimulations.org"
   };
   return config;
 });
