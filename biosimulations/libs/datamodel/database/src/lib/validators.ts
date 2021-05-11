2c103e08e9982d84988d515db4e467929edc660d
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-05-11 16:56:53 -0400

    feat (database): isUrl checks for valid protocol

--- biosimulations/libs/datamodel/database/src/lib/validators.ts
+++ biosimulations/libs/datamodel/database/src/lib/validators.ts
@@ -8,7 +8,8 @@ export const isEmail = {
 
 export const isUrl = {
   validator: (value: any): boolean => {
-    return value == null || validator.isURL(value);
+    return value == null || validator.isURL(value, { require_protocol: true });
   },
-  message: (props: any): string => `${props.value} is not a valid email`,
+  message: (props: any): string =>
+    `${props.value} is not a valid url. The url must begin with a valid protocol such as 'https://'`,
 };
