dfdd04987299ee27201b0a90224a89919a2f4a3b
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-05-11 15:13:43 -0400

    feat (storage): include correct location for uploads

--- biosimulations/libs/shared/storage/src/lib/shared-storage.service.ts
+++ biosimulations/libs/shared/storage/src/lib/shared-storage.service.ts
@@ -6,11 +6,15 @@ import * as AWS from 'aws-sdk';
 @Injectable()
 export class SharedStorageService {
   private BUCKET: string;
+  private PUBLIC_ENDPOINT: string;
   public constructor(
     @InjectS3() private readonly s3: S3,
     private configService: ConfigService,
   ) {
     this.BUCKET = configService.get('storage.bucket') || 'biosimdev';
+    this.PUBLIC_ENDPOINT =
+      configService.get('storage.publicEndpoint') ||
+      'files-dev.biosimulations.org';
     s3.config.update({ region: 'us-east-1' });
   }
 
@@ -36,9 +40,10 @@ export class SharedStorageService {
       Body: data,
       Bucket: this.BUCKET,
     };
+    const public_url = this.PUBLIC_ENDPOINT + id;
     const call = this.s3.upload(request);
     const res = await call.promise();
-
+    res.Location = public_url;
     return res;
   }
 }
