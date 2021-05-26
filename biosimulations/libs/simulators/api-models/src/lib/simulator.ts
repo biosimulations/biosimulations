d53416b855a52b1be123b42a4cfbe8ef72aa19a0
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-05-26 14:03:54 -0400

    fix (simulators-api): don't overwrite created date

--- biosimulations/libs/simulators/api-models/src/lib/simulator.ts
+++ biosimulations/libs/simulators/api-models/src/lib/simulator.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
 import { ExternalReferences, Person, Url } from '@biosimulations/datamodel/api';
 import {
   SoftwareInterfaceType,
