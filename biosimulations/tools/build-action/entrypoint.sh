2ea66c2878166c1d7ae5252b1039e2cb21faaa00
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-05-27 18:18:41 -0400

    try switching to docker

---
+++ biosimulations/tools/build-action/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -l
+echo building app ${APP} for env ${ENVNAME}
+mv libs/shared/environments/src/lib/environment.${ENVNAME}.ts libs/shared/environments/src/lib/environment.prod.ts nx build ${APP} --prod
+exit 1
