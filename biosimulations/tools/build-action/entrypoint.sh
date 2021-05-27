2a128107a75ca0bd41ce54636265db65874af8ae
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-05-27 19:15:19 -0400

    add python deps

--- biosimulations/tools/build-action/entrypoint.sh
+++ biosimulations/tools/build-action/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 echo running
 echo building app $1 for env $2
-cd /github/workspace/biosimulations
+cd biosimulations
 cp libs/shared/environments/src/lib/environment.$2.ts libs/shared/environments/src/lib/environment.prod.ts
 npm ci 
 npm run nx build $1 --prod
