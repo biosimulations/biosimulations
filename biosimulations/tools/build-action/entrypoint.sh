ce6758b8bb766a63e9f0b2674699b47ae453afc6
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-05-27 19:02:11 -0400

    debug docker ccreation

--- biosimulations/tools/build-action/entrypoint.sh
+++ biosimulations/tools/build-action/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
+echo running
 echo building app ${APP} for env ${ENVNAME}
 mv libs/shared/environments/src/lib/environment.${ENVNAME}.ts libs/shared/environments/src/lib/environment.prod.ts nx build ${APP} --prod
 
