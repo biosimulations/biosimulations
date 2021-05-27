7d4afa61092f41f47a7239214065e8b458fec8f0
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-05-27 19:10:41 -0400

    use path

--- biosimulations/tools/build-action/entrypoint.sh
+++ biosimulations/tools/build-action/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 echo running
 echo building app $1 for env $2
-cd biosimulations
+cd /github/workspace/biosimulations
 cp libs/shared/environments/src/lib/environment.$2.ts libs/shared/environments/src/lib/environment.prod.ts
 npm ci 
 npm run nx build $1 --prod
