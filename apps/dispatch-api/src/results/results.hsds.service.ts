cb97b5f3fcf38be98fd64d872e6db4107780baca
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-06-10 14:49:42 -0400

    implement data transformation

--- apps/dispatch-api/src/results/results.hsds.service.ts
+++ apps/dispatch-api/src/results/results.hsds.service.ts
@@ -1,16 +1,84 @@
 import { SimulationHDFService } from '@biosimulations/hsds/client';
 import { SharedStorageService } from '@biosimulations/shared/storage';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class HSDSResultsService {
   public async getResults(simId: string) {
+    const timestamps = this.results.getResultsTimestamps(simId);
     const datasets = await this.results.getDatasets(simId);
-    const metadata = await this.results.getMetadata(simId);
-    const data = {
-      metadata,
+
+    const results = [];
+
+    type report = {
+      id: string;
+      label: string;
+      shape: string;
+      type: string;
+      name: string;
     };
-    return { ...data, reports: datasets };
+    datasets.forEach((dataset) => {
+      const data: report[] = [];
+      const sedIds = dataset.attributes.sedmlDataSetIds || [];
+      const sedLabels = dataset.attributes.sedmlDataSetLabels || [];
+      const sedShapes = dataset.attributes.sedmlDataSetShapes || [];
+      const sedTypes = dataset.attributes.sedmlDataSetDataTypes || [];
+      const sedNames = dataset.attributes.sedmlDataSetNames || [];
+      console.log(dataset.attributes);
+
+      const len1 = sedIds.length;
+      const len2 = sedLabels.length;
+      const len3 = sedShapes.length;
+      const len4 = sedTypes.length;
+      const len5 = sedNames.length;
+      const consistent =
+        sedIds.length == sedLabels.length &&
+        sedIds.length == sedShapes.length &&
+        sedIds.length == sedTypes.length &&
+        sedIds.length == sedNames.length;
+
+      if (!consistent) {
+        console.error(
+          'Cannot process file due to inconsistent annotation sizes',
+        );
+        console.error(len1);
+        console.error(len2);
+        console.error(len3);
+        console.error(len4);
+        console.error(len5);
+      } else {
+        sedIds.forEach((sedId, index) => {
+          const report = {
+            id: sedId,
+            label: sedLabels[index],
+            shape: sedShapes[index],
+            type: sedTypes[index],
+            name: sedNames[index],
+          };
+          data.push(report);
+        });
+      }
+
+      const result = {
+        simId,
+        reportId: dataset.attributes.uri,
+        created: dataset.created,
+        updated: dataset.updated,
+        sedmlId: dataset.attributes.sedmlId,
+        sedmlName: dataset.attributes.sedmlName,
+        type: dataset.attributes._type,
+        data,
+      };
+
+      results.push(result);
+    });
+
+    return {
+      simId,
+      created: (await timestamps).created,
+      updated: (await timestamps).updated,
+      reports: results,
+    };
   }
   public constructor(
     private storage: SharedStorageService,
--- libs/hsds/client/src/lib/dataset.service.ts
+++ libs/hsds/client/src/lib/dataset.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import {
   AttributeService,
   DatasetService,
@@ -10,32 +10,47 @@ import {
   LinkService,
 } from '@biosimulations/hdf5apiclient';
 
-type BiosimulationsDataAttribute =
-  | '_type'
-  | 'uri'
-  | 'sedmlDataSetDataTypes'
-  | 'sedmlDataSetIds'
-  | 'sedmlDataSetLabels'
-  | 'sedmlDataSetNames'
-  | 'sedmlDataSetShapes'
-  | 'sedmlId'
-  | 'sedmlName';
-
+type BiosimulationsDataAtributes = {
+  _type: string;
+  uri: string;
+  sedmlId: string;
+  sedmlName: string;
+  sedmlDataSetDataTypes: string[];
+  sedmlDataSetIds: string[];
+  sedmlDataSetLabels: string[];
+  sedmlDataSetNames: string[];
+  sedmlDataSetShapes: string[];
+};
+const ATTRIBUTES: (keyof BiosimulationsDataAtributes)[] = [
+  '_type',
+  'sedmlId',
+  'sedmlName',
+  'sedmlDataSetDataTypes',
+  'sedmlDataSetIds',
+  'sedmlDataSetNames',
+  'sedmlDataSetShapes',
+  'sedmlDataSetLabels',
+];
 const ACCEPT = 'application/json';
 const DATASET = 'datasets';
 const AUTH = undefined;
 const GROUP = 'groups';
 Injectable();
 export class SimulationHDFService {
-  public async getMetadata(simId: string) {
+  private logger = new Logger(SimulationHDFService.name);
+  public async getResultsTimestamps(simId: string) {
     const domain = simId + '.results';
     const root_id = await this.getRootGroupId(domain);
     if (root_id) {
       const metadata = await this.getGroup(domain, root_id);
-      return metadata;
-    } else {
-      return undefined;
+      if (metadata.created && metadata.lastModified) {
+        return {
+          created: new Date(metadata.created * 1000),
+          updated: new Date(metadata.lastModified * 1000),
+        };
+      }
     }
+    return { created: undefined, updated: undefined };
   }
 
   public constructor(
@@ -56,25 +71,28 @@ export class SimulationHDFService {
     const datasetIds = response.data.datasets || [];
     const datasets = [];
     for (const datasetId of datasetIds) {
-      const dataset = await this.getDataset(domain, datasetId);
-      const attributeNames = await this.getDatasetAttributeIds(
-        domain,
-        datasetId,
-      );
-      const attributes: { [name in BiosimulationsDataAttribute]?: string } = {};
+      const dataset = this.getDataset(domain, datasetId);
+      let attributeNames = await this.getDatasetAttributeIds(domain, datasetId);
+      const logger = new Logger('test');
+      logger.error(attributeNames);
+      attributeNames = ATTRIBUTES;
+      logger.error(attributeNames);
+      const attributes = {};
       for (const attribute of attributeNames) {
         const value = await this.getDatasetAttributeValue(
           domain,
           datasetId,
           attribute,
         );
+
         attributes[attribute] = value;
       }
+
       datasets.push({
         uri: attributes['uri'],
         id: datasetId,
-        created: dataset.created,
-        updated: dataset.lastModified,
+        created: (await dataset).created,
+        updated: (await dataset).lastModified,
         attributes: attributes,
       });
     }
@@ -114,30 +132,39 @@ export class SimulationHDFService {
   private async getDatasetAttributeIds(
     domain: string,
     datasetId: string,
-  ): Promise<BiosimulationsDataAttribute[]> {
+  ): Promise<(keyof BiosimulationsDataAtributes)[]> {
     const response = await this.attrbuteService
       .collectionObjUuidAttributesGet(ACCEPT, DATASET, datasetId, AUTH, domain)
       .toPromise();
     const attributes = response.data.attributes || [];
-    return attributes.map((value) => value.name as BiosimulationsDataAttribute);
+    return attributes.map(
+      (value) => value.name as keyof BiosimulationsDataAtributes,
+    );
   }
 
   private async getDatasetAttributeValue(
     domain: string,
     datasetId: string,
-    attribute: BiosimulationsDataAttribute,
-  ): Promise<string | undefined> {
-    const uriResponse = await this.attrbuteService
-      .collectionObjUuidAttributesAttrGet(
-        ACCEPT,
-        DATASET,
-        datasetId,
-        attribute,
-        AUTH,
-        domain,
-      )
-      .toPromise();
-    return uriResponse.data.value;
+    attribute: keyof BiosimulationsDataAtributes,
+  ): Promise<(string & string[]) | undefined> {
+    try {
+      const uriResponse = await this.attrbuteService
+        .collectionObjUuidAttributesAttrGet(
+          ACCEPT,
+          DATASET,
+          datasetId,
+          attribute,
+          AUTH,
+          domain,
+        )
+        .toPromise();
+
+      // This must be hillariously wrong. I am unsure how to get this typed properly. We know the names of the attributes, and the types of each value.
+      return uriResponse.data.value as string & string[];
+    } catch (e) {
+      this.logger.error('error for attribute' + attribute);
+      return 'Testname';
+    }
   }
   private async getRootGroupId(domain: string): Promise<string | undefined> {
     const domainResponse = await this.domainService
