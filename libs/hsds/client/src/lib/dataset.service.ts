5e772f2cf160ac81945aeea968ce2095855e1124
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-06-10 11:30:31 -0400

    add implementation for getting data from hsds

--- libs/hsds/client/src/lib/dataset.service.ts
+++ libs/hsds/client/src/lib/dataset.service.ts
@@ -4,14 +4,40 @@ import {
   DatasetService,
   DomainService,
   GroupService,
-  InlineResponse2003,
-  InlineResponse2004Links,
+  InlineResponse2007 as HDF5Dataset,
+  InlineResponse2003 as HDF5Group,
+  InlineResponse2004Links as HDF5Links,
   LinkService,
 } from '@biosimulations/hdf5apiclient';
 
+type BiosimulationsDataAttribute =
+  | '_type'
+  | 'uri'
+  | 'sedmlDataSetDataTypes'
+  | 'sedmlDataSetIds'
+  | 'sedmlDataSetLabels'
+  | 'sedmlDataSetNames'
+  | 'sedmlDataSetShapes'
+  | 'sedmlId'
+  | 'sedmlName';
+
 const ACCEPT = 'application/json';
+const DATASET = 'datasets';
+const AUTH = undefined;
+const GROUP = 'groups';
 Injectable();
 export class SimulationHDFService {
+  public async getMetadata(simId: string) {
+    const domain = simId + '.results';
+    const root_id = await this.getRootGroupId(domain);
+    if (root_id) {
+      const metadata = await this.getGroup(domain, root_id);
+      return metadata;
+    } else {
+      return undefined;
+    }
+  }
+
   public constructor(
     @Inject(DatasetService) private datasetService: DatasetService,
     @Inject(DomainService) private domainService: DomainService,
@@ -20,13 +46,99 @@ export class SimulationHDFService {
     @Inject(LinkService) private linkService: LinkService,
   ) {}
 
-  private async getLinks(groupId: string, domain: string) {
+  public async getDatasets(simId: string) {
+    const domain = this.getDomainName(simId);
+
+    const response = await this.domainService
+      .datasetsGet('application/json', domain)
+      .toPromise();
+
+    const datasetIds = response.data.datasets || [];
+    const datasets = [];
+    for (const datasetId of datasetIds) {
+      const dataset = await this.getDataset(domain, datasetId);
+      const attributeNames = await this.getDatasetAttributeIds(
+        domain,
+        datasetId,
+      );
+      const attributes: { [name in BiosimulationsDataAttribute]?: string } = {};
+      for (const attribute of attributeNames) {
+        const value = await this.getDatasetAttributeValue(
+          domain,
+          datasetId,
+          attribute,
+        );
+        attributes[attribute] = value;
+      }
+      datasets.push({
+        uri: attributes['uri'],
+        id: datasetId,
+        created: dataset.created,
+        updated: dataset.lastModified,
+        attributes: attributes,
+      });
+    }
+
+    return datasets;
+  }
+
+  private getDomainName(simId: string): string {
+    return simId + '.results';
+  }
+
+  private async getGroup(domain: string, id: string): Promise<HDF5Group> {
+    const response = await this.groupService
+      .groupsIdGet(id, ACCEPT, domain)
+      .toPromise();
+    const data = response.data;
+    return data;
+  }
+
+  private async getDataset(domain: string, id: string): Promise<HDF5Dataset> {
+    const response = await this.datasetService
+      .datasetsIdGet(id, ACCEPT, domain)
+      .toPromise();
+    return response.data;
+  }
+
+  private async getLinks(
+    groupId: string,
+    domain: string,
+  ): Promise<HDF5Links[]> {
     const linksResponse = await this.linkService
       .groupsIdLinksGet(groupId, ACCEPT, domain)
       .toPromise();
-    return linksResponse.data.links;
+    return linksResponse.data.links || [];
   }
 
+  private async getDatasetAttributeIds(
+    domain: string,
+    datasetId: string,
+  ): Promise<BiosimulationsDataAttribute[]> {
+    const response = await this.attrbuteService
+      .collectionObjUuidAttributesGet(ACCEPT, DATASET, datasetId, AUTH, domain)
+      .toPromise();
+    const attributes = response.data.attributes || [];
+    return attributes.map((value) => value.name as BiosimulationsDataAttribute);
+  }
+
+  private async getDatasetAttributeValue(
+    domain: string,
+    datasetId: string,
+    attribute: BiosimulationsDataAttribute,
+  ): Promise<string | undefined> {
+    const uriResponse = await this.attrbuteService
+      .collectionObjUuidAttributesAttrGet(
+        ACCEPT,
+        DATASET,
+        datasetId,
+        attribute,
+        AUTH,
+        domain,
+      )
+      .toPromise();
+    return uriResponse.data.value;
+  }
   private async getRootGroupId(domain: string): Promise<string | undefined> {
     const domainResponse = await this.domainService
       .rootGet(ACCEPT, domain)
@@ -36,82 +148,5 @@ export class SimulationHDFService {
 
     const rootGroup = domainInfo.root;
     return rootGroup;
-  }
-  public async getGroups(simId: string) {
-    const domain = simId + '.results';
-
-    const rootGroup = (await this.getRootGroupId(domain)) || '';
-    this.linkService.groupsIdLinksLinknameGet;
-    const rootGroupResponse = this.groupService
-      .groupsIdGet(rootGroup, ACCEPT, domain)
-      .toPromise();
-    const groupsResponse = await this.domainService
-      .groupsGet(ACCEPT, domain)
-      .toPromise();
-
-    const groups = groupsResponse.data.groups || [];
-    const returned_groups = [];
-    for (const group of groups) {
-      const group_name = await this.getGroupName(group, domain);
-      const linksResponse = await this.linkService
-        .groupsIdLinksGet(group, ACCEPT, domain)
-        .toPromise();
-      const links = linksResponse.data.links;
-      const return_group: {
-        [key: string]: {
-          links: InlineResponse2004Links[] | undefined;
-          info: InlineResponse2003;
-        };
-      } = {};
-      return_group[group] = { links: links, info: group_name };
-      returned_groups.push(return_group);
-    }
-
-    return returned_groups;
-  }
-  async getGroupName(group: string, domain: string) {
-    const response = await this.groupService
-      .groupsIdGet(group, ACCEPT, domain)
-      .toPromise();
-    return response.data;
-  }
-  async getDatasets(simId: string) {
-    const domain = simId + '.results';
-
-    const response = await this.domainService
-      .datasetsGet('application/json', domain)
-      .toPromise();
-    this.domainService.groupsPost;
-
-    const datasetIds = response.data.datasets || [];
-    const datasetNames = [];
-    for (const datasetId of datasetIds) {
-      const dataResponse = await this.datasetService
-        .datasetsIdGet(datasetId, 'application/json', domain)
-        .toPromise();
-      const datasetAttributes = await this.attrbuteService
-        .collectionObjUuidAttributesGet(
-          'application/json',
-          'datasets',
-          datasetId,
-          undefined,
-          domain,
-        )
-        .toPromise();
-      const datasetIdResponse = await this.attrbuteService
-        .collectionObjUuidAttributesAttrGet(
-          'application/json',
-          'datasets',
-          datasetId,
-          'id',
-          undefined,
-          domain,
-        )
-        .toPromise();
-      const datasetIdName = datasetIdResponse.data;
-      datasetNames.push(dataResponse.data);
-    }
-
-    return datasetNames;
   }
 }
