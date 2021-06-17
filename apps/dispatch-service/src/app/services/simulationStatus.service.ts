fd47b7ad85e19adb9e43aae33f7046d883e4bcd2
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-06-17 17:26:00 -0400

    refactor(dispatch-api, dispatch-service): Move messaging from dispatch-service to dispatch-api
    closes #2563

--- apps/dispatch-service/src/app/services/simulationStatus.service.ts
+++ apps/dispatch-service/src/app/services/simulationStatus.service.ts
@@ -1,19 +1,11 @@
 import { SimulationRunStatus } from '@biosimulations/datamodel/common';
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { SimulationRunService } from '@biosimulations/dispatch/nest-client';
-import { ClientProxy } from '@nestjs/microservices';
-import {
-  DispatchFailedPayload,
-  DispatchMessage,
-  DispatchProcessedPayload,
-} from '@biosimulations/messages/messages';
+
 @Injectable()
 export class SimulationStatusService {
   private logger = new Logger(SimulationStatusService.name);
-  public constructor(
-    private simService: SimulationRunService,
-    @Inject('NATS_CLIENT') private client: ClientProxy,
-  ) {}
+  public constructor(private simService: SimulationRunService) {}
   public updateStatus(
     simId: string,
     simStatus: SimulationRunStatus,
@@ -24,13 +16,6 @@ export class SimulationStatusService {
       .toPromise()
       .then((val) => {
         this.logger.log('Successfully updated simulation');
-        if (simStatus == SimulationRunStatus.FAILED) {
-          const message = new DispatchFailedPayload(simId);
-          this.client.emit(DispatchMessage.failed, message);
-        } else if (simStatus == SimulationRunStatus.SUCCEEDED) {
-          const message = new DispatchProcessedPayload(simId);
-          this.client.emit(DispatchMessage.processed, message);
-        }
       })
       .catch((err) => {
         this.logger.error('Failed to update status');
