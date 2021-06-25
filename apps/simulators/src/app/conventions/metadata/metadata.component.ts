2decdaafad7255055287e2e38f809e94d193a1af
Jonathan Karr <jonrkarr@gmail.com>
2021-06-25 12:08:28 -0400

    adding documentation about format for metadata (#2649)
    * adding documentation about format for metadata
    
    * updating to biosimulators-util 0.1.86
    
    * fixing OMEX meta files in examples
    
    * adding logging option for biosimulators-utils
    
    * debugging failing test

---
+++ apps/simulators/src/app/conventions/metadata/metadata.component.ts
@@ -0,0 +1,32 @@
+import { Component, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+  TocSection,
+  TocSectionsContainerDirective,
+} from '@biosimulations/shared/ui';
+import { ConfigService } from '@biosimulations/shared/services';
+import exampleMetadata from './example-metadata.json';
+import exampleFigureMetadata from './example-figure-metadata.json';
+
+@Component({
+  templateUrl: './metadata.component.html',
+  styleUrls: ['./metadata.component.sass'],
+})
+export class MetadataComponent {
+  tocSections!: Observable<TocSection[]>;
+
+  @ViewChild(TocSectionsContainerDirective)
+  set tocSectionsContainer(container: TocSectionsContainerDirective) {
+    setTimeout(() => {
+      this.tocSections = container.sections$;
+    });
+  }
+
+  exampleMetadata: string;
+  exampleFigureMetadata: string;
+
+  constructor(public config: ConfigService) {
+    this.exampleMetadata = exampleMetadata;
+    this.exampleFigureMetadata = exampleFigureMetadata;
+  }
+}
