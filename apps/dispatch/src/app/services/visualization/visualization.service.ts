dc54f14d6880f1503269b3cb76aaf7602cc81547
Biosimulations-Daemon <biosimulationsdaemon@gmail.com>
2021-07-01 16:37:57 +0000

    chore: formatted files. [skip ci]
    Signed-off-by: Biosimulations-Daemon <biosimulationsdaemon@gmail.com>

--- apps/dispatch/src/app/services/visualization/visualization.service.ts
+++ apps/dispatch/src/app/services/visualization/visualization.service.ts
@@ -212,36 +212,47 @@ export class VisualizationService {
 
 class RetryStrategy {
   constructor(
-    private maxAttempts=7,
-    private initialDelayMs=1000,
-    private scalingFactor=2,
-    private includedStatusCodes: number[]=[500],
-    private excludedStatusCodes: number[]=[],
-    private shouldErrorBeRetried: ((error: HttpErrorResponse) => boolean) = (error: HttpErrorResponse) => true,
+    private maxAttempts = 7,
+    private initialDelayMs = 1000,
+    private scalingFactor = 2,
+    private includedStatusCodes: number[] = [500],
+    private excludedStatusCodes: number[] = [],
+    private shouldErrorBeRetried: (error: HttpErrorResponse) => boolean = (
+      error: HttpErrorResponse,
+    ) => true,
   ) {}
 
- handler(attempts: Observable<any>) {
-   return attempts.pipe(
-    mergeMap((error: HttpErrorResponse, iRetryAttempt: number): Observable<number> => {
-      if ((iRetryAttempt + 1) >= this.maxAttempts) {
-        return throwError(error);
-      }
+  handler(attempts: Observable<any>) {
+    return attempts.pipe(
+      mergeMap(
+        (
+          error: HttpErrorResponse,
+          iRetryAttempt: number,
+        ): Observable<number> => {
+          if (iRetryAttempt + 1 >= this.maxAttempts) {
+            return throwError(error);
+          }
 
-      if (this.includedStatusCodes.length && !this.includedStatusCodes.includes(error.status)) {
-        return throwError(error);
-      }
+          if (
+            this.includedStatusCodes.length &&
+            !this.includedStatusCodes.includes(error.status)
+          ) {
+            return throwError(error);
+          }
 
-      if (this.excludedStatusCodes.includes(error.status)) {
-        return throwError(error);
-      }
+          if (this.excludedStatusCodes.includes(error.status)) {
+            return throwError(error);
+          }
 
-      if (!this.shouldErrorBeRetried(error)) {
-        return throwError(error);
-      }
+          if (!this.shouldErrorBeRetried(error)) {
+            return throwError(error);
+          }
 
-      const delay = this.initialDelayMs * (this.scalingFactor ** iRetryAttempt);
-      return timer(delay);
-    }),
-  );
- }
+          const delay =
+            this.initialDelayMs * this.scalingFactor ** iRetryAttempt;
+          return timer(delay);
+        },
+      ),
+    );
+  }
 }
