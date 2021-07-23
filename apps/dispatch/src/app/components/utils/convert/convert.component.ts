00665225875e75a62a93dd93fd545f8e823f9ecc
Jonathan Karr <jonrkarr@gmail.com>
2021-07-23 01:31:42 -0400

    feat(dispatch,simulators): added documentation about generating data visualizations

---
+++ apps/dispatch/src/app/components/utils/convert/convert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './convert.component.html',
+  styleUrls: ['./convert.component.scss'],
+})
+export class ConvertComponent {
+  constructor() {}
+}
