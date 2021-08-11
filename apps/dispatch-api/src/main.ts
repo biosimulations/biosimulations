453397e0f25577160f102fc323e0d90bab7b318f
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-08-11 13:48:41 -0400

    refactor(dispatch-api): switch to @nestjs/axios, seperate open api

--- apps/dispatch-api/src/main.ts
+++ apps/dispatch-api/src/main.ts
@@ -9,16 +9,12 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { CustomOrigin } from '@nestjs/common/interfaces/external/cors-options.interface';
-import {
-  ScopesObject,
-  SecuritySchemeObject,
-} from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
+
 import { ConfigService } from '@nestjs/config';
 import { json } from 'body-parser';
-import { Resolver } from '@stoplight/json-ref-resolver';
-import * as toJsonSchema from '@openapi-contrib/openapi-schema-to-json-schema';
+import { setupOpenApi } from './openApi';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -45,7 +41,7 @@ async function bootstrap() {
       'https://biosimulations.org',
       'https://run.biosimulations.dev',
       'https://run.biosimulations.org',
-      'https://bio.libretexts.org',
+      'https://bio.libretexts.org', // Libre text collaboration
       'https://vega.github.io', // Vega editor
       'http://idl.cs.washington.edu', // Lyra Vega visual editor
     ];
@@ -54,142 +50,12 @@ async function bootstrap() {
     callback(error, allow);
   };
   app.enableCors({ origin: allowOrigin });
-  const favIcon =
-    'https://github.com/biosimulations/Biosimulations/raw/dev/biosimulations/libs/shared/assets/src/assets/icons/favicon-32x32.png';
-  const removeIcon = ' .swagger-ui .topbar { display: none }';
-  // Swagger doc
-  const tags = [
-    {
-      name: 'Simulation runs',
-      description:
-        'Operations for submitting a simulation run, checking the status of a run, modifying the details of a run, and deleting a run.',
-    },
-    {
-      name: 'Results',
-      description:
-        'Operations for viewing and retrieving the results of a simulation run.',
-    },
-    {
-      name: 'Downloads',
-      description:
-        'Operations for downloading the results of a simulation run.',
-    },
-    {
-      name: 'Logs',
-      description:
-        'Operations for submitting and retrieving a log of the execution of a simulation run.',
-    },
-    {
-      name: 'Authentication testing',
-      description:
-        "Operations for checking whether a user is logged in and retrieving information about a user's privileges.",
-    },
-    {
-      name: 'Internal management',
-      description:
-        'Operations for the management of runBioSimulations by the runBioSimulations Team.',
-    },
-  ];
-  const builder = new DocumentBuilder()
-    .setTitle('runBioSimulations API')
-    .setDescription(
-      'API for submiting and managing simulation jobs to the runBioSimulations simulation service.',
-    )
-    .setVersion('0.1')
-    .setLicense(
-      'MIT License',
-      'https://github.com/biosimulations/Biosimulations/blob/dev/LICENSE',
-    )
-    .setTermsOfService('https://run.biosimulations.org/help/terms')
-    .setExternalDoc(
-      'API specifications (Open API JSON)',
-      'https://run.api.biosimulations.org/openapi.json',
-    )
-    .setContact(
-      'runBioSimulations Team',
-      'https://run.biosimulations.org/help/about',
-      'info@biosimulations.org',
-    );
-
-  for (const tag of tags) {
-    builder.addTag(tag.name, tag.description);
-  }
-
-  const scopes: ScopesObject = {
-    'read:SimulationRuns': 'Get information about a submitted run',
-    'write:SimulationRuns': 'Modify a run, including status',
-    'delete:SimulationsRuns': 'Delete runs from the database',
-    // TODO add all scopes/find a way to automate this from auth0
-  };
-  const authorizationUrl =
-    'https://auth.biosimulations.org/authorize?audience=dispatch.biosimulations.org';
-  const openIdConnectUrl =
-    'https://auth.biosimulations.org/.well-known/openid-configuration';
-  const clientId = 'pMatIe0TqLPbnXBn6gcDjdjnpIrlKG3a';
-
-  const oauthSchema: SecuritySchemeObject = {
-    type: 'oauth2',
-    flows: {
-      implicit: {
-        authorizationUrl: authorizationUrl,
-        scopes: scopes,
-      },
-    },
-  };
-
-  builder.addOAuth2(oauthSchema);
-
-  const openIDSchema: SecuritySchemeObject = {
-    type: 'openIdConnect',
-    openIdConnectUrl: openIdConnectUrl,
-  };
-
-  builder.addSecurity('OpenIdc', openIDSchema);
-
-  const options = builder.build();
-  const document = SwaggerModule.createDocument(app, options);
-
-  const components = document.components as any;
-  const unsortedSchemas = components.schemas;
-  if (unsortedSchemas) {
-    const schemaNames = Object.keys(unsortedSchemas).sort();
-
-    const schemas: { [name: string]: any } = {};
-    for (const schemaName of schemaNames) {
-      schemas[schemaName] = unsortedSchemas?.[schemaName];
-    }
-    components.schemas = schemas;
-  }
-
-  SwaggerModule.setup('', app, document, {
-    customfavIcon: favIcon,
-    customSiteTitle: 'runBioSimulations API',
-    customCss: removeIcon,
-    swaggerOptions: {
-      oauth: {
-        clientId: clientId,
-      },
-      //tagsSorter: 'alpha',
-      operationsSorter: 'alpha',
-    },
-  });
-
-  const httpAdapter = app.getHttpAdapter();
-
-  httpAdapter.get('/openapi.json', (req, res) => res.json(document));
-
-  const resolver = new Resolver();
-  const resolvedDocument = await resolver.resolve(document);
-  const schema = resolvedDocument.result.components.schemas.CombineArchiveLog;
-  httpAdapter.get('/schema/CombineArchiveLog.json', (req, res) =>
-    res.json(toJsonSchema(schema)),
-  );
 
   const configService = app.get(ConfigService);
   const limit = configService.get('server.limit');
   app.use(json({ limit }));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-
+  setupOpenApi(app);
   await app.listen(port, () => {
     logger.log('Listening at http://localhost:' + port);
   });
---
+++ apps/dispatch-api/src/openApi.ts
@@ -0,0 +1,145 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Resolver } from '@stoplight/json-ref-resolver';
+import * as toJsonSchema from '@openapi-contrib/openapi-schema-to-json-schema';
+import {
+  ScopesObject,
+  SecuritySchemeObject,
+} from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
+import { INestApplication } from '@nestjs/common';
+
+export async function setupOpenApi(app: INestApplication): Promise<void> {
+  const favIcon =
+    'https://github.com/biosimulations/Biosimulations/raw/dev/libs/shared/assets/src/assets/icons/favicon-32x32.png';
+  const removeIcon = ' .swagger-ui .topbar { display: none }';
+  // Swagger doc
+  const tags = [
+    {
+      name: 'Simulation runs',
+      description:
+        'Operations for submitting a simulation run, checking the status of a run, modifying the details of a run, and deleting a run.',
+    },
+    {
+      name: 'Results',
+      description:
+        'Operations for viewing and retrieving the results of a simulation run.',
+    },
+    {
+      name: 'Downloads',
+      description:
+        'Operations for downloading the files and results of a simulation run.',
+    },
+    {
+      name: 'Logs',
+      description:
+        'Operations for submitting and retrieving a log of the execution of a simulation run.',
+    },
+    {
+      name: 'Internal management',
+      description:
+        'Operations for the management of runBioSimulations by the runBioSimulations Team.',
+    },
+  ];
+  const builder = new DocumentBuilder()
+    .setTitle('runBioSimulations API')
+    .setDescription(
+      'API for submiting and managing simulation jobs to the runBioSimulations simulation service.',
+    )
+    .setVersion('0.1')
+    .setLicense(
+      'MIT License',
+      'https://github.com/biosimulations/Biosimulations/blob/dev/LICENSE',
+    )
+    .setTermsOfService('https://run.biosimulations.org/help/terms')
+    .setExternalDoc(
+      'API specifications (Open API JSON)',
+      'https://run.api.biosimulations.org/openapi.json',
+    )
+    .setContact(
+      'runBioSimulations Team',
+      'https://run.biosimulations.org/help/about',
+      'info@biosimulations.org',
+    );
+
+  for (const tag of tags) {
+    builder.addTag(tag.name, tag.description);
+  }
+  // TODO add all scopes/find a way to automate this from auth0 or atleast create a common library to keep consistent
+  const scopes: ScopesObject = {
+    'read:SimulationRuns': 'Get information about a submitted run',
+    'write:SimulationRuns': 'Modify a run, including status',
+    'delete:SimulationsRuns': 'Delete runs from the database',
+    'read:Results': 'Get the results of a run',
+    'write:Results': 'Modify the results of a run',
+    'delete:Results': 'Delete the results of a run',
+    'read:Logs': 'Get the log of a run',
+    'write:Logs': 'Modify the log of a run',
+    'delete:Logs': 'Delete the log of a run',
+    'read:Metadata': 'Get the metadata for a run',
+    'write:Metadata': 'Modify the metadata for a run',
+    'delete:Metadata': 'Delete the metadata for a run',
+  };
+  const authorizationUrl =
+    'https://auth.biosimulations.org/authorize?audience=dispatch.biosimulations.org';
+  const openIdConnectUrl =
+    'https://auth.biosimulations.org/.well-known/openid-configuration';
+
+  const clientId = 'pMatIe0TqLPbnXBn6gcDjdjnpIrlKG3a';
+
+  const oauthSchema: SecuritySchemeObject = {
+    type: 'oauth2',
+    flows: {
+      implicit: {
+        authorizationUrl: authorizationUrl,
+        scopes: scopes,
+      },
+    },
+  };
+
+  builder.addOAuth2(oauthSchema);
+
+  const openIDSchema: SecuritySchemeObject = {
+    type: 'openIdConnect',
+    openIdConnectUrl: openIdConnectUrl,
+  };
+
+  builder.addSecurity('OpenIdc', openIDSchema);
+
+  const options = builder.build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  const components = document.components as any;
+  const unsortedSchemas = components.schemas;
+  if (unsortedSchemas) {
+    const schemaNames = Object.keys(unsortedSchemas).sort();
+
+    const schemas: { [name: string]: any } = {};
+    for (const schemaName of schemaNames) {
+      schemas[schemaName] = unsortedSchemas?.[schemaName];
+    }
+    components.schemas = schemas;
+  }
+
+  SwaggerModule.setup('', app, document, {
+    customfavIcon: favIcon,
+    customSiteTitle: 'runBioSimulations API',
+    customCss: removeIcon,
+    swaggerOptions: {
+      oauth: {
+        clientId: clientId,
+      },
+      //tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+
+  const httpAdapter = app.getHttpAdapter();
+
+  httpAdapter.get('/openapi.json', (req, res) => res.json(document));
+
+  const resolver = new Resolver();
+  const resolvedDocument = await resolver.resolve(document);
+  const schema = resolvedDocument.result.components.schemas.CombineArchiveLog;
+  httpAdapter.get('/schema/CombineArchiveLog.json', (req, res) =>
+    res.json(toJsonSchema(schema)),
+  );
+}
