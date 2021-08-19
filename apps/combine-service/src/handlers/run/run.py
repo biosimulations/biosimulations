c0cd6998dc850c5afbcee00fa2afec7390ffd235
Jonathan Karr <jonrkarr@gmail.com>
2021-08-19 15:41:42 -0400

    feat(combine-service): added options to export simulation results in HDF5, zip formats

--- apps/combine-service/src/handlers/run/run.py
+++ apps/combine-service/src/handlers/run/run.py
@@ -1,15 +1,29 @@
 from ...exceptions import BadRequestException
 from ...utils import get_temp_file, get_temp_dir
 from .utils import get_simulators, get_simulator_api
+from biosimulators_utils.config import get_config
+from biosimulators_utils.report.data_model import ReportFormat
 from biosimulators_utils.sedml.data_model import Report, Plot2D, Plot3D
 from biosimulators_utils.sedml.exec import get_report_for_plot2d, get_report_for_plot3d
 from biosimulators_utils.sedml.io import SedmlSimulationReader
+from biosimulators_utils.viz.data_model import VizFormat
 from unittest import mock
+import connexion
+import flask
 import os.path
 import requests
 import requests.exceptions
 import werkzeug.wrappers.response  # noqa: F401
 import zipfile
+
+IGNORED_ENV_VARS = [
+    'H5_REPORTS_PATH',
+    'REPORTS_PATH',
+    'PLOTS_PATH',
+    'BUNDLE_OUTPUTS',
+    'KEEP_INDIVIDUAL_OUTPUTS',
+    'LOG_PATH',
+]
 
 
 def handler(body, archiveFile=None):
@@ -28,6 +42,59 @@ def handler(body, archiveFile=None):
     archive_url = body.get('archiveUrl', None)
     simulator_id = body['simulator']
     env_vars = body.get('environment', {}).get('variables', [])
+
+    # set up environment (i.e. options)
+    env = {}
+    for env_var in env_vars:
+        key = env_var['key']
+        if key not in IGNORED_ENV_VARS:
+            env[key] = env_var['value']
+
+    if 'REPORT_FORMATS' not in env:
+        env['REPORT_FORMATS'] = 'h5'
+
+    # process requested return type
+    accept = connexion.request.headers.get('Accept', 'application/json')
+    if accept in ['application/json']:
+        return_results = True
+        report_formats = []
+        viz_formats = []
+        bundle_outputs = False
+        keep_individual_outputs = True
+        env['LOG_PATH'] = ''
+        return_type = 'json'
+
+    elif accept in ['application/x-hdf', 'application/x-hdf5']:
+        return_results = False
+        report_formats = [
+            ReportFormat[format.strip().lower()]
+            for format in env.get('REPORT_FORMATS', 'h5').split(',')
+        ]
+        viz_formats = []
+        bundle_outputs = False
+        keep_individual_outputs = True
+        env['LOG_PATH'] = ''
+        return_type = 'h5'
+
+    elif accept in ['application/zip']:
+        return_results = False
+        report_formats = [
+            ReportFormat[format.strip().lower()]
+            for format in env.get('REPORT_FORMATS', 'h5').split(',')
+        ]
+        viz_formats = [
+            VizFormat[format.strip().lower()]
+            for format in env.get('VIZ_FORMATS', 'pdf').split(',')
+        ]
+        bundle_outputs = False
+        keep_individual_outputs = True
+        return_type = 'zip'
+
+    else:
+        raise BadRequestException(
+            title='`Accept` header must be one of `application/hdf5`, `application/json`, or `application/zip`.',
+            instance=NotImplementedError(),
+        )
 
     # get the COMBINE/OMEX archive
     if archive_file and archive_url:
@@ -68,73 +135,90 @@ def handler(body, archiveFile=None):
 
     simulator_api = get_simulator_api(simulator['api']['module'])
 
-    # set up environment
-    env = {}
-    for env_var in env_vars:
-        env[env_var['key']] = env_var['value']
-
     # execute the simulation
     out_dir = get_temp_dir()
     with mock.patch.dict('os.environ', env):
         results, log = simulator_api.exec_sedml_docs_in_combine_archive(
             archive_filename, out_dir,
-            return_results=True, report_formats=[], plot_formats=[],
+            return_results=return_results, report_formats=report_formats, plot_formats=viz_formats,
+            bundle_outputs=bundle_outputs, keep_individual_outputs=keep_individual_outputs,
             raise_exceptions=False,
         )
 
     # transform the results
-    archive_dirname = get_temp_dir()
-    with zipfile.ZipFile(archive_filename, 'r') as zip_file:
-        zip_file.extractall(archive_dirname)
+    if return_type == 'json':
+        archive_dirname = get_temp_dir()
+        with zipfile.ZipFile(archive_filename, 'r') as zip_file:
+            zip_file.extractall(archive_dirname)
 
-    outputs = []
-    for sed_doc_location, sed_doc_outputs_results in (results or {}).items():
-        sed_doc = SedmlSimulationReader().run(os.path.join(archive_dirname, sed_doc_location))
+        outputs = []
+        for sed_doc_location, sed_doc_outputs_results in (results or {}).items():
+            sed_doc = SedmlSimulationReader().run(os.path.join(archive_dirname, sed_doc_location))
 
-        for output in sed_doc.outputs:
-            if output.id not in sed_doc_outputs_results:
-                continue
-            output_results = sed_doc_outputs_results.get(output.id, None)
-
-            if isinstance(output, Report):
-                type = 'SedReport'
-                report = output
-            elif isinstance(output, Plot2D):
-                type = 'SedPlot2D'
-                report = get_report_for_plot2d(output)
-            elif isinstance(output, Plot3D):
-                type = 'SedPlot3D'
-                report = get_report_for_plot3d(output)
-            else:  # pragma: no cover #
-                raise NotImplementedError('Outputs of type `{}` are not supported.'.format(output.__class__.__name__))
-
-            data = []
-            for data_set in report.data_sets:
-                if data_set.id not in output_results:
+            for output in sed_doc.outputs:
+                if output.id not in sed_doc_outputs_results:
                     continue
-                data_set_results = output_results[data_set.id]
+                output_results = sed_doc_outputs_results.get(output.id, None)
 
-                data.append({
-                    '_type': 'SimulationRunOutputDatum',
-                    'id': data_set.id,
-                    'label': data_set.label,
-                    'name': data_set.name,
-                    'shape': '' if data_set_results is None else ','.join(str(dim_len) for dim_len in data_set_results.shape),
-                    'type': '__None__' if data_set_results is None else data_set_results.dtype.name,
-                    'values': None if data_set_results is None else data_set_results.tolist(),
+                if isinstance(output, Report):
+                    type = 'SedReport'
+                    report = output
+                elif isinstance(output, Plot2D):
+                    type = 'SedPlot2D'
+                    report = get_report_for_plot2d(output)
+                elif isinstance(output, Plot3D):
+                    type = 'SedPlot3D'
+                    report = get_report_for_plot3d(output)
+                else:  # pragma: no cover #
+                    raise NotImplementedError('Outputs of type `{}` are not supported.'.format(output.__class__.__name__))
+
+                data = []
+                for data_set in report.data_sets:
+                    if data_set.id not in output_results:
+                        continue
+                    data_set_results = output_results[data_set.id]
+
+                    data.append({
+                        '_type': 'SimulationRunOutputDatum',
+                        'id': data_set.id,
+                        'label': data_set.label,
+                        'name': data_set.name,
+                        'shape': '' if data_set_results is None else ','.join(str(dim_len) for dim_len in data_set_results.shape),
+                        'type': '__None__' if data_set_results is None else data_set_results.dtype.name,
+                        'values': None if data_set_results is None else data_set_results.tolist(),
+                    })
+
+                outputs.append({
+                    '_type': 'SimulationRunOutput',
+                    'outputId': sed_doc_location + '/' + output.id,
+                    'name': output.name,
+                    'type': type,
+                    'data': data,
                 })
 
-            outputs.append({
-                '_type': 'SimulationRunOutput',
-                'outputId': sed_doc_location + '/' + output.id,
-                'name': output.name,
-                'type': type,
-                'data': data,
-            })
+        # return
+        return {
+            '_type': 'SimulationRunResults',
+            'outputs': outputs,
+            'log': log.to_json(),
+        }
 
-    # return
-    return {
-        '_type': 'SimulationRunResults',
-        'outputs': outputs,
-        'log': log.to_json(),
-    }
+    elif return_type == 'h5':
+        h5_filename = os.path.join(out_dir, get_config().H5_REPORTS_PATH)
+        return flask.send_file(h5_filename,
+                               mimetype=accept,
+                               as_attachment=True,
+                               attachment_filename='outputs.h5')
+
+    else:
+        zip_filename = get_temp_file()
+        with zipfile.ZipFile(zip_filename, 'w') as zip_file:
+            for root, dirs, files in os.walk(out_dir):
+                for file in files:
+                    zip_file.write(os.path.join(root, file),
+                                   os.path.relpath(os.path.join(root, file), out_dir))
+
+        return flask.send_file(zip_filename,
+                               mimetype=accept,
+                               as_attachment=True,
+                               attachment_filename='outputs.zip')
--- apps/combine-service/tests/run/test_run_run.py
+++ apps/combine-service/tests/run/test_run_run.py
@@ -1,4 +1,8 @@
+from biosimulators_utils.archive.io import ArchiveReader
+from biosimulators_utils.combine.io import CombineArchiveReader
 from biosimulators_utils.log.data_model import Status
+from biosimulators_utils.report.io import ReportReader
+from biosimulators_utils.sedml.io import SedmlSimulationReader
 from src import app
 from src.handlers.run.utils import get_simulator_api
 from unittest import mock
@@ -8,11 +12,19 @@ import numpy
 import numpy.testing
 import os
 import requests.exceptions
+import shutil
+import tempfile
 import unittest
 
 
 class RunSimulationTestCase(unittest.TestCase):
     FIXTURES_DIRNAME = os.path.join(os.path.dirname(__file__), '..', 'fixtures')
+
+    def setUp(self):
+        self.tmp_dirname = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_dirname)
 
     def test_url(self):
         endpoint = '/run/run'
@@ -215,9 +227,126 @@ class RunSimulationTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200, response.json)
         response = response.json
 
+    def test_hdf5_response(self):
+        endpoint = '/run/run'
+        archive_filename = os.path.join(self.FIXTURES_DIRNAME, 'Caravagna-J-Theor-Biol-2010-tumor-suppressive-oscillations.omex')
+        data = MultiDict([
+            ('_type', 'SimulationRun'),
+            ('archiveUrl', 'https://web.site/archive.omex'),
+            ('simulator', 'copasi'),
+        ])
+        with app.app.app.test_client() as client:
+            with open(archive_filename, 'rb') as archive_file:
+                with mock.patch('requests.get', return_value=mock.Mock(raise_for_status=lambda: None, content=archive_file.read())):
+                    response = client.post(endpoint, data=data, content_type="multipart/form-data", headers={'Accept': 'application/x-hdf'})
+
+        self.assertEqual(response.status_code, 200, response.json)
+        h5_filename = os.path.join(self.tmp_dirname, 'reports.h5')
+        with open(h5_filename, 'wb') as file:
+            file.write(response.data)
+
+        self.assertEqual([
+            'BIOMD0000000912_sim.sedml/BIOMD0000000912_report',
+            'BIOMD0000000912_sim.sedml/plot_1'
+        ], ReportReader().get_ids(self.tmp_dirname))
+
+        archive_dirname = os.path.join(self.tmp_dirname, 'archive')
+        CombineArchiveReader().run(archive_filename, archive_dirname)
+        sed_doc = SedmlSimulationReader().run(os.path.join(archive_dirname, 'BIOMD0000000912_sim.sedml'))
+        report = next(output for output in sed_doc.outputs if output.id == 'BIOMD0000000912_report')
+
+        data_set_results = ReportReader().run(report, self.tmp_dirname, 'BIOMD0000000912_sim.sedml/BIOMD0000000912_report')
+
+        self.assertEqual(
+            set(data_set_results.keys()),
+            set([
+                'data_set_time',
+                'data_set_T',
+                'data_set_E',
+                'data_set_I',
+            ]),
+        )
+
+        numpy.testing.assert_allclose(data_set_results['data_set_time'], numpy.linspace(0., 1000., 5001))
+        for values in data_set_results.values():
+            self.assertEqual(values.shape, (5001,))
+            self.assertFalse(numpy.any(numpy.isnan(values)))
+
+    def test_zip_response(self):
+        endpoint = '/run/run'
+        archive_filename = os.path.join(self.FIXTURES_DIRNAME, 'Caravagna-J-Theor-Biol-2010-tumor-suppressive-oscillations.omex')
+        data = MultiDict([
+            ('_type', 'SimulationRun'),
+            ('archiveUrl', 'https://web.site/archive.omex'),
+            ('simulator', 'copasi'),
+        ])
+        with app.app.app.test_client() as client:
+            with open(archive_filename, 'rb') as archive_file:
+                with mock.patch('requests.get', return_value=mock.Mock(raise_for_status=lambda: None, content=archive_file.read())):
+                    response = client.post(endpoint, data=data, content_type="multipart/form-data", headers={'Accept': 'application/zip'})
+
+        self.assertEqual(response.status_code, 200, response.json)
+        zip_filename = os.path.join(self.tmp_dirname, 'outputs.zip')
+        with open(zip_filename, 'wb') as file:
+            file.write(response.data)
+        zip_dirname = os.path.join(self.tmp_dirname, 'outputs')
+        archive = ArchiveReader().run(zip_filename, zip_dirname)
+
+        self.assertEqual(
+            set(file.archive_path for file in archive.files),
+            set([
+                'log.yml',
+                'reports.h5',
+                'BIOMD0000000912_sim.sedml/plot_1.pdf'
+            ]))
+
+        self.assertEqual([
+            'BIOMD0000000912_sim.sedml/BIOMD0000000912_report',
+            'BIOMD0000000912_sim.sedml/plot_1'
+        ], ReportReader().get_ids(zip_dirname))
+
+        archive_dirname = os.path.join(zip_dirname, 'archive')
+        CombineArchiveReader().run(archive_filename, archive_dirname)
+        sed_doc = SedmlSimulationReader().run(os.path.join(archive_dirname, 'BIOMD0000000912_sim.sedml'))
+        report = next(output for output in sed_doc.outputs if output.id == 'BIOMD0000000912_report')
+
+        data_set_results = ReportReader().run(report, zip_dirname, 'BIOMD0000000912_sim.sedml/BIOMD0000000912_report')
+
+        self.assertEqual(
+            set(data_set_results.keys()),
+            set([
+                'data_set_time',
+                'data_set_T',
+                'data_set_E',
+                'data_set_I',
+            ]),
+        )
+
+        numpy.testing.assert_allclose(data_set_results['data_set_time'], numpy.linspace(0., 1000., 5001))
+        for values in data_set_results.values():
+            self.assertEqual(values.shape, (5001,))
+            self.assertFalse(numpy.any(numpy.isnan(values)))
+
     def test_error_handling(self):
         endpoint = '/run/run'
         archive_filename = os.path.join(self.FIXTURES_DIRNAME, 'Caravagna-J-Theor-Biol-2010-tumor-suppressive-oscillations.omex')
+
+        # unsupported return type
+        data = MultiDict([
+            ('_type', 'SimulationRun'),
+            ('archiveUrl', 'https://web.site/archive.omex'),
+            ('simulator', 'copasi'),
+        ])
+
+        with app.app.app.test_client() as client:
+            response = client.post(endpoint, data=data, content_type="multipart/form-data", headers={'Accept': 'undefined'})
+        self.assertEqual(response.status_code, 400, response.json)
+        self.assertIn('header must be one of', response.json['title'])
+
+        with app.app.app.test_client() as client:
+            response = client.post(endpoint, data=data, content_type="multipart/form-data", headers={'accept': 'undefined'})
+        self.assertEqual(response.status_code, 400, response.json)
+        self.assertIn('header must be one of', response.json['title'])
 
         # archiveFile and archiveUrl
         with open(archive_filename, 'rb') as archive_file:
