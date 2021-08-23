d7b4c7b6f4c67b8659b51b8d89730435ccf17265
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-08-23 10:56:50 -0400

    chore: fix test

--- apps/dispatch-api/src/metadata/metadata.service.spec.ts
+++ apps/dispatch-api/src/metadata/metadata.service.spec.ts
@@ -1,12 +1,27 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { SimulationRunModel } from '../simulation-run/simulation-run.model';
+import { SimulationRunMetadataModel } from './metadata.model';
 import { MetadataService } from './metadata.service';
 
+
+class MockModel { }
 describe('MetadataService', () => {
   let service: MetadataService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MetadataService],
+      providers: [
+        MetadataService,
+        {
+          provide: getModelToken(SimulationRunMetadataModel.name),
+          useClass: MockModel,
+        },
+        {
+          provide: getModelToken(SimulationRunModel.name),
+          useClass: MockModel,
+        },
+      ],
     }).compile();
 
     service = module.get<MetadataService>(MetadataService);
