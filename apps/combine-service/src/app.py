791c9682e879e6fe40f7f7d43283e964c8616678
Jonathan Karr <jonrkarr@gmail.com>
2021-08-26 18:45:59 -0400

    chore: disabled /run endpoints on production

--- apps/combine-service/src/app.py
+++ apps/combine-service/src/app.py
@@ -1,14 +1,41 @@
 from . import exceptions
 from flask_cors import CORS
 import connexion
+import os
+import tempfile
+import yaml
+
+spec_dirname = 'spec'
+spec_filename = 'spec.yml'
+
+# disable ``/run`` endpoints from production
+if os.environ.get('ENV', 'dev').lower() == 'prod':
+    with open(os.path.join(os.path.dirname(__file__), spec_dirname, spec_filename), 'r') as file:
+        specs = yaml.load(file, Loader=yaml.Loader)
+
+    for key in list(specs['paths'].keys()):
+        if key.startswith('/run/'):
+            specs['paths'].pop(key)
+
+    fid, temp_spec_filename = tempfile.mkstemp(dir=os.path.join(os.path.dirname(__file__), spec_dirname), suffix='.yml')
+    os.close(fid)
+    with open(temp_spec_filename, 'w') as file:
+        file.write(yaml.dump(specs))
+
+    spec_filename = os.path.basename(temp_spec_filename)
 
 # Instantiate app from specs
-app = connexion.App(__name__, specification_dir='spec')
+app = connexion.App(__name__, specification_dir=spec_dirname)
 
 # Setup handlers for APIs
-app.add_api('spec.yml',
+app.add_api(spec_filename,
             strict_validation=True,
             validate_responses=False)
+
+# clean up temporary spec file for production
+if os.environ.get('ENV', 'dev').lower() == 'prod':
+    os.remove(temp_spec_filename)
+
 # Validate_response = True will give error when API returns something that
 # does not match the schema. If you want to send a response even if invalid,
 # set to false. Set to false in production if optimistic that client can
