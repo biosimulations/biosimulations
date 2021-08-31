fb8a400367694b348fcdb87c1cbf6aff24805e84
Jonathan Karr <jonrkarr@gmail.com>
2021-08-31 14:21:35 -0400

    chore(datamodel): fixed lint issue

--- libs/datamodel/common/src/lib/common/python-api.ts
+++ libs/datamodel/common/src/lib/common/python-api.ts
@@ -1,6 +1,3 @@
-import { IEdamOntologyIdVersion } from './ontology';
-import { OperatingSystemType } from './operating-system-type';
-
 export interface IPythonApi {
   package: string;
   module: string;
