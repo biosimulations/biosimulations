20ca7106eeb2dcbe2ddb8a6aead78aa810d5ddaa
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-09-02 17:26:49 -0400

    test: fix tests

--- apps/dispatch-api/src/metadata/metadata.service.spec.ts
+++ apps/dispatch-api/src/metadata/metadata.service.spec.ts
@@ -1,3 +1,4 @@
+import { BiosimulationsConfigModule } from '@biosimulations/config/nest';
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { SimulationRunModel } from '../simulation-run/simulation-run.model';
@@ -10,6 +11,7 @@ describe('MetadataService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [BiosimulationsConfigModule],
       providers: [
         MetadataService,
         {
