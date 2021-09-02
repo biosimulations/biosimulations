9e3ea0d39465312d8212cc4c13c5b4c6c95ebed5
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-09-02 15:52:34 -0400

    refactor: work on environment setup

--- libs/config/common/src/lib/endpoints.ts
+++ libs/config/common/src/lib/endpoints.ts
@@ -1,20 +1,24 @@
 import { environment } from '@biosimulations/shared/environments';
 
 export type endpoints =
+  | 'api'
   | 'simulationRuns'
   | 'simulationRunResults'
   | 'simulationRunLogs'
   | 'simulationRunMetadata'
-  | 'simulators';
+  | 'simulators'
+  | 'files';
 
 type endpointMap = { [key in endpoints]: string };
 
 const baseUrl = environment.baseUrl || 'https://api.biosimulations.dev';
 
 export const Endpoints: endpointMap = {
+  api: baseUrl,
   simulationRuns: `${baseUrl}/runs`,
   simulationRunResults: `${baseUrl}/results`,
   simulationRunLogs: `${baseUrl}/logs`,
   simulationRunMetadata: `${baseUrl}/metadata`,
   simulators: `${baseUrl}/simulators`,
+  files: `${baseUrl}/files`
 };
