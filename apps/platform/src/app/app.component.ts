7d98b5388e1a96f63df439c43fafc46b55706a3a
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-09-20 20:47:10 -0400

    refactor(platform): clean up old code

--- apps/platform/src/app/app.component.ts
+++ apps/platform/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { ConfigService, ScrollService } from '@biosimulations/shared/services';
-import { AuthService } from '@biosimulations/auth/angular';
+
 
 @Component({
   selector: 'biosimulations-root',
@@ -12,20 +12,12 @@ export class AppComponent implements AfterViewInit {
   constructor(
     public config: ConfigService,
     private scrollService: ScrollService,
-    private auth: AuthService,
+    
   ) {}
 
   ngAfterViewInit(): void {
     this.scrollService.init();
   }
 
-  login() {
-    this.auth.login();
-  }
-  logout() {
-    this.auth.logout();
-  }
-  isAuthenticated() {
-    return this.auth.isAuthenticated$;
-  }
+
 }
--- apps/platform/src/app/projects/project-card/project-card.component.ts
+++ apps/platform/src/app/projects/project-card/project-card.component.ts
@@ -1,11 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'biosimulations-project-card',
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss'],
 })
-export class ProjectCardComponent {
-  @Input() project: any;
-  constructor() {}
+export class ProjectCardComponent implements OnInit {
+  @Input() public project: {id:string, title:string, thumbnails:string[]} = {id:'', title:'', thumbnails:[]};
+  public url= "";
+  public constructor() {}
+  public ngOnInit(): void {     
+    this.url = `/projects/${this.project.id}`
+  }
 }
