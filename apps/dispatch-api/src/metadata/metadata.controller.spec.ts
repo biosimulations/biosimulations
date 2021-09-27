9ad98b67ff0c49a97627a96f3d7fdb0409d9ada8
Jonathan Karr <jonrkarr@gmail.com>
2021-09-27 11:43:26 -0400

    chore(dispatch-api): updated tests for authentication

--- apps/dispatch-api/src/metadata/metadata.controller.spec.ts
+++ apps/dispatch-api/src/metadata/metadata.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MetadataController } from './metadata.controller';
 import { MetadataService } from './metadata.service';
+import { BiosimulationsAuthModule } from '@biosimulations/auth/nest';
+import { BiosimulationsConfigModule } from '@biosimulations/config/nest';
 
 class MockMetadataService {}
 
@@ -10,6 +12,10 @@ describe('MetadataController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MetadataController],
+      imports: [
+        BiosimulationsAuthModule,
+        BiosimulationsConfigModule,
+      ],
       providers: [{ provide: MetadataService, useClass: MockMetadataService }],
     }).compile();
 
