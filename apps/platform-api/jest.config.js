58d9e169fde70168d8b2756b40218fb8ca09675b
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-09-28 12:45:39 -0400

    refactor: remove plarform-api
    consolidate dispatch-api and platform-api

--- apps/platform-api/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
-  },
-  coverageDirectory: '../../coverage/apps/platform-api',
-  displayName: 'platform-api',
-};
