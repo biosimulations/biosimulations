a0ea51e998f063d2c84555cce6f9d219a2da2f5d
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-09-28 12:43:53 -0400

    refactor: remove ontology api
    moved endpoints of ontology api directly into main api

--- apps/ontology-api/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
-  },
-  coverageDirectory: '../../coverage/apps/ontology-api',
-  displayName: 'ontology-api',
-  testEnvironment: 'node',
-};
