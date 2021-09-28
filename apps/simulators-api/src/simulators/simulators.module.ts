35860da8f7648e42335c68c7ad041569d07afa22
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-09-28 12:34:17 -0400

    refactor: move simulators api model into shared api-models

--- apps/simulators-api/src/simulators/simulators.module.ts
+++ apps/simulators-api/src/simulators/simulators.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { SimulatorsController } from './simulators.controller';
 import { SimulatorsService } from './simulators.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Simulator } from '@biosimulations/simulators/api-models';
+import { Simulator } from '@biosimulations/simulators/database-models';
 import { SimulatorSchema } from '@biosimulations/simulators/database-models';
 
 import {
