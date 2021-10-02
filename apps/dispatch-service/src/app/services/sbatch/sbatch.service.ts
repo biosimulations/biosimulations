cb0aa04cbdc7bd8f2a3e7cf818c541470d7c4519
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-10-02 17:29:19 -0400

    fix(dispatch-service): fix processing of environment variables

--- apps/dispatch-service/src/app/services/sbatch/sbatch.service.ts
+++ apps/dispatch-service/src/app/services/sbatch/sbatch.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EnvironmentVariable, Purpose } from '@biosimulations/datamodel/common';
 
 @Injectable()
 export class SbatchService {
   public constructor(private configService: ConfigService) {}
-
+  private logger = new Logger(SbatchService.name);
   public generateSbatch(
     tempSimDir: string,
     simulator: string,
@@ -20,7 +20,7 @@ export class SbatchService {
   ): string {
     const homeDir = this.configService.get('hpc.homeDir');
     const bucket = this.configService.get('storage.bucket');
-    const endpoint = this.configService.get('storage.endpoint');
+    let endpoint = this.configService.get('storage.endpoint');
 
     const memoryFormatted = Math.ceil(memory * 1000);
 
@@ -39,13 +39,17 @@ export class SbatchService {
     const cyan = '\\033[0;36m';
 
     if (apiDomain.startsWith('http://localhost')) {
-      apiDomain = 'https://run.api.biosimulations.dev/';
+      apiDomain = 'https://api.biosimulations.dev/';
+    }
+    if (endpoint.startsWith('https://localhost')) {
+      endpoint = 'http://s3low.scality.uchc.edu';
     }
 
-    const allEnvVars = [...envVars];
-    this.configService
-      .get('singularity')
-      .forEach((envVarPurpose: any): void => {
+    let allEnvVars = [...envVars];
+    const vars = this.configService.get('singularity').envVars;
+    this.logger.error(vars);
+    try {
+      vars.forEach((envVarPurpose: any): void => {
         if (envVarPurpose.purpose === 'ALL') {
           allEnvVars.push({
             key: envVarPurpose.key,
@@ -61,6 +65,10 @@ export class SbatchService {
           });
         }
       });
+    } catch (e) {
+      this.logger.error(e);
+      allEnvVars = [...envVars];
+    }
 
     const allEnvVarsString =
       allEnvVars.length > 0
@@ -97,7 +105,7 @@ export SINGULARITY_CACHEDIR=${homeDir}/singularity/cache/
 export SINGULARITY_PULLFOLDER=${homeDir}/singularity/images/
 cd ${tempSimDir}
 echo -e '${cyan}=============Downloading Combine Archive=============${nc}'
-( ulimit -f 1048576; srun wget --progress=bar:force ${apiDomain}run/${simId}/download -O '${omexName}')
+( ulimit -f 1048576; srun wget --progress=bar:force ${apiDomain}runs/${simId}/download -O '${omexName}')
 echo -e '${cyan}=============Extracting Combine Archive==============${nc}'
 unzip -o ${omexName} -d contents
 echo -e '${cyan}=================Running simulation==================${nc}'
