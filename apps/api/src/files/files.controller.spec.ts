fda027deeae27419cb6759e4db5103f6d481cdf5
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-10-03 23:07:31 -0400

    test(api): setup api tests

--- apps/api/src/files/files.controller.spec.ts
+++ apps/api/src/files/files.controller.spec.ts
@@ -1,11 +1,16 @@
+import { BiosimulationsAuthModule } from '@biosimulations/auth/nest';
+import { BiosimulationsConfigModule } from '@biosimulations/config/nest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilesController } from './files.controller';
+import { FilesService } from './files.service';
 
 describe('FilesController', () => {
   let controller: FilesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [BiosimulationsAuthModule, BiosimulationsConfigModule],
+      providers: [{ provide: FilesService, useValue: {} }],
       controllers: [FilesController],
     }).compile();
 
--- libs/ontology/client/src/lib/ontology.service.spec.ts
+++ libs/ontology/client/src/lib/ontology.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { OntologyService } from './ontology.service';
 
 describe('OntologyService', () => {
   let service: OntologyService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(OntologyService);
   });
 
