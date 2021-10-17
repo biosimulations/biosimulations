d945f70ee4b5510c156eb84ad209269b6b9327c8
Jonathan Karr <jonrkarr@gmail.com>
2021-10-17 17:47:47 -0400

    feat(simulators): added repository digest to image model; closes #3194

--- libs/datamodel/api/src/lib/simulators/image.ts
+++ libs/datamodel/api/src/lib/simulators/image.ts
@@ -4,12 +4,32 @@ import { EdamOntologyIdVersion } from '../common';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class Image implements IImage {
-  @ApiProperty({ type: String, format: 'url' })
+  @ApiProperty({
+    type: String,
+    description: 'URL for the image',
+    example: 'ghcr.io/virtualcell/biosimulators_vcell:latest',
+  })
   url!: string;
 
-  @ApiProperty({ type: EdamOntologyIdVersion })
+  @ApiProperty({
+    type: String,
+    description: 'Repository digest for the image',
+    example: 'sha256:5d1595553608436a2a343f8ab7e650798ef5ba5dab007b9fe31cd342bf18ec81',
+    pattern: '^sha256:[a-z0-9]{64,64}$'
+  })
+  digest!: string;
+
+  @ApiProperty({ 
+    type: EdamOntologyIdVersion,
+    description: 'Format of the image',
+  })
   format!: EdamOntologyIdVersion;
 
-  @ApiProperty({ type: String, enum: OperatingSystemType, nullable: true })
+  @ApiProperty({ 
+    type: String, 
+    enum: OperatingSystemType, 
+    nullable: true,
+    description: 'Operating system in the image',
+  })
   operatingSystemType!: OperatingSystemType | null;
 }
