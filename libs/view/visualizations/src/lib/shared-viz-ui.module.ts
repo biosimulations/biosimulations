277b1d7389751ba7955a5d20041f4a03e4933fb7
Jonathan Karr <jonrkarr@gmail.com>
2021-10-18 16:44:56 -0400

    refactor(ui): removed old unused component

--- libs/view/visualizations/src/lib/shared-viz-ui.module.ts
+++ libs/view/visualizations/src/lib/shared-viz-ui.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SharedUiModule } from '@biosimulations/shared/ui';
-import { VegaEmbedComponent } from './vega-embed/vega-embed.component';
 import { VegaVisualizationComponent } from './vega-visualization/vega-visualization.component';
 import { PlotlyViaWindowModule } from 'angular-plotly.js';
 import { PlotlyVisualizationComponent } from './plotly-visualization/plotly-visualization.component';
@@ -10,12 +9,10 @@ import { PlotlyVisualizationComponent } from './plotly-visualization/plotly-visu
 @NgModule({
   imports: [CommonModule, RouterModule, SharedUiModule, PlotlyViaWindowModule],
   exports: [
-    VegaEmbedComponent,
     VegaVisualizationComponent,
     PlotlyVisualizationComponent,
   ],
   declarations: [
-    VegaEmbedComponent,
     VegaVisualizationComponent,
     PlotlyVisualizationComponent,
   ],
