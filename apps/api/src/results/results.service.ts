53a6bbc5d2ccab9ea086f405656bf26d0cb2bacb
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-10-18 22:35:05 -0400

    fix(api): correct field name to get values from dataservice
    the dataservice api specs incorrectly has "values" instead of "value" as the name of the field that
    contains the values of the dataset. This error propogates to the library, so must be overridden by casting the return type to any
    
    closes #3313

--- apps/api/src/results/results.service.ts
+++ apps/api/src/results/results.service.ts
@@ -37,7 +37,9 @@ export class ResultsService {
     datasetId: string,
   ): Promise<undefined | (string[] | number[] | boolean[])[]> {
     // The index feild will be needed when we are doing slicing of the data so this will need to change
-    return (await this.results.getDatasetValues(simId, datasetId))?.values;
+    // TODO update the hsds client to use the correct name "value" not "values"
+    return ((await this.results.getDatasetValues(simId, datasetId)) as any)
+      ?.value;
   }
 
   public async download(simId: string): Promise<S3.Body | undefined> {
@@ -79,8 +81,8 @@ export class ResultsService {
       sedIds.length == sedLabels.length &&
       sedIds.length == sedShapes.length &&
       sedIds.length == sedTypes.length &&
-      sedIds.length == sedNames.length &&
-      ((includeValues && sedIds.length == values.length) || !includeValues);
+      sedIds.length == sedNames.length;
+    (includeValues && sedIds.length == values.length) || !includeValues;
 
     if (!consistent) {
       this.logger.error(
