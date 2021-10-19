43e14df0632e7088a76ff8505f87dd0351a7847d
Jonathan Karr <jonrkarr@gmail.com>
2021-10-19 18:35:44 -0400

    refactor(platform): removed old help

--- apps/platform/src/app/app.module.ts
+++ apps/platform/src/app/app.module.ts
@@ -27,13 +27,6 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
   {
-    path: 'help',
-    loadChildren: () => import('./help/help.module').then((m) => m.HelpModule),
-    data: {
-      breadcrumb: 'Help',
-    },
-  },
-  {
     path: 'error',
     loadChildren: () => SharedErrorComponentsModule,
   },
