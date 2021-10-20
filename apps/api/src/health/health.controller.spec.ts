334c3e8bd14cdce7267ca7632426314472ad32fa
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-10-20 17:40:05 -0400

    test(api,simulators-api): set up tests for health checks

--- apps/api/src/health/health.controller.spec.ts
+++ apps/api/src/health/health.controller.spec.ts
@@ -1,5 +1,7 @@
+import { BiosimulationsConfigModule } from '@biosimulations/config/nest';
 import { TerminusModule } from '@nestjs/terminus';
 import { Test, TestingModule } from '@nestjs/testing';
+import { BullHealthIndicator } from './bullHealthCheck';
 import { HealthController } from './health.controller';
 
 describe('HealthController', () => {
@@ -7,7 +9,8 @@ describe('HealthController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TerminusModule],
+      imports: [TerminusModule, BiosimulationsConfigModule],
+      providers: [{ provide: BullHealthIndicator, useValue: {} }],
       controllers: [HealthController],
     }).compile();
 
