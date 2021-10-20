cf1107aec461d17a566114c2d5a4fb5e4c55a891
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-10-20 17:28:33 -0400

    feat(api): add various health checks and endpoints

--- apps/api/src/health/health.module.ts
+++ apps/api/src/health/health.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { TerminusModule } from '@nestjs/terminus';
+import { BiosimulationsConfigModule } from '@biosimulations/config/nest';
+import { BullModule } from '@nestjs/bull';
+import { BullHealthIndicator, HealthCheckProcessor } from './bullHealthCheck';
 
 @Module({
-  imports: [TerminusModule],
+  imports: [
+    TerminusModule,
+    BiosimulationsConfigModule,
+    BullModule.registerQueue({
+      name: 'health',
+      prefix: '{health}',
+    }),
+  ],
+  providers: [HealthCheckProcessor, BullHealthIndicator],
   controllers: [HealthController],
 })
 export class HealthModule {}
