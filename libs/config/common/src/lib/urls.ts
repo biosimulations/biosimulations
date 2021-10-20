29c557390697a5cb3ac85d77f6b63a7f8e7f3215
Jonathan Karr <jonrkarr@gmail.com>
2021-10-20 19:29:48 -0400

    refactor(config): reverted simulators API endpoint

--- libs/config/common/src/lib/urls.ts
+++ libs/config/common/src/lib/urls.ts
@@ -36,7 +36,7 @@ const envUrls: { [key in envs]: urlMap } = {
     accountApi: 'https://account.biosimulations.dev/',
     dispatchApi: 'https://run.api.biosimulations.dev/',
     combineApi: 'https://combine.api.biosimulations.dev/',
-    simulatorsApi: 'http://localhost:3333/',
+    simulatorsApi: 'https://api.biosimulators.dev/',
     platform: 'https://biosimulations.dev',
     account: 'https://login.biosimulations.dev',
     dispatch: 'https://run.biosimulations.dev',
