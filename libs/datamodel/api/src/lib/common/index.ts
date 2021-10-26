26f4bfacc698fe81d4ea102355481634d194a166
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-10-26 09:43:58 -0400

    refactor(datamodel): remove old license datamodel
    closes #3380

--- libs/datamodel/api/src/lib/common/index.ts
+++ libs/datamodel/api/src/lib/common/index.ts
@@ -1,7 +1,6 @@
 export * from './format';
 export * from './citation';
 export * from './taxon';
-export * from './license.dto';
 export * from './ontology.dto';
 export * from './person.dto';
 export * from './externalreferences.dto';
