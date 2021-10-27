20a70b5dbcc72a6b05985113a157d836cbefc2d8
Jonathan Karr <jonrkarr@gmail.com>
2021-10-26 23:40:01 -0400

    refactor(api): expanded project validation

--- apps/api/src/projects/projects.service.spec.ts
+++ apps/api/src/projects/projects.service.spec.ts
@@ -3,9 +3,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SimulationRunModel } from '../simulation-run/simulation-run.model';
 import { ProjectModel } from './project.model';
 import { ProjectsService } from './projects.service';
+import { FilesService } from '../files/files.service';
+import { SpecificationsService } from '../specifications/specifications.service';
+import { SimulationHDFService } from '@biosimulations/hsds/client';
 import { SimulationFile, SimulationFileSchema } from '../simulation-run/file.model';
+import { FileModel } from '../files/files.model';
+import { SpecificationsModel } from '../specifications/specifications.model';
+import { SimulationRunLog, CombineArchiveLog } from '../logs/logs.model';
 import { SimulationRunMetadataModel, SimulationRunMetadataSchema } from '../metadata/metadata.model';
 import { SimulationRunService } from '../simulation-run/simulation-run.service';
+import { ResultsService } from '../results/results.service';
+import { LogsService } from '../logs/logs.service';
 import { MetadataService } from '../metadata/metadata.service';
 import { HttpModule } from '@nestjs/axios';
 import { SharedNatsClientModule } from '@biosimulations/shared/nats-client';
@@ -18,6 +26,16 @@ import {
 
 describe('ProjectsService', () => {
   let service: ProjectsService;
+
+  class mockModel {
+    constructor(private data: any) {}
+    static save = jest.fn().mockResolvedValue('test');
+    save = mockModel.save;
+    static find = jest.fn().mockResolvedValue({});
+    static findOne = jest.fn().mockResolvedValue({});
+    static findOneAndUpdate = jest.fn().mockResolvedValue({});
+    static deleteOne = jest.fn().mockResolvedValue(true);
+  }
 
   class mockFile {
     data: any;
@@ -35,6 +53,13 @@ describe('ProjectsService', () => {
     getObject() {}
   }
 
+  class mockSimService {
+    getDataSets(id: string) {
+      return;
+    }
+    getDataSetbyId(id: string) {}
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [HttpModule, SharedNatsClientModule, BiosimulationsConfigModule],
@@ -48,11 +73,29 @@ describe('ProjectsService', () => {
           provide: getModelToken(SimulationRunModel.name),
           useClass: mockFile,
         },
+        { provide: getModelToken(FileModel.name), useValue: {} },
+        { provide: getModelToken(SpecificationsModel.name), useValue: {} },
+        {
+          provide: getModelToken(SimulationRunLog.name),
+          useValue: mockModel,
+        },
+        {
+          provide: getModelToken(CombineArchiveLog.name),
+          useValue: mockModel,
+        },
         { provide: getModelToken(SimulationRunMetadataModel.name), useValue: {} },
         { provide: SharedStorageService, useClass: mockStorage },
         { provide: SimulationStorageService, useClass: mockStorage },
+        {
+          provide: SimulationHDFService,
+          useClass: mockSimService,
+        },
         ProjectsService,
+        FilesService,
+        SpecificationsService,
         SimulationRunService,
+        ResultsService,
+        LogsService,
         MetadataService,
       ],
     }).compile();
