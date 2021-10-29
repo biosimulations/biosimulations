7f6aff0605d5bcfa7e982337afb3d9b28391a5eb
Jonathan Karr <jonrkarr@gmail.com>
2021-10-28 21:41:53 -0400

    refactor(api): fixed failing test

--- apps/api/src/simulation-run/simulation-run.service.spec.ts
+++ apps/api/src/simulation-run/simulation-run.service.spec.ts
@@ -19,8 +19,24 @@ import { SimulationFile } from './file.model';
 import { SimulationRunModel } from './simulation-run.model';
 import { SimulationRunService } from './simulation-run.service';
 
+import { FilesService } from '../files/files.service';
+import { FileModel } from '../files/files.model';
+import { SpecificationsService } from '../specifications/specifications.service';
+import { SpecificationsModel } from '../specifications/specifications.model';
+import { ResultsService } from '../results/results.service';
+import { SimulationHDFService } from '@biosimulations/hsds/client';
+import { LogsService } from '../logs/logs.service';
+import { SimulationRunLog, CombineArchiveLog } from '../logs/logs.model';
+import { MetadataService } from '../metadata/metadata.service';
+import {
+  SimulationRunMetadataModel,
+  SimulationRunMetadataSchema,
+} from '../metadata/metadata.model';
+
+
 describe('SimulationRunService', () => {
   let service: SimulationRunService;
+
   class mockFile {
     data: any;
     save: () => any;
@@ -31,10 +47,29 @@ describe('SimulationRunService', () => {
       };
     }
   }
+
   class mockStorage {
     putObject() {}
     getObject() {}
   }
+
+  class mockModel {
+    constructor(private data: any) {}
+    static save = jest.fn().mockResolvedValue('test');
+    save = mockModel.save;
+    static find = jest.fn().mockResolvedValue({});
+    static findOne = jest.fn().mockResolvedValue({});
+    static findOneAndUpdate = jest.fn().mockResolvedValue({});
+    static deleteOne = jest.fn().mockResolvedValue(true);
+  }
+
+  class mockSimService {
+    getDataSets(id: string) {
+      return;
+    }
+    getDataSetbyId(id: string) {}
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [HttpModule, SharedNatsClientModule, BiosimulationsConfigModule],
@@ -48,8 +83,31 @@ describe('SimulationRunService', () => {
           provide: getModelToken(SimulationRunModel.name),
           useClass: mockFile,
         },
+         { provide: getModelToken(FileModel.name), useValue: {} },
+        { provide: getModelToken(SpecificationsModel.name), useValue: {} },
+        {
+          provide: getModelToken(SimulationRunLog.name),
+          useValue: mockModel,
+        },
+        {
+          provide: getModelToken(CombineArchiveLog.name),
+          useValue: mockModel,
+        },
+        {
+          provide: getModelToken(SimulationRunMetadataModel.name),
+          useValue: {},
+        },
         { provide: SharedStorageService, useClass: mockStorage },
         { provide: SimulationStorageService, useClass: mockStorage },
+        {
+          provide: SimulationHDFService,
+          useClass: mockSimService,
+        },
+        FilesService,
+        SpecificationsService,
+        ResultsService,
+        LogsService,
+        MetadataService,
       ],
     }).compile();
 
