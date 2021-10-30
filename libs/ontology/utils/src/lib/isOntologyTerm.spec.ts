7996058645825d21660ac5deb514db411f8f0004
Jonathan Karr <jonrkarr@gmail.com>
2021-10-30 13:48:30 -0400

    refactor(ontology): expanded method to check parents of ontology terms

--- libs/ontology/utils/src/lib/isOntologyTerm.spec.ts
+++ libs/ontology/utils/src/lib/isOntologyTerm.spec.ts
@@ -2,10 +2,34 @@ import { IsOntologyTermConstraint } from './isOntologyTerm';
 import { Ontologies } from '@biosimulations/datamodel/common';
 
 describe('isOntologyTerm', () => {
-  it('Should Accept valid URLs', () => {
+  it('Should accept valid ids', () => {
     const value = 'KISAO_0000019';
     const args = {
-      constraints: [Ontologies.KISAO],
+      constraints: [Ontologies.KISAO, undefined],
+      value: value,
+      targetName: '',
+      object: {},
+      property: 'term',
+    }
+    expect(new IsOntologyTermConstraint().validate(value, args)).toBe(true);
+  });
+
+  it('Should accept valid immediate parents', () => {
+    const value = 'KISAO_0000019';
+    const args = {
+      constraints: [Ontologies.KISAO, 'KISAO_0000433'],
+      value: value,
+      targetName: '',
+      object: {},
+      property: 'term',
+    }
+    expect(new IsOntologyTermConstraint().validate(value, args)).toBe(true);
+  });
+
+  it('Should accept valid parents', () => {
+    const value = 'KISAO_0000019';
+    const args = {
+      constraints: [Ontologies.KISAO, 'KISAO_0000000'],
       value: value,
       targetName: '',
       object: {},
@@ -17,7 +41,7 @@ describe('isOntologyTerm', () => {
   it('Should reject non-strings', () => {
     const value = 19;
     const args = {
-      constraints: [Ontologies.KISAO],
+      constraints: [Ontologies.KISAO, undefined],
       value: value,
       targetName: '',
       object: {},
@@ -29,7 +53,7 @@ describe('isOntologyTerm', () => {
   it('Should reject invalid ids', () => {
     let value = 'KISAO:0000019';
     const args = {
-      constraints: [Ontologies.KISAO],
+      constraints: [Ontologies.KISAO, undefined],
       value: value,
       targetName: '',
       object: {},
@@ -44,6 +68,18 @@ describe('isOntologyTerm', () => {
     expect(new IsOntologyTermConstraint().validate(value, args)).toBe(false);
 
     value = '19';
+    expect(new IsOntologyTermConstraint().validate(value, args)).toBe(false);
+  });
+
+  it('Should accept reject invalid parents', () => {
+    const value = 'KISAO_0000019';
+    const args = {
+      constraints: [Ontologies.KISAO, 'KISAO_0000201'],
+      value: value,
+      targetName: '',
+      object: {},
+      property: 'term',
+    }
     expect(new IsOntologyTermConstraint().validate(value, args)).toBe(false);
   });
 });
--- libs/ontology/utils/src/lib/isOntologyTerm.ts
+++ libs/ontology/utils/src/lib/isOntologyTerm.ts
@@ -8,13 +8,13 @@ import {
 import { Ontologies } from '@biosimulations/datamodel/common';
 import { OntologiesService } from '@biosimulations/ontology/ontologies';
 
-export function IsOntologyTerm(ontology: Ontologies, validationOptions?: ValidationOptions) {
+export function IsOntologyTerm(ontology: Ontologies, parentTermId?: string, validationOptions?: ValidationOptions) {
   return function (object: object, propertyName: string) {
     registerDecorator({
       name: 'isOntologyTerm',
       target: object.constructor,
       propertyName: propertyName,
-      constraints: [ontology],
+      constraints: [ontology, parentTermId],
       options: validationOptions,
       validator: IsOntologyTermConstraint,
     });
@@ -33,10 +33,16 @@ export class IsOntologyTermConstraint implements ValidatorConstraintInterface {
       return false;
     }
 
-    return OntologiesService.isTermId(ontology, value);
+    return OntologiesService.isTermId(ontology, value, args?.constraints[1]);
   }
 
   public defaultMessage(args?: ValidationArguments): string {
-    return `The value is not an id of a term in the ${args?.constraints[0]} ontology. Information about the available terms is available at https://ontology.api.biosimulations.org/.`;
+    const ontologyId = args?.constraints[0];
+    const parentTermId = args?.constraints[1];
+    if (parentTermId) {
+      return `The value is not an id of a child term of ${parentTermId} in the ${ontologyId} ontology. Information about the terms of the ${ontologyId} ontology is available at https://ontology.api.biosimulations.org/.`;
+    } else {
+      return `The value is not an id of a term in the ${ontologyId} ontology. Information about the terms of the ${ontologyId} ontology is available at https://ontology.api.biosimulations.org/.`;
+    }
   }
 }
