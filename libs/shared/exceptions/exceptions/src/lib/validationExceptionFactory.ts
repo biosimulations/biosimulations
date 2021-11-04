d74b9e0cd59a61cb05eab128f5944f778965a371
Biosimulations-Daemon <biosimulationsdaemon@gmail.com>
2021-11-04 04:27:53 +0000

    chore: format files. [skip ci]
    Signed-off-by: Biosimulations-Daemon <biosimulationsdaemon@gmail.com>

--- libs/shared/exceptions/exceptions/src/lib/validationExceptionFactory.ts
+++ libs/shared/exceptions/exceptions/src/lib/validationExceptionFactory.ts
@@ -5,9 +5,7 @@ import { inspect } from 'util';
 export const BiosimulationsValidationExceptionFactory = (
   errors: ValidationError[],
 ): BiosimulationsException => {
-  const message = errors
-    .map(getErrorMesage)
-    .join('\n\n');
+  const message = errors.map(getErrorMesage).join('\n\n');
 
   const bioSimErr = new BiosimulationsException(
     HttpStatus.BAD_REQUEST,
@@ -23,8 +21,10 @@ export const BiosimulationsValidationExceptionFactory = (
   return bioSimErr;
 };
 
-function getErrorMesage(error: ValidationError, level=0): string {
-  let msg = `${level === 0 ? 'Property or parameter' : 'Property'} '${error.property}' is invalid.`;
+function getErrorMesage(error: ValidationError, level = 0): string {
+  let msg = `${level === 0 ? 'Property or parameter' : 'Property'} '${
+    error.property
+  }' is invalid.`;
 
   const inspectedValue = inspect(error.value).replace(/\n/g, '\n    ');
   msg += `\n\n  Value:\n    ${inspectedValue}`;
@@ -32,14 +32,20 @@ function getErrorMesage(error: ValidationError, level=0): string {
   msg += '\n\n  Error(s):';
 
   if (error?.constraints && Object.entries(error?.constraints).length) {
-    msg += Object.entries(error?.constraints).map((typeMessage: [string, string]): string => {
+    msg += Object.entries(error?.constraints)
+      .map((typeMessage: [string, string]): string => {
         return `\n    - ${typeMessage[0]}: ${typeMessage[1]}`;
       })
       .join('');
   }
 
   if (error?.children?.length) {
-    msg += '\n    - ' + error.children.map(getErrorMesage, level + 1).join('\n').replace(/\n/g, '\n      ');
+    msg +=
+      '\n    - ' +
+      error.children
+        .map(getErrorMesage, level + 1)
+        .join('\n')
+        .replace(/\n/g, '\n      ');
   }
 
   return msg;
