e03f9c2c61f7237e02de7ab57361a695c9f967e1
Jonathan Karr <jonrkarr@gmail.com>
2021-11-03 23:45:12 -0400

    refactor(api): improved formatting of validation errors; closes #3372, closes #3373

--- libs/shared/exceptions/exceptions/src/lib/validationExceptionFactory.ts
+++ libs/shared/exceptions/exceptions/src/lib/validationExceptionFactory.ts
@@ -1,16 +1,13 @@
 import { HttpStatus, ValidationError } from '@nestjs/common';
 import { BiosimulationsException } from './exception';
 import { inspect } from 'util';
+
 export const BiosimulationsValidationExceptionFactory = (
   errors: ValidationError[],
 ): BiosimulationsException => {
-  // TODO handle multiple errors
-  // TODO parse the validation error to create readable error messages
-  // Need to recursively parse the errors and children, get the path names, and the eror message for each
-  const err = errors[0];
-  const message = `Parameter or property '${err.property}' with value ${inspect(
-    err.value,
-  )} is invalid`;
+  const message = errors
+    .map(getErrorMesage)
+    .join('\n\n');
 
   const bioSimErr = new BiosimulationsException(
     HttpStatus.BAD_REQUEST,
@@ -18,10 +15,32 @@ export const BiosimulationsValidationExceptionFactory = (
     message,
     undefined,
     undefined,
-    err.property,
+    undefined,
     undefined,
     [...errors],
   );
 
   return bioSimErr;
 };
+
+function getErrorMesage(error: ValidationError, level=0): string {
+  let msg = `${level === 0 ? 'Property or parameter' : 'Property'} '${error.property}' is invalid.`;
+
+  const inspectedValue = inspect(error.value).replace(/\n/g, '\n    ');
+  msg += `\n\n  Value:\n    ${inspectedValue}`;
+
+  msg += '\n\n  Error(s):';
+
+  if (error?.constraints && Object.entries(error?.constraints).length) {
+    msg += Object.entries(error?.constraints).map((typeMessage: [string, string]): string => {
+        return `\n    - ${typeMessage[0]}: ${typeMessage[1]}`;
+      })
+      .join('');
+  }
+
+  if (error?.children?.length) {
+    msg += '\n    - ' + error.children.map(getErrorMesage, level + 1).join('\n').replace(/\n/g, '\n      ');
+  }
+
+  return msg;
+}
