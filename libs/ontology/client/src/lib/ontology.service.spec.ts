f823a25bcf39a7582b9dbdd9c196365f9302038d
Jonathan Karr <jonrkarr@gmail.com>
2021-11-06 12:33:54 -0400

    refactor(ontology): fixed missing provider for test

--- libs/ontology/client/src/lib/ontology.service.spec.ts
+++ libs/ontology/client/src/lib/ontology.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { OntologyService } from './ontology.service';
+import { ConfigService } from '@biosimulations/config/angular';
 
 describe('OntologyService', () => {
   let service: OntologyService;
@@ -8,6 +9,7 @@ describe('OntologyService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
+      providers: [ConfigService],
     });
     service = TestBed.inject(OntologyService);
   });
