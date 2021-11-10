90a22732bf0935ee9b2d3b67782623bdc30ba06e
Jonathan Karr <jonrkarr@gmail.com>
2021-11-10 02:09:05 -0500

    refactor(api): trying to debug retrieval of results of simulation runs

--- libs/hsds/client/src/lib/hsds-client.module.ts
+++ libs/hsds/client/src/lib/hsds-client.module.ts
@@ -1,4 +1,4 @@
-import { HttpModule, Module, Logger } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { BiosimulationsConfigModule } from '@biosimulations/config/nest';
 
 import { Configuration, DomainService } from '@biosimulations/hdf5/api-client';
@@ -18,14 +18,6 @@ import { SimulationHDFService } from './dataset.service';
           const username = service.get('data.username');
           const password = service.get('data.password');
           const withCredentials = service.get('data.withCredentials');
-
-          const logger = new Logger(HSDSClientModule.name);
-          logger.log(
-            `Using HSDS client with HSDS configuration:` +
-              `\n  basePath: ${basePath}` +
-              `\n  username: ${username}` +
-              `\n  password: ${password}`,
-          );
 
           return new Configuration({
             username,
