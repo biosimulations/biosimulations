815c91c33e244f1fe2ac8a891a22951a94599918
Jonathan Karr <jonrkarr@gmail.com>
2021-11-12 17:37:17 -0500

    refactor: restore linting and testing for COMBINE API; closes #3534

---
+++ apps/combine-api/run-tests-in-docker-container.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker run \
+  --rm \
+  --entrypoint bash \
+  --mount type=bind,source=$(pwd)/apps/combine-api,target=/app/apps/combine-api \
+  ghcr.io/biosimulations/combine-api:sha-$(git rev-parse HEAD | cut -c 1-7) \
+  -c "
+    pipenv install --dev --system --deploy
+    pip uninstall -y matplotlib
+    pip install matplotlib==3.2.0
+    mv src src-ignore
+    export PYTHONPATH=apps/combine-api
+    /bin/bash /xvfb-startup.sh python -m pytest \
+      --verbose \
+      --cov apps/combine-api/src/ --cov-report=xml \
+      apps/combine-api/tests/
+  "
