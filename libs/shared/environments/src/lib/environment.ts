36030b42261d9782c84455865da05535c0f6628d
Jonathan Karr <jonrkarr@gmail.com>
2021-11-19 11:37:27 -0500

    refactor(config): undo temporary change to temporary

--- libs/shared/environments/src/lib/environment.ts
+++ libs/shared/environments/src/lib/environment.ts
@@ -1,3 +1,3 @@
 import { environmentType } from './environment.type';
-import { environment as currentEnvironment } from './environment.local';
+import { environment as currentEnvironment } from './environment.dev';
 export const environment: environmentType = currentEnvironment;
