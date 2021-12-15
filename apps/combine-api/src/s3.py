626a2f2a6deadaaa71517a09c19451f975316ad0
Jonathan Karr <jonrkarr@gmail.com>
2021-12-15 14:31:51 -0500

    feat(combine-api): added error messages for invalid S3 bucket configuration

--- apps/combine-api/src/s3.py
+++ apps/combine-api/src/s3.py
@@ -8,25 +8,18 @@ class S3Bucket(object):
     public_endpoint = ""
 
     def __init__(self) -> None:
-        config = {
-            **dotenv_values("secret/secret.env"),
-            **dotenv_values("config/config.env"),
-        }
-
-        access_key_id = config.get("STORAGE_ACCESS_KEY")
-        secret_access_key = config.get("STORAGE_SECRET")
-        default_bucket = config.get("STORAGE_BUCKET")
-        endpoint = config.get("STORAGE_ENDPOINT")
-        self.public_endpoint = config.get("STORAGE_PUBLIC_ENDPOINT")
-        self.bucket_name = default_bucket
+        config = self.get_configuration()
+        self.validate_configuration(config)
+        self.public_endpoint = config['public_endpoint']
+        self.bucket_name = config['default_bucket']
 
         s3 = boto3.resource('s3',
-                            endpoint_url=endpoint,
-                            aws_access_key_id=access_key_id,
-                            aws_secret_access_key=secret_access_key,
+                            endpoint_url=config['endpoint'],
+                            aws_access_key_id=config['access_key_id'],
+                            aws_secret_access_key=config['secret_access_key'],
                             verify=False)
 
-        self.bucket = s3.Bucket(default_bucket)
+        self.bucket = s3.Bucket(config['default_bucket'])
 
     def uploadFile(self, file, public, id):
         if public:
@@ -36,3 +29,41 @@ class S3Bucket(object):
 
         self.bucket.upload_file(file, str(id), ExtraArgs=extra_args)
         return self.public_endpoint + str(id)
+
+    @staticmethod
+    def get_configuration(config_filename="config/config.env", secret_filename="secret/secret.env"):
+        config = {
+            **dotenv_values(secret_filename),
+            **dotenv_values(config_filename),
+        }
+
+        endpoint = config.get("STORAGE_ENDPOINT")
+        public_endpoint = config.get("STORAGE_PUBLIC_ENDPOINT")
+        default_bucket = config.get("STORAGE_BUCKET")
+        access_key_id = config.get("STORAGE_ACCESS_KEY")
+        secret_access_key = config.get("STORAGE_SECRET")
+
+        return {
+            'endpoint': endpoint,
+            'public_endpoint': public_endpoint,
+            'default_bucket': default_bucket,
+            'access_key_id': access_key_id,
+            'secret_access_key': secret_access_key,
+        }
+
+    @staticmethod
+    def validate_configuration(config):
+        errors = []
+        if not config.get('endpoint', None):
+            errors.append('Storage endpoint (`STORAGE_ENDPOINT`) must be set, not `{}`.'.format(config.get('endpoint', None)))
+        if not config.get('public_endpoint', None):
+            errors.append('Public storage access (`STORAGE_PUBLIC_ENDPOINT`) must be set, not `{}`.'.format(
+                config.get('public_endpoint', None)))
+        if not config.get('default_bucket', None):
+            errors.append('Storage bucket (`STORAGE_BUCKET`) key must be set, not `{}`.'.format(config.get('default_bucket', None)))
+        if not config.get('access_key_id', None):
+            errors.append('Storage access key (`STORAGE_ACCESS_KEY`) must be set, not `{}`.'.format(config.get('access_key_id', None)))
+        if not config.get('secret_access_key', None):
+            errors.append('Storage secret (`STORAGE_SECRET`) must be set, not `{}`.'.format(config.get('secret_access_key', None)))
+        if errors:
+            raise ValueError('The configuration for the S3 bucket is not valid:\n  {}'.format('\n  '.join(errors)))
