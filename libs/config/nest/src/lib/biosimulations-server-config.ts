d95dc08beb496c075da379e566d488820f640a96
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-12-16 13:33:50 -0500

    build: remove extra dependencies to dev

--- libs/config/nest/src/lib/biosimulations-server-config.ts
+++ libs/config/nest/src/lib/biosimulations-server-config.ts
@@ -1,5 +1,5 @@
 import { registerAs } from '@nestjs/config';
-import { urls, appName } from '@biosimulations/config/common';
+import { appName } from '@biosimulations/config/common';
 
 export default registerAs('server', () => {
   // What is the environment?
@@ -8,7 +8,7 @@ export default registerAs('server', () => {
   // The name of the running app
   const app: appName = (process.env.APP as appName) || 'platformApi';
 
-  const host = process.env.HOST || urls[app];
+  const host = process.env.HOST;
 
   const port = process.env.SERVER_PORT
     ? parseInt(process.env.SERVER_PORT)
