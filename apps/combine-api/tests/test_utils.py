f01fa44f30e8dd269147e7b14a09cc499468c787
Jonathan Karr <jonrkarr@gmail.com>
2022-01-07 02:16:25 -0500

    refactor(combine-api): fixed failing test

--- apps/combine-api/tests/test_utils.py
+++ apps/combine-api/tests/test_utils.py
@@ -1,19 +1,11 @@
 from src import utils
+from unittest import mock
 import unittest
 
 
 class UtilsTestCase(unittest.TestCase):
-    def setUp(self):
-        self.bucket = bucket = utils.get_s3_bucket()
-
-        self.delete_files_with_prefix = bucket.delete_files_with_prefix
-
-        def mock_delete_files_with_prefix(prefix=None, max_last_modified=None):
-            pass
-        bucket.delete_files_with_prefix = mock_delete_files_with_prefix
-
-    def tearDown(self):
-        self.bucket.delete_files_with_prefix = self.delete_files_with_prefix
-
     def test_delete_temporary_combine_archives_in_s3_bucket(self):
-        utils.delete_temporary_combine_archives_in_s3_bucket()
+        def delete_files_with_prefix(prefix=None, max_last_modified=None):
+            pass
+        with mock.patch.object(utils, 'get_s3_bucket', return_value=mock.Mock(delete_files_with_prefix=delete_files_with_prefix)):
+            utils.delete_temporary_combine_archives_in_s3_bucket()
