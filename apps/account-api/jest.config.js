6571e5e1a04310e591e71127e22a9a63d9fc450f
Bilal Shaikh <bilalshaikh42@gmail.com>
2022-01-10 11:49:46 -0500

    refactor: remove unused account api

--- apps/account-api/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
-  },
-  coverageDirectory: '../../coverage/apps/account-api',
-  displayName: 'account-api',
-};
