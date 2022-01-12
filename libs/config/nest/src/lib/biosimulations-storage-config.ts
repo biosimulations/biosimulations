9c266430e6361ceb62a62363ea974881c53a0cdf
Bilal Shaikh <bilalshaikh42@gmail.com>
2022-01-12 16:50:52 -0500

    perf(api,dispatch-service): change storage backend to Google Cloud
    change the storage bucket used in the backed from the uconn hpc to the google cloud storage buckets.
    
    closes #4039

--- libs/config/nest/src/lib/biosimulations-storage-config.ts
+++ libs/config/nest/src/lib/biosimulations-storage-config.ts
@@ -3,11 +3,9 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('storage', () => {
   const config = {
     endpoint: process.env.STORAGE_ENDPOINT,
-    externalEndpoint: process.env.STORAGE_EXTERNAL_ENDPOINT,
     accessKey: process.env.STORAGE_ACCESS_KEY,
     secret: process.env.STORAGE_SECRET,
     bucket: process.env.STORAGE_BUCKET,
-    publicEndpoint: process.env.STORAGE_PUBLIC_ENDPOINT,
   };
   return config;
 });
---
+++ libs/shared/storage/src/lib/simulation-storage.service.spec.ts
@@ -0,0 +1,38 @@
+import { Test } from '@nestjs/testing';
+import { BiosimulationsConfigModule } from '@biosimulations/config/nest';
+import { S3Module } from 'nestjs-s3';
+import { SimulationStorageService } from './simulation-storage.service';
+import { SharedStorageService } from './shared-storage.service';
+
+describe('SimulationStorageService', () => {
+  let service: SimulationStorageService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [
+        BiosimulationsConfigModule,
+        S3Module.forRoot({
+          config: {
+            credentials: {
+              accessKeyId: '',
+              secretAccessKey: '',
+            },
+            endpoint: 'https://storage.googleapis.com',
+            s3ForcePathStyle: true,
+            region: 'us-east-1',
+          },
+        }),
+      ],
+      providers: [
+        SimulationStorageService,
+        { provide: SharedStorageService, useValue: {} },
+      ],
+    }).compile();
+
+    service = module.get(SimulationStorageService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeTruthy();
+  });
+});
