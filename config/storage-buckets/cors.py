fa5c8fd929dd8cc53d519fc3433f3381c4dcf06f
Jonathan Karr <jonrkarr@gmail.com>
2022-01-19 10:44:53 -0500

    refactor(config): cleaned up printing

--- config/storage-buckets/cors.py
+++ config/storage-buckets/cors.py
@@ -1,63 +1,65 @@
-from google.cloud import storage
-import os 
-from dotenv import load_dotenv
 from auth import GoogleStorageAuth
+from dotenv import load_dotenv
+from google.cloud import storage
+import os
+import sys
+
+DEV_CORS_SETTINGS = [{
+    "origin": ["*"],
+    "method": ["*"],
+    "maxAgeSeconds": 3600
+}]
+
+# TODO narrow origins for prod
+PROD_CORS_SETTINGS = [{
+    "origin": ["*"],
+    "method": ["*"],
+    "maxAgeSeconds": 3600
+}]
+
+
 def set_cors():
     load_dotenv("config/config.env")
-    
+
     dev_bucket = os.getenv("STORAGE_BUCKET")
-    prod_bucket = os.getenv("STORAGE_BUCKET_PROD")   
+    prod_bucket = os.getenv("STORAGE_BUCKET_PROD")
     temp_dev_bucket = os.getenv("TEMP_STORAGE_BUCKET_DEV")
     temp_prod_bucket = os.getenv("TEMP_STORAGE_BUCKET_PROD")
-              
 
-    dev_cors_settings = [{
-        "origin": ["*"],
-        "method": ["*"],
-        "maxAgeSeconds": 3600
-        }]
-    
-    
-    # TODO narrow origins for prod 
-    prod_cors_settings=  [{
-        "origin": ["*"],
-        "method": ["*"],
-        "maxAgeSeconds": 3600
-        }]
-    
     with GoogleStorageAuth() as auth:
         s3 = storage.Client()
 
-        if(dev_bucket):
-            print("Setting CORS for dev bucket")
-            bucket= s3.get_bucket(dev_bucket)
-            bucket.cors=dev_cors_settings
+        if dev_bucket:
+            print("Setting CORS for dev bucket ({}) to {} ... ".format(dev_bucket, DEV_CORS_SETTINGS), end='')
+            sys.stdout.flush()
+            bucket = s3.get_bucket(dev_bucket)
+            bucket.cors = DEV_CORS_SETTINGS
             bucket.patch()
-            print("Set CORS policies for bucket {} is {}".format(bucket.name, bucket.cors))
-        
-        if(temp_dev_bucket):
-            print("Setting CORS for temp dev bucket")
-            bucket= s3.get_bucket(temp_dev_bucket)
-            bucket.cors=dev_cors_settings
-            bucket.patch()
-            print("Set CORS policies for bucket {} is {}".format(bucket.name, bucket.cors))
-        
+            print("done.")
 
-        if(prod_bucket):
-            print("Setting CORS for prod bucket")
-            bucket= s3.get_bucket(prod_bucket)
-            bucket.cors=prod_cors_settings
+        if temp_dev_bucket:
+            print("Setting CORS for temp dev bucket ({}) to {} ... ".format(temp_dev_bucket, DEV_CORS_SETTINGS), end='')
+            sys.stdout.flush()
+            bucket = s3.get_bucket(temp_dev_bucket)
+            bucket.cors = DEV_CORS_SETTINGS
             bucket.patch()
-            print("Set CORS policies for bucket {} is {}".format(bucket.name, bucket.cors))
-        
-        if(temp_prod_bucket):
-            print("Setting CORS for temp prod bucket")
-            bucket= s3.get_bucket(temp_prod_bucket)
-            bucket.cors=prod_cors_settings
-            bucket.patch()
-            print("Set CORS policies for bucket {} is {}".format(bucket.name, bucket.cors))
-        
+            print("done.")
 
+        if prod_bucket:
+            print("Setting CORS for prod bucket ({}) to {} ... ".format(prod_bucket, PROD_CORS_SETTINGS), end='')
+            sys.stdout.flush()
+            bucket = s3.get_bucket(prod_bucket)
+            bucket.cors = PROD_CORS_SETTINGS
+            bucket.patch()
+            print("done.")
+
+        if temp_prod_bucket:
+            print("Setting CORS for temp bucket ({}) to {} ... ".format(temp_prod_bucket, PROD_CORS_SETTINGS), end='')
+            sys.stdout.flush()
+            bucket = s3.get_bucket(temp_prod_bucket)
+            bucket.cors = PROD_CORS_SETTINGS
+            bucket.patch()
+            print("done.")
 
 
 if __name__ == "__main__":
--- config/storage-buckets/retention.py
+++ config/storage-buckets/retention.py
@@ -1,42 +1,38 @@
-import os
-from google.cloud import storage
-from dotenv import load_dotenv
 from auth import GoogleStorageAuth
+from dotenv import load_dotenv
+from google.cloud import storage
+import os
+import sys
+
 
 def set_retention():
     load_dotenv("config/config.env")
-    
-    # Need to use different storage buckets for lifecycle since google only supports bucket wide lifecycle
+
+    # Need to use different storage buckets for lifecycle since google only supports bucket-wide lifecycles
 
     dev_bucket = os.getenv("TEMP_STORAGE_BUCKET_DEV")
     prod_bucket = os.getenv("TEMP_STORAGE_BUCKET_PROD")
-  
 
-    temp_storage_expiration_days = int(float(os.getenv('TEMP_STORAGE_MAX_AGE', '1')))
-
+    dev_temp_storage_expiration_days = int(float(os.getenv('TEMP_STORAGE_MAX_AGE_DEV', 30)))
+    prod_temp_storage_expiration_days = int(float(os.getenv('TEMP_STORAGE_MAX_AGE_PROD', 1)))
 
     with GoogleStorageAuth() as auth:
         storage_client = storage.Client()
 
-        if(dev_bucket):
-            print("Setting retention for dev bucket")
-            bucket= storage_client.get_bucket(dev_bucket)
-            bucket.add_lifecycle_delete_rule(age=30)
+        if dev_bucket:
+            print("Setting lifecycle delete rule for temporary dev bucket ({}) to {} days ... ".format(
+                dev_bucket, dev_temp_storage_expiration_days), end='')
+            sys.stdout.flush()
+            bucket = storage_client.get_bucket(dev_bucket)
+            bucket.add_lifecycle_delete_rule(age=dev_temp_storage_expiration_days)
             bucket.patch()
-            print("Set retention policies for bucket {} is {}".format(bucket.name, bucket.lifecycle_rules))
-            
-        if(prod_bucket):
-            print("Setting retention for prod bucket")
-            bucket= storage_client.get_bucket(prod_bucket)
-            bucket.add_lifecycle_delete_rule(age=temp_storage_expiration_days)
+            print("done.")
+
+        if prod_bucket:
+            print("Setting lifecycle delete rule for temporary prod bucket ({}) to {} days ... ".format(
+                dev_bucket, prod_temp_storage_expiration_days), end='')
+            sys.stdout.flush()
+            bucket = storage_client.get_bucket(prod_bucket)
+            bucket.add_lifecycle_delete_rule(age=prod_temp_storage_expiration_days)
             bucket.patch()
-            print("Set retention policies for bucket {} is {}".format(bucket.name, bucket.lifecycle_rules))
-
-
-
-    
-    
- 
-    
-
-  
+            print("done.")
