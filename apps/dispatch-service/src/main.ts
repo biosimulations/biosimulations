7192d3f02f485f1acf03e747eeaf682ad005f63e
Bilal Shaikh <bilalshaikh42@gmail.com>
2022-01-26 13:26:10 -0500

    feat(dispatch-service): add ui to monitor queues

--- apps/dispatch-service/src/main.ts
+++ apps/dispatch-service/src/main.ts
@@ -8,9 +8,30 @@ import { NatsOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app/app.module';
 
 import { ConfigService } from '@nestjs/config';
+import Queue from 'bull';
+
+import { createBullBoard } from '@bull-board/api';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
+
+import { ExpressAdapter } from '@bull-board/express';
+import { JobQueue } from '@biosimulations/messages/messages';
 
 async function bootstrap() {
+  const serverAdapter = new ExpressAdapter();
+
+  const queues: any[] = [];
+  Object.values(JobQueue).forEach((value) =>
+    queues.push(new BullAdapter(new Queue(value as string))),
+  );
+
+  const _ = createBullBoard({
+    queues: queues,
+    serverAdapter: serverAdapter,
+  });
   const app = await NestFactory.create(AppModule);
+
+  serverAdapter.setBasePath('/queues');
+  app.use('/queues', serverAdapter.getRouter());
   const configService = app.get(ConfigService);
   const natsUrl = configService.get('nats.url');
   const natsQueue = configService.get('nats.queue');
@@ -25,8 +46,8 @@ async function bootstrap() {
 
   app.connectMicroservice(natsConfig);
   await app.startAllMicroservices();
-
-  app.listen('3334');
+  const port = process.env['PORT'] || 3334;
+  app.listen(port, () => console.log(`Listening on port ${port}`));
 }
 
 bootstrap();
