993ece17d441e03e94e4810266cb3b6b95d51353
John Detloff <jmdetloff@gmail.com>
2022-01-31 01:47:03 -0600

    style(ui): correct whitespace within clipboard.service.ts
    Add EOF newlines to clipboard.service.ts  and clipboard.service.spec.ts

--- libs/shared/angular/src/lib/clipboard/clipboard.service.ts
+++ libs/shared/angular/src/lib/clipboard/clipboard.service.ts
@@ -5,19 +5,19 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ClipboardService {
 
-    public constructor(private snackBar: MatSnackBar) {}
-
-    public copyToClipboard(toCopy: string, snackbarNotification: string): void {
-      if (navigator && navigator.clipboard) {
-        navigator.clipboard.writeText(toCopy);
-      }
-      this.snackBar.open(
-        snackbarNotification,
-        undefined,
-        {
-          duration: SNACK_BAR_DURATION,
-        },
-      );
+  public constructor(private snackBar: MatSnackBar) {}
+  
+  public copyToClipboard(toCopy: string, snackbarNotification: string): void {
+    if (navigator && navigator.clipboard) {
+      navigator.clipboard.writeText(toCopy);
     }
+    this.snackBar.open(
+      snackbarNotification,
+      undefined,
+      {
+        duration: SNACK_BAR_DURATION,
+      },
+    );
+  }
 
 }
