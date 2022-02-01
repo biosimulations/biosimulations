9cd4b19e76382d7ca44e38151feefc0ad07e39bc
Bilal Shaikh <bilalshaikh42@gmail.com>
2022-01-31 19:12:20 -0500

    feat(storage): add datamodel and methods for getting output files
    added methods parralell to getting simulation run contents for simulation run outputs such as output
    archive, logs, hdf file etc.

--- libs/shared/storage/src/lib/file-paths.spec.ts
+++ libs/shared/storage/src/lib/file-paths.spec.ts
@@ -1,6 +1,7 @@
 import { BiosimulationsConfigModule } from '@biosimulations/config/nest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigService } from 'aws-sdk';
+import { OutputFileName } from './datamodel';
 import { FilePaths } from './file-paths';
 
 describe('FilePaths', () => {
@@ -18,6 +19,46 @@ describe('FilePaths', () => {
     expect(filePaths).toBeDefined();
   });
 
+  it('should return correct path for output archive', () => {
+    const runId = 'test-run-id';
+    const expectedPath = 'outputs/output.zip';
+    const path = filePaths.getSimulationRunOutputFilePath(
+      runId,
+      OutputFileName.OUTPUT_ARCHIVE,
+      false,
+    );
+    expect(path).toEqual(expectedPath);
+  });
+  it('should return correct absolute path for output archive', () => {
+    const runId = 'test-run-id';
+    const expectedPath = 'simulations/test-run-id/outputs/output.zip';
+    const path = filePaths.getSimulationRunOutputFilePath(
+      runId,
+      OutputFileName.OUTPUT_ARCHIVE,
+      true,
+    );
+    expect(path).toEqual(expectedPath);
+  });
+  it('should return correct path for raw log', () => {
+    const runId = 'test-run-id';
+    const expectedPath = 'outputs/rawLog.txt';
+    const path = filePaths.getSimulationRunOutputFilePath(
+      runId,
+      OutputFileName.RAW_LOG,
+      false,
+    );
+    expect(path).toEqual(expectedPath);
+  });
+  it('should return correct path for structured Log', () => {
+    const runId = 'test-run-id';
+    const expectedPath = 'outputs/log.yml';
+    const path = filePaths.getSimulationRunOutputFilePath(
+      runId,
+      OutputFileName.LOG,
+      false,
+    );
+    expect(path).toEqual(expectedPath);
+  });
   it('Should return correct S3 URL', () => {
     expect(filePaths.getSimulationRunFileContentEndpoint('x', 'file.txt')).toBe(
       'https://storage.googleapis.com/files.biosimulations.dev/simulations/x/contents/file.txt',
