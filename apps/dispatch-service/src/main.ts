3971d9f86eaad3d39b464691d2845d8f071e736a
Bilal Shaikh <bilalshaikh42@gmail.com>
2022-02-03 10:43:38 -0500

    fix(dispatch-service): fix queue connection for dashboard

--- apps/dispatch-service/src/main.ts
+++ apps/dispatch-service/src/main.ts
@@ -8,31 +8,43 @@ import { NatsOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app/app.module';
 
 import { ConfigService } from '@nestjs/config';
-import Queue from 'bull';
+import { Queue } from 'bullmq';
 
 import { createBullBoard } from '@bull-board/api';
-import { BullAdapter } from '@bull-board/api/bullAdapter';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 
 import { ExpressAdapter } from '@bull-board/express';
 import { JobQueue } from '@biosimulations/messages/messages';
 
-async function bootstrap() {
-  const serverAdapter = new ExpressAdapter();
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
+  const serverAdapter = new ExpressAdapter();
+  const queuehost = configService.get('queue.host');
+  const queueport = configService.get('queue.port');
   const queues: any[] = [];
   Object.values(JobQueue).forEach((value) =>
-    queues.push(new BullAdapter(new Queue(value as string))),
+    queues.push(
+      new BullMQAdapter(
+        new Queue(value, {
+          connection: {
+            host: queuehost,
+            port: queueport,
+          },
+        }),
+      ),
+    ),
   );
 
   const _ = createBullBoard({
     queues: queues,
     serverAdapter: serverAdapter,
   });
-  const app = await NestFactory.create(AppModule);
 
   serverAdapter.setBasePath('/queues');
   app.use('/queues', serverAdapter.getRouter());
-  const configService = app.get(ConfigService);
+
   const natsUrl = configService.get('nats.url');
   const natsQueue = configService.get('nats.queue');
   const natsConfig: NatsOptions = {
