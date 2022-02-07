0bca1785ed8ff3250ae5bcb41cd588f936557466
Bilal Shaikh <bilalshaikh42@gmail.com>
2022-02-07 14:17:19 -0500

    feat: add version to environment

--- libs/shared/environments/src/lib/environment.prod.ts
+++ libs/shared/environments/src/lib/environment.prod.ts
@@ -1,7 +1,8 @@
 import { environmentType } from './environment.type';
-
+import { VERSION } from './version';
 export const environment: environmentType = {
   production: true,
   env: 'prod',
   baseUrl: 'https://api.biosimulations.org',
+  version: VERSION,
 };
--- libs/shared/environments/src/lib/environment.stage.ts
+++ libs/shared/environments/src/lib/environment.stage.ts
@@ -1,7 +1,8 @@
 import { environmentType } from './environment.type';
-
+import { VERSION } from './version';
 export const environment: environmentType = {
   production: true,
   env: 'stage',
   baseUrl: 'https://api.biosimulations.dev',
+  version: VERSION,
 };
