2f3031798d3ba5162a80f9de0f8654bc34c915e4
Jonathan Karr <jonrkarr@gmail.com>
2022-02-28 09:52:10 -0500

    refactor(combine-api): organizing configuration for timeouts

--- apps/combine-api/src/handlers/run/get_simulators.py
+++ apps/combine-api/src/handlers/run/get_simulators.py
@@ -1,6 +1,15 @@
 from . import utils
+from dotenv import dotenv_values
 import functools
 import werkzeug.wrappers.response  # noqa: F401
+
+config = {
+    **dotenv_values("secret/secret.env"),
+    **dotenv_values("config/config.env"),
+    **dotenv_values("shared/shared.env"),
+}
+
+TIMEOUT = float(config.get('COMBINE_API_GET_SIMULATORS_TIMEOUT', 120.))
 
 
 @functools.lru_cache(maxsize=None)
@@ -14,6 +23,6 @@ def handler():
     simulators = []
 
     for sim in utils.get_simulators():
-        simulators.append(utils.exec_in_subprocess(utils.get_simulator_metadata, sim['id'], timeout=20.))
+        simulators.append(utils.exec_in_subprocess(utils.get_simulator_metadata, sim['id'], timeout=TIMEOUT))
 
     return simulators
