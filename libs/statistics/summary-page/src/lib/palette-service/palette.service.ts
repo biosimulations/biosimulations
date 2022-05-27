18346e3db1dee6ac7076bea49b355f6f223fc32c
Jonathan Karr <jonrkarr@gmail.com>
2022-05-27 10:40:47 -0400

    fix(platform): applied our color palette to summary charts

--- libs/statistics/summary-page/src/lib/palette-service/palette.service.ts
+++ libs/statistics/summary-page/src/lib/palette-service/palette.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
-import * as palette2 from 'google-palette';
-
-let palette: (arg: string, count: number) => string[];
+import { srgb, multiColorGradient, css, GradientColorStop, TypedColor } from '@thi.ng/color';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PaletteService {
+  private static palette = ['#951ed9', '#2196f3', '#38cc00', '#ff9800', '#f44336'];
+
   getColorPalette(count: number): string[] {
-    palette = palette2;
-    return palette('tol-rainbow', count).map((hex: string) => `#${hex}`);
+    const nStops = Math.min(count, PaletteService.palette.length);
+    const stopPalette = PaletteService.palette.slice(0, nStops);
+    const stops = stopPalette.map((color: string, iStop: number): GradientColorStop<TypedColor<any>> => {
+      return [iStop / (nStops - 1), srgb(color)];
+    });
+
+    const gradient = multiColorGradient({
+      num: count,
+      stops: stops,
+    });
+
+    const palette = gradient.map(css);
+    console.log(palette);
+    return palette;
   }
+
   constructor() {}
 }
