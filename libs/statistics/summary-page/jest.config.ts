8af18288fa5ee4b4eb38bd1a3f789154a3023da5
Bilal Shaikh <bilalshaikh42@gmail.com>
2022-05-27 17:34:18 -0400

    test: fix tests for summary page

--- libs/statistics/summary-page/jest.config.ts
+++ libs/statistics/summary-page/jest.config.ts
@@ -12,7 +12,8 @@ export default {
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
-  transformIgnorePatterns: ['node_modules/(?!@thi.ng/color)'],
+  transformIgnorePatterns: ['node_modules/(?!(.*\\.mjs$)|(@thi.ng/*))'],
+
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
