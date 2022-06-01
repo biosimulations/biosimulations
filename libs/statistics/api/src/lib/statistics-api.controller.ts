622e42e898d4c586dd4d59fe39078fd9ef9b4c21
Bilal Shaikh <bilalshaikh42@gmail.com>
2022-06-01 14:50:19 -0400

    refactor: create statistics datamodel library

--- libs/statistics/api/src/lib/statistics-api.controller.ts
+++ libs/statistics/api/src/lib/statistics-api.controller.ts
@@ -1,12 +1,69 @@
+import { StatItem, StatsChartType } from '@biosimulations/statistics-datamodel';
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { StatisticsApiService } from './statistics-api.service';
 
 @Controller('')
+@ApiTags('Statistics')
 export class StatisticsApiController {
-  constructor(private statisticsApiService: StatisticsApiService) {}
+  public constructor(private statisticsApiService: StatisticsApiService) {}
 
   @Get('/')
   public getStatistics() {
     return this.statisticsApiService.getStatistics();
+  }
+
+  @Get('models')
+  public getModels(): StatItem[] {
+    return [this.getTaxonomy(), this.getSystems(), this.getModelsFormats()];
+  }
+  @Get('models/taxa')
+  public getTaxonomy() {
+    return this.statisticsApiService.getTaxonomy();
+  }
+
+  @Get('models/systems')
+  public getSystems() {
+    return this.statisticsApiService.getSystems();
+  }
+
+  @Get('models/formats')
+  public getModelsFormats() {
+    return this.statisticsApiService.getModelsFormats();
+  }
+
+  @Get('projects/sizes')
+  public getModelsSizes() {
+    return this.statisticsApiService.getModelsSizes();
+  }
+
+  @Get('projects/contributors')
+  public getContributors() {
+    return this.statisticsApiService.getContributors();
+  }
+
+  @Get('projects/repositories')
+  public getRepositories() {
+    return this.statisticsApiService.getRepositories();
+  }
+
+  @Get('projects/licenses')
+  public getLicenses() {
+    return this.statisticsApiService.getLicenses();
+  }
+
+  @Get('simulations/frameworks')
+  public getFrameworks() {
+    return this.statisticsApiService.getFrameworks();
+  }
+
+  @Get('simulations/algorithms')
+  public getAlgorithms() {
+    return this.statisticsApiService.getAlgorithms();
+  }
+
+  @Get('simulations/tools')
+  public getTools(): StatItem {
+    return this.statisticsApiService.getTools();
   }
 }
---
+++ libs/statistics/datamodel/src/lib/statistics-datamodel.spec.ts
@@ -0,0 +1,7 @@
+import { statisticsDatamodel } from './statistics-datamodel';
+
+describe('statisticsDatamodel', () => {
+  it('should work', () => {
+    expect(statisticsDatamodel()).toEqual('statistics-datamodel');
+  });
+});
