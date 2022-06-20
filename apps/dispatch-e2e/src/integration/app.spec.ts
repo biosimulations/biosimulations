2d50f7872f63207e149bb5c36aaa8143ffdf3896
Bilal Shaikh <bilalshaikh42@gmail.com>
2022-06-20 18:57:08 -0400

    test(dispatch): add e2e tests to test basic loading of modules

--- apps/dispatch-e2e/src/integration/app.spec.ts
+++ apps/dispatch-e2e/src/integration/app.spec.ts
@@ -1,13 +1,26 @@
-import { getGreeting } from '../support/app.po';
-
 describe('dispatch', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to dispatch!');
+  it('should accept the cookies', () => {
+    // accept the cookie prefrences
+    cy.contains('Save').click();
+  });
+  it('should load main page', () => {
+    cy.contains('Easily run biosimulations');
+  });
+  it('should load the run simulations page', () => {
+    cy.get('.button-content').contains('Run a simulation').click();
+    cy.url().should('includes', '/runs/new');
+  });
+  it('should load the your simulations page', () => {
+    cy.get('.button-content').contains('Your simulation runs').click();
+    cy.url().should('includes', '/runs');
+  });
+  it('should load the example simulations', () => {
+    cy.get('.button-content').contains('Try simulation runs').click();
+    cy.url().should('includes', '/runs?try=1');
+    cy.contains('Budding yeast cell cycle (Irons, J Theor Biol, 2009');
   });
 });
