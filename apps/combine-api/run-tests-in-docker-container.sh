77c76647f6f7f27cd2f189bbac2f11a0d8fa4a7b
Jim Schaff <schaff.jim@gmail.com>
2023-04-05 10:05:17 -0400

    fix: combine-api CI test fix, and added nx run combine-api:test_offline
    switched test script from pipenv to poetry
    added 'nx run combine-api:test_offline' for testing local python code within Docker container

--- apps/combine-api/run-tests-in-docker-container.sh
+++ apps/combine-api/run-tests-in-docker-container.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
+set -x
+
+# installs and tests local combine-api inside of docker container
+# need to re-install because local poetry virtual environment is not mounted.
 
 docker run \
   --rm \
   --entrypoint bash \
-  --mount type=bind,source=$(pwd)/apps/combine-api,target=/app/apps/combine-api \
+  --mount type=bind,source=$(pwd)/apps/combine-api,target=/app/combine-api \
   ghcr.io/biosimulations/combine-api:sha-$(git rev-parse HEAD | cut -c 1-7) \
   -c "
-    pipenv install --dev --system --deploy
-    pip uninstall -y matplotlib
-    pip install matplotlib==3.2.0
-    mv src src-ignore
-    export PYTHONPATH=apps/combine-api
-    /bin/bash /xvfb-startup.sh python -m pytest \
-      --verbose \
-      --cov apps/combine-api/combine_api/ --cov-report=xml \
-      apps/combine-api/tests/
-  "
+    cd /app/combine-api
+    poetry install
+    poetry run python -m pytest --verbose /app/tests/
+    "
