eaef2e01470151a36cc38f34cae34aea68e366e3
Jim Schaff <schaff.jim@gmail.com>
2023-06-01 19:16:13 -0400

    feat: add projex app to patch project browsing

---
+++ apps/projex/src/app/components/home/home.component.spec.ts
@@ -0,0 +1,32 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeComponent } from './home.component';
+import { SharedUiModule } from '@biosimulations/shared/ui';
+import { BiosimulationsIconsModule } from '@biosimulations/shared/icons';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ConfigService } from '@biosimulations/config/angular';
+import { HomeModule } from './home.module';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, SharedUiModule, BiosimulationsIconsModule, RouterTestingModule, HomeModule],
+      declarations: [HomeComponent],
+      providers: [ConfigService],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ apps/projex/src/app/components/simulations/view/view.model.ts
@@ -0,0 +1,23 @@
+import { SimulationRunStatus, EnvironmentVariable, Purpose } from '@biosimulations/datamodel/common';
+
+export interface FormattedSimulation {
+  id: string;
+  name: string;
+  simulator: string;
+  simulatorVersion: string;
+  simulatorDigest: string;
+  simulatorUrl: string;
+  cpus: number;
+  memory: string;
+  maxTime: string;
+  envVars: EnvironmentVariable[];
+  purpose: Purpose;
+  status: SimulationRunStatus;
+  statusRunning: boolean;
+  statusSucceeded: boolean;
+  statusFailed: boolean;
+  statusLabel: string;
+  submitted: string;
+  updated: string;
+  // runtime: string;
+}
