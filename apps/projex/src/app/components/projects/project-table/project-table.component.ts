a56767c72b624820adc6f1a3ad48167a7e4b5f48
alex patrie <alexanderpatrie@gmail.com>
2023-06-05 16:32:19 -0400

    feat: added first-page and last-page buttons to Simulation Projects table paginator

--- apps/projex/src/app/components/projects/project-table/project-table.component.ts
+++ apps/projex/src/app/components/projects/project-table/project-table.component.ts
@@ -9,6 +9,7 @@ import { SearchCriteria } from '@biosimulations/angular-api-client';
 import { debounceTime, distinctUntilChanged, switchMap, takeUntil } from 'rxjs/operators';
 import { MatTable } from '@angular/material/table';
 import { ProjectFilterQueryItem, ProjectFilterStatsItem } from '@biosimulations/datamodel/common';
+import { ThemePalette } from '@angular/material/core';
 
 export class ProjectTableDataSource extends DataSource<FormattedProjectSummary> {
   public paginator: MatPaginator | undefined;
@@ -125,6 +126,7 @@ export class ProjectTableComponent implements AfterViewInit {
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   public displayedColumns = ['id', 'title'];
   @Input() searchTerm = '';
+  @Input() color?: ThemePalette = 'accent';
   @Output() searchTermChange: EventEmitter<string> = new EventEmitter<string>();
 
   @Output() filterStats$ = new BehaviorSubject([] as ProjectFilterStatsItem[]);
