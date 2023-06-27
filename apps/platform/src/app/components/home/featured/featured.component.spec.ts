4e8857960c74257697d7b8815562956b98682b85
alex patrie <alexanderpatrie@gmail.com>
2023-06-27 16:55:59 -0400

    fix: added NoopAnimationsModule to home and featured component spec file

--- apps/platform/src/app/components/home/featured/featured.component.spec.ts
+++ apps/platform/src/app/components/home/featured/featured.component.spec.ts
@@ -4,6 +4,7 @@ import { FeaturedComponent } from './featured.component';
 import { FeaturedService } from './featured.service';
 import { HomeModule } from '../home.module';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('FeaturedComponent', () => {
   let component: FeaturedComponent;
@@ -11,7 +12,7 @@ describe('FeaturedComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeModule, RouterTestingModule],
+      imports: [HomeModule, RouterTestingModule, NoopAnimationsModule],
       declarations: [FeaturedComponent],
       providers: [FeaturedService],
     }).compileComponents();
