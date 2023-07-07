c7886b1dad0d1d9e87f56a18f24ba11cff875669
alex patrie <alexanderpatrie@gmail.com>
2023-07-07 11:17:42 -0400

    fix: removed unused color prop and removed comments from project colors

--- libs/shared/ui/src/lib/topbar/topbar-menu.component.ts
+++ libs/shared/ui/src/lib/topbar/topbar-menu.component.ts
@@ -1,11 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'biosimulations-topbar-menu',
   templateUrl: './topbar-menu.component.html',
   styleUrls: ['./topbar-menu.component.scss'],
 })
-export class TopbarMenuComponent {
-  @Input()
-  color!: any;
-}
+export class TopbarMenuComponent {}
