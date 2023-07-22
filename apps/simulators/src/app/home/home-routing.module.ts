b5eeee331639cd7ae898e17b4ce46dd635e987d8
alex patrie <alexanderpatrie@gmail.com>
2023-07-21 21:18:00 -0400

    feat: added browse component to home routing redirection

--- apps/simulators/src/app/home/home-routing.module.ts
+++ apps/simulators/src/app/home/home-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    redirectTo: '/simulators',
+    pathMatch: 'full',
   },
 ];
 
