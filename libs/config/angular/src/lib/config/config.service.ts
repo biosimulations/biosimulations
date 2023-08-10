58754f72736fcfc5d30ef4a38301a9e595a4df61
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2023-08-10 16:25:03 -0400

    chore: adjusted schema of config service to include only truly external

--- libs/config/angular/src/lib/config/config.service.ts
+++ libs/config/angular/src/lib/config/config.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-@Injectable()
+/*@Injectable()
 export class ConfigService {
   appId!: string;
   appName!: string;
@@ -33,6 +33,30 @@ export class ConfigService {
   simulatorsAppUrl!: string;
   simulatorsAppBrowseSimulatorsUrl!: string;
   simulatorsApiUrl!: string;
+  simulatorsNewIssueUrl!: string;
+  simulatorsNewPullUrl!: string;
+  appConfig!: any;
+  analyticsId!: string;
+}*/
+
+@Injectable()
+export class ConfigService {
+  appId!: string;
+  appName!: string;
+  appNameParts!: string[];
+  logo!: string;
+  banner!: string;
+  appUrl!: string;
+  apiUrl!: string;
+  email!: string;
+  tutorialsUrl!: string | null;
+  privacyPolicyVersion = 1;
+  newIssueUrl!: string;
+  newPullUrl!: string;
+  platformNewIssueUrl!: string;
+  platformNewPullUrl!: string;
+  dispatchNewIssueUrl!: string;
+  dispatchNewPullUrl!: string;
   simulatorsNewIssueUrl!: string;
   simulatorsNewPullUrl!: string;
   appConfig!: any;
