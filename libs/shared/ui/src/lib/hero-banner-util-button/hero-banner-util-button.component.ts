772ff26504fb7912c248c4411e1fe411eb847cf4
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2023-08-11 13:12:55 -0400

    feat: implemented and adjusted shared utils button component

--- libs/shared/ui/src/lib/hero-banner-util-button/hero-banner-util-button.component.ts
+++ libs/shared/ui/src/lib/hero-banner-util-button/hero-banner-util-button.component.ts
@@ -8,15 +8,13 @@ import { Component, Input } from '@angular/core';
 export class HeroBannerUtilButtonComponent {
   @Input() public validateSimulatorUrl?: string;
   @Input() public apiUrl?: string;
-  public menuConfig = [
+  public routerLinkMenuConfig = [
     { heading: 'Convert a file', routerLink: ['/utils', 'convert-file'] },
     { heading: 'Create a project', routerLink: ['/utils', 'create-project'] },
     { heading: 'Validate a model', routerLink: ['/utils', 'validate-model'] },
     { heading: 'Validate a simulation', routerLink: ['/utils', 'validate-simulation'] },
     { heading: 'Validate metadata', routerLink: ['/utils', 'validate-metadata'] },
     { heading: 'Validate a project', routerLink: ['/utils', 'validate-project'] },
-    { heading: 'Validate a simulator', href: this.validateSimulatorUrl },
     { heading: 'Suggest a simulator', routerLink: ['/utils', 'suggest-simulator'] },
-    { heading: 'REST API', href: this.apiUrl },
   ];
 }
