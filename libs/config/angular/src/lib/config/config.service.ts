fbe8ca8d51ae24464a7bb2f03815e98e33b70a8c
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2023-08-16 13:42:27 -0400

    chore: removed unused component code

--- libs/config/angular/src/lib/config/config.service.ts
+++ libs/config/angular/src/lib/config/config.service.ts
@@ -1,44 +1,5 @@
 import { Injectable } from '@angular/core';
 
-/*@Injectable()
-export class ConfigService {
-  appId!: string;
-  appName!: string;
-  appNameParts!: string[];
-  logo!: string;
-  banner!: string;
-  appUrl!: string;
-  apiUrl!: string;
-  email!: string;
-  tutorialsUrl!: string | null;
-  privacyPolicyVersion = 1;
-  newIssueUrl!: string;
-  newPullUrl!: string;
-  platformAppUrl!: string;
-  platformApiUrl!: string;
-  platformNewIssueUrl!: string;
-  platformNewPullUrl!: string;
-  dispatchAppUrl!: string;
-  dispatchAppRunsNewUrl!: string;
-  utilsConvertFileUrl!: string;
-  utilsCreateProjectUrl!: string;
-  utilsValidateModelUrl!: string;
-  utilsValidateSimulationUrl!: string;
-  utilsValidateMetadataUrl!: string;
-  utilsValidateProjectUrl!: string;
-  utilsSuggestSimulatorUrl!: string;
-  dispatchApiUrl!: string;
-  dispatchNewIssueUrl!: string;
-  dispatchNewPullUrl!: string;
-  simulatorsAppUrl!: string;
-  simulatorsAppBrowseSimulatorsUrl!: string;
-  simulatorsApiUrl!: string;
-  simulatorsNewIssueUrl!: string;
-  simulatorsNewPullUrl!: string;
-  appConfig!: any;
-  analyticsId!: string;
-}*/
-
 @Injectable()
 export class ConfigService {
   appId!: string;
