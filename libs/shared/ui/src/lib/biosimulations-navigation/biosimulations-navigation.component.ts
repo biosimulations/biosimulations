1b489ac5f31d628eea44d03f3e94792c8f3d4f45
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2023-08-18 10:40:47 -0400

    chore: accessibility added to potentially prevent pipeline errors

--- libs/shared/ui/src/lib/biosimulations-navigation/biosimulations-navigation.component.ts
+++ libs/shared/ui/src/lib/biosimulations-navigation/biosimulations-navigation.component.ts
@@ -9,44 +9,44 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./biosimulations-navigation.component.scss'],
 })
 export class BiosimulationsNavigationComponent implements OnInit {
+  @Input()
+  public appName!: string;
+
+  @Input()
+  public appNameParts!: string[];
+
+  @Input()
+  public logo!: string;
+
+  @Input()
+  public privacyPolicyVersion!: number;
+
+  @Input()
+  public newsVersion!: number;
+
+  @Input()
+  public showNews!: boolean;
+
+  @Input()
+  public healthy!: boolean | null;
+
+  @Input()
+  public darkMode = false;
+
   public useSimulations = true;
 
   public simulationsColor = '#2196f3';
 
   public simulatorsColor = '#ff9800';
 
-  @Input()
-  appName!: string;
-
-  @Input()
-  appNameParts!: string[];
-
-  @Input()
-  logo!: string;
-
-  @Input()
-  privacyPolicyVersion!: number;
-
-  @Input()
-  newsVersion!: number;
-
-  @Input()
-  showNews!: boolean;
-
-  @Input()
-  healthy!: boolean | null;
-
-  @Input()
-  darkMode = false;
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  public isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map((result) => result.matches),
     shareReplay(),
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  public constructor(private breakpointObserver: BreakpointObserver) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     if (this.appName === 'BioSimulators') {
       this.useSimulations = !this.useSimulations;
     }
