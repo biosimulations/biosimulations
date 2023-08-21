6f55021c257204a3bcdad3a37d8ec308f7cd705a
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2023-08-21 15:43:04 -0400

    chore: refactored code to iterate over shared collection and implemented

--- libs/shared/ui/src/lib/app-footer/app-footer.component.ts
+++ libs/shared/ui/src/lib/app-footer/app-footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BiosimulationsIcon } from '@biosimulations/shared/icons';
 
 interface FooterItemInterface {
   cssClass: string;
@@ -25,24 +26,55 @@ class FooterItem implements FooterItemInterface {
   }
 }
 
+class HomeSubsectionItem {
+  public heading: string;
+  public icon: BiosimulationsIcon;
+  public contentItems: FooterItem[];
+  public subheadingSize = 'small';
+  public subheading = '';
+  public _id = 'subsection';
+  public style = 'justify-content: center';
+
+  public constructor(heading: string, icon: BiosimulationsIcon, contentItems: FooterItem[]) {
+    this.heading = heading;
+    this.icon = icon;
+    this.contentItems = contentItems;
+  }
+}
+
+const helpFooterItems = [
+  new FooterItem('faq', 'users/faqs/', 'Frequently Asked Questions'),
+  new FooterItem('contact', 'about/contact/', 'Contact Us'),
+];
+
+const conventionsFooterItems = [
+  new FooterItem('standards', 'concepts/conventions/', 'Standards'),
+  new FooterItem('specs', 'concepts/conventions/simulator-capabilities/', 'Simulator Specs'),
+  new FooterItem('interfaces', 'concepts/conventions/simulator-interface/', 'Simulator Interfaces'),
+  new FooterItem('images', 'concepts/conventions/simulator-images/', 'Simulator Images'),
+  new FooterItem('reports', 'concepts/conventions/simulations-run-reports/', 'Simulation Reports'),
+  new FooterItem('viz', 'concepts/conventions/simulation-run-visualizations/', 'Data Visualizations'),
+  new FooterItem('metadata', 'concepts/conventions/simulation-project-metadata/', 'Simulation Metadata'),
+  new FooterItem('logs', 'concepts/conventions/simulation-run-logs/', 'Simulation Logs'),
+];
+
+const aboutFooterItems = [
+  new FooterItem('about', '', 'About'),
+  new FooterItem('terms', 'about/terms/', 'Terms of Service'),
+  new FooterItem('privacy', 'about/privacy', 'Privacy Policy'),
+  new FooterItem('status', '', 'Status'),
+  new FooterItem('vivarium', '', '@Vivarium'),
+];
+
 @Component({
   selector: 'biosimulations-app-footer',
   templateUrl: './app-footer.component.html',
   styleUrls: ['./app-footer.component.scss'],
 })
 export class AppFooterComponent {
-  public helpFooterItems = [
-    new FooterItem('faq', 'users/faqs/', 'Frequently Asked Questions'),
-    new FooterItem('contact', 'about/contact/', 'Contact Us'),
-  ];
-
-  //public conventionFooterItems!: FooterItem[];
-
-  public aboutFooterItems = [
-    new FooterItem('about', '', 'About'),
-    new FooterItem('terms', 'about/terms/', 'Terms of Service'),
-    new FooterItem('privacy', 'about/privacy', 'Privacy Policy'),
-    new FooterItem('status', '', 'Status'),
-    new FooterItem('vivarium', '', '@Vivarium'),
+  public homeSubsectionItems = [
+    new HomeSubsectionItem('Help', 'help', helpFooterItems),
+    new HomeSubsectionItem('Conventions', 'info', conventionsFooterItems),
+    new HomeSubsectionItem('About', 'info', aboutFooterItems),
   ];
 }
