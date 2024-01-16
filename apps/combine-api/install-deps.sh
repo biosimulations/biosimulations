f6aab9b1ac6cf5a16e90ab4ab82722ec5fa3b88d
alexPatrie <alexanderpatrie@gmail.com>
2024-01-16 07:36:52 -0500

    chore: updated installer

--- apps/combine-api/install-deps.sh
+++ apps/combine-api/install-deps.sh
@@ -1,6 +1,10 @@
 #!/bin/bash 
 
+# poetry run python --version && poetry run python <-- verify python version for env and open the shell
+# poetry add <PACKAGE> || pip install <PACKAGE>  <-- installs package
+
 poetry env use python3.10
+pip install --upgrade pip
 poetry lock 
 
 if poetry install; then 
