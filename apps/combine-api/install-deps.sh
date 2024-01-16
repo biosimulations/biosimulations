031cab563886953d8f4842ede066a3bb192a5866
alexPatrie <alexanderpatrie@gmail.com>
2024-01-16 10:38:55 -0500

    chore: updated scripts and deps

--- apps/combine-api/install-deps.sh
+++ apps/combine-api/install-deps.sh
@@ -1,15 +1,43 @@
-#!/bin/bash 
+#!/bin/zsh
 
 # poetry run python --version && poetry run python <-- verify python version for env and open the shell
 # poetry add <PACKAGE> || pip install <PACKAGE>  <-- installs package
 
-poetry env use python3.10
-pip install --upgrade pip
-poetry lock 
+set -e 
 
-if poetry install; then 
-   echo "Poetry successfully installed all packages."
+poetry env use python3.10
+poetry run pip install --upgrade pip 
+poetry lock
+
+if poetry install -v; then
+   if poetry run ./install-smoldyn.sh; then
+      echo "Poetry installed Smoldyn"
+   else 
+      echo "Smoldyn could not be installed"
+      exit 1
+   fi
+   if poetry run pip install biosimulators-simularium==0.5.3; then
+      echo "Poetry successfully installed all packages."
+   else
+      echo "Poetry could not install biosimulators-simularium"
+      exit 1
+   fi
 else
-   echo "Poetry failed to install all packages. Attempting to install Smoldyn manually..."
-   ./install-smoldyn.sh
+   echo "Poetry failed to successfully install all packages"
+   exit 1
 fi 
+
+# if poetry install; then 
+#    echo "Poetry successfully installed all packages."
+# else
+#    echo "Poetry failed to install all packages. Attempting to install Smoldyn manually..."
+#    poetry run pip install --upgrade pip
+#    if poetry run ./install-smoldyn.sh; then
+#        echo "Smoldyn manually installed successfully."
+#        poetry install
+#        echo "Poetry successfully install all packages"
+#    else
+#        echo "Failed to manually install Smoldyn."
+#        exit 1
+#    fi
+# fi 
--- apps/combine-api/uninstall.sh
+++ apps/combine-api/uninstall.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
 
-poetry env remove combine2-api-dM9t1Ilw-py3.10
+if sudo poetry env remove combine2-api-dM9t1Ilw-py3.10; then
+   echo "Python 3.10 env successfully removed."
+else
+   sudo poetry env remove combine2-api-dM9t1Ilw-py3.9
+fi
