512bded53cf35e19fa5f412033d4d71113469f00
alexPatrie <alexanderpatrie@gmail.com>
2024-01-17 15:21:40 -0500

    chore: updated installer and pyproject for simulariumio

--- apps/combine-api/install-deps.sh
+++ apps/combine-api/install-deps.sh
@@ -7,24 +7,24 @@
 # poetry cache list <-- view all the caches by name
 # poetry cache clear <CACHE NAME> --all <-- removes all the files from the relative cache.
 
-set -e 
+set -e
 
 # TODO: read this from pyproject.toml
 echo "What python version would you like to use for this environment?: "
 read -r python_version
 
-poetry env use python${python_version}
-poetry run pip install --upgrade pip 
+poetry env use python"${python_version}"
+poetry run pip install --upgrade pip
 poetry lock --no-update
 
 if poetry install -v; then
    if poetry run ./install-smoldyn.sh; then
       echo "Poetry installed Smoldyn"
-   else 
+   else
       echo "Smoldyn could not be installed"
       exit 1
    fi
-   if poetry run pip install biosimulators-simularium==0.5.3; then
+   if poetry run pip install biosimulators-simularium==0.5.5; then
       echo "Poetry successfully installed all packages."
    else
       echo "Poetry could not install biosimulators-simularium."
@@ -33,4 +33,4 @@ if poetry install -v; then
 else
    echo "Poetry failed to successfully install all packages"
    exit 1
-fi 
+fi
