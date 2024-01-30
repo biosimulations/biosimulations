2631de5b4eed5b4a1a7c5bf2d20912849671cf1d
Jim Schaff <schaff.jim@gmail.com>
2024-01-29 20:17:31 -0500

    fix: use async DynamicModule with config to initialize simdata-api path

--- libs/simdata-api/nest-client-wrapper/src/lib/simdata-api-nest-client-wrapper.module.ts
+++ libs/simdata-api/nest-client-wrapper/src/lib/simdata-api-nest-client-wrapper.module.ts
@@ -1,12 +1,43 @@
-import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
-import { BiosimulationsConfigModule } from '@biosimulations/config/nest';
-
+import { Abstract, Global, Logger, Module, Type } from '@nestjs/common';
+import { ApiModule, Configuration as SimdataAPIConfiguration } from '@biosimulations/simdata-api-nest-client';
+import { ConfigService } from '@nestjs/config';
 import { SimulationHDFService } from './dataset.service';
-import { ApiModule } from '@biosimulations/simdata-api-nest-client';
+import { BiosimulationsConfigModule } from '@biosimulations/config/nest';
+import { HttpModule } from '@nestjs/axios';
+import { Endpoints } from '@biosimulations/config/common';
 
+export { SimdataAPIConfiguration };
+
+export type SimdataAPIConnectionOptionsFactory = (...args: any[]) => SimdataAPIConfiguration;
+
+export interface SimdataAPIConnectionAsyncOptions {
+  imports: any[];
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  inject: (string | symbol | Function | Type<any> | Abstract<any>)[];
+  useFactory: SimdataAPIConnectionOptionsFactory;
+}
+
+@Global()
 @Module({
-  imports: [HttpModule, BiosimulationsConfigModule, ApiModule],
+  imports: [
+    HttpModule,
+    BiosimulationsConfigModule,
+    ApiModule.forRootAsync({
+      imports: [BiosimulationsConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const env = configService.get('server.env');
+        const endpoints = new Endpoints(env);
+        const simdataBaseUrl = endpoints.getSimdataApiBaseUrl(false);
+        const logger = new Logger(SimdataApiNestClientWrapperModule.name);
+        logger.log(`Using Combine API: ${simdataBaseUrl}`);
+        return new SimdataAPIConfiguration({
+          basePath: simdataBaseUrl,
+        });
+      },
+    })
+  ],
+  controllers: [],
   providers: [SimulationHDFService],
   exports: [SimulationHDFService],
 })
