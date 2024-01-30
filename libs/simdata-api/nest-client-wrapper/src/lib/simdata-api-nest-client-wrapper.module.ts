5689356fb6d355c0a74f00b54621d2b3346da47d
BioSimulations-Daemon <biosimulationsdaemon@gmail.com>
2024-01-30 12:36:47 +0000

    chore: format files. [skip ci]
    Signed-off-by: BioSimulations-Daemon <biosimulationsdaemon@gmail.com>

--- libs/simdata-api/nest-client-wrapper/src/lib/simdata-api-nest-client-wrapper.module.ts
+++ libs/simdata-api/nest-client-wrapper/src/lib/simdata-api-nest-client-wrapper.module.ts
@@ -35,7 +35,7 @@ export interface SimdataAPIConnectionAsyncOptions {
           basePath: simdataBaseUrl,
         });
       },
-    })
+    }),
   ],
   controllers: [],
   providers: [SimulationHDFService],
