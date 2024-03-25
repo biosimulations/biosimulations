affa3cfea8088f8b324f2aff05ab1a0657fb81fa
codefactor-io <support@codefactor.io>
2024-03-25 22:39:06 +0000

    [CodeFactor] Apply fixes to commit 2cc47f4

--- apps/platform/src/app/components/run/dispatch/dispatch.component.ts
+++ apps/platform/src/app/components/run/dispatch/dispatch.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import {
   UntypedFormBuilder,
