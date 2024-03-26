61fb13d3858490ede58b3c6d5d27465d0e008559
alexPatrie <alexanderpatrie@gmail.com>
2024-03-26 18:26:44 -0400

    chore: checkpoint save to edit

--- libs/shared/ui/src/lib/run-custom-simulation/run-custom-simulation.component.ts
+++ libs/shared/ui/src/lib/run-custom-simulation/run-custom-simulation.component.ts
@@ -3,7 +3,7 @@ import { UntypedFormArray, UntypedFormBuilder, UntypedFormGroup, Validators, Abs
 import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
-import { SedSimulation } from '@biosimulations/datamodel/common';
+import { SedSimulation, SedUniformTimeCourseSimulation, SimulationType } from '@biosimulations/datamodel/common';
 import {
   CombineArchiveSedDocSpecsContent,
   CombineArchiveSedDocSpecs,
@@ -18,8 +18,11 @@ import {
   CustomizableSedDocumentData,
   CustomSimulationDatasource,
   FormStepData,
+  ReRunQueryParams,
   SEDML_ID_VALIDATOR,
   UNIQUE_ATTRIBUTE_VALIDATOR_CREATOR,
+  SimMethodData,
+  ModelData,
 } from '@biosimulations/shared/services';
 
 @Component({
@@ -28,16 +31,24 @@ import {
   styleUrls: ['./run-custom-simulation.component.scss'],
 })
 export class RunCustomSimulationComponent implements OnInit, OnChanges {
-  public formGroup: UntypedFormGroup;
-  public introspectionData$?: Observable<CustomizableSedDocumentData | null> | any;
-  //public introspectedData?: CustomizableSedDocumentData;
-  public dataSource!: CustomSimulationDatasource;
-  public formArray: UntypedFormArray | any;
+  // data loaded from rerun query params
+  public reRunParams!: ReRunQueryParams;
+  public simMethodData!: SimMethodData;
+  public modelData!: ModelData;
+  public simulationDataSource!: CustomSimulationDatasource;
+  public formArray: UntypedFormArray;
+  public sedDoc!: SedDocument;
+  public simType!: SimulationType;
+
+  // network data
+  public reRunSedData$!: Observable<CombineArchiveSedDocSpecs | undefined>;
+  public introspectionData$?: Observable<CustomizableSedDocumentData | any>;
+
+  // lifecycle
   public nextClicked = false;
-  public isReRun = false;
-  public reRunSedParams!: Observable<CombineArchiveSedDocSpecs | undefined>;
   public subscriptions: Subscription[] = [];
 
+  // input received from dispatch
   @Input() public archive?: File;
 
   public constructor(
@@ -46,55 +57,43 @@ export class RunCustomSimulationComponent implements OnInit, OnChanges {
     private route: ActivatedRoute,
     private simulationService: SharedSimulationService,
   ) {
-    this.formGroup = this.formBuilder.group({
-      projectUrl: ['', []],
-      modelSource: ['', []],
-      modelFormats: ['', []],
-      simulationAlgorithms: ['', []],
-      simType: ['', []],
-      formArray: this.formBuilder.array([], {
-        validators: [UNIQUE_ATTRIBUTE_VALIDATOR_CREATOR('id')],
-      }),
+    this.formArray = this.formBuilder.array([], {
+      validators: [UNIQUE_ATTRIBUTE_VALIDATOR_CREATOR('id')],
     });
-    this.formArray = this.formGroup.get('formArray') as UntypedFormArray;
-  }
-
-  private setControlsFromParams(params: Params): void {
-    if (!params) {
-      return;
-    }
-    this.setProject(params.projectUrl);
-    console.log(`simulator set!`);
-  }
-
-  private setProject(projectUrl: string): void {
-    if (!projectUrl) {
-      return;
-    }
-    this.formGroup.controls.projectUrl.setValue(projectUrl);
-    console.log(`The project is set!`);
   }
 
   public ngOnInit() {
     this.addDefaultFields();
 
-    // 1. set the datasource(CustomSimulationDataSource) by deriving vals from reRun Query params
+    /* 1. Set the datasource(CustomSimulationDataSource) by deriving vals from reRun Query params.
+      These are expected by Dispatch. overview -> changes -> dispatch(with changes)*/
     this.route.queryParams.subscribe((params) => {
-      this.setControlsFromParams(params);
-      if (params.projectUrl) {
-        this.setProject(params?.projectUrl);
-      }
+      this.reRunParams = {
+        projectUrl: params?.projectUrl,
+        simulator: params?.simulator,
+        simulatorVersion: params?.simulatorVersion,
+        files: params?.files,
+      };
     });
 
+    /* 2. Introspect datasource */
     if (this.archive) {
-      this.reRunSedParams = this.simulationService.getSpecsOfSedDocsInCombineArchive(this.archive);
-      const sub = this.reRunSedParams.subscribe(this.archiveSedDocSpecsLoaded.bind(this));
+      // 1. Set the simMethod data and modelData from simService.getspecsofseddocs
+      const reRunSedData$ = this.simulationService.getSpecsOfSedDocsInCombineArchive(this.archive);
+      reRunSedData$.subscribe((sedDocSpecs: CombineArchiveSedDocSpecs | undefined) => {
+        this.archiveSedDocSpecsLoaded(sedDocSpecs);
+      });
+      const sub = this.reRunSedData$.subscribe(this.archiveSedDocSpecsLoaded.bind(this));
       this.subscriptions.push(sub);
     }
 
-    console.log(`THE url val: ${this.formGroup.get('projectUrl')?.value}`);
+    // first, instantiate datasource with rerun params as they are coming from service
+    this.simulationDataSource = {
+      reRunParams: this.reRunParams,
+    };
+    console.log(`THE url val from rerunQuery: ${this.reRunParams.projectUrl}`);
 
-    this.introspectionData$ = this.introspectionProvider(this.dataSource);
+    this.introspectionData$ = this.introspectionProvider();
 
     // 1. Set the datasource (CustomSimulationDataSource) by deriving vals from reRunProject query params
     // 2. set this.introspectedData$ = this.introspectionProvider(customDataSource) , coming from file.
@@ -113,25 +112,23 @@ export class RunCustomSimulationComponent implements OnInit, OnChanges {
     }
   }
 
-  private introspectionProvider(dataSource: CustomSimulationDatasource): Observable<void | null> | null {
+  private introspectionProvider(): Observable<void | null> | null {
     const errorHandler = this.showIntrospectionFailedSnackbar.bind(this);
-    const formatData = dataSource?.modelData;
-    const simMethodData = dataSource?.simMethodData;
+    const formatData = this.simulationDataSource?.modelData;
+    const simMethodData = this.simulationDataSource?.simMethodData;
     const introspectionObservable = this.simulationService.introspectNewProject(
       this.httpClient,
       formatData,
       simMethodData,
       errorHandler,
     );
-
-    console.log(`INTROSPECTION PROVIDER EVOKED IN CREATE PROJECT: ${introspectionObservable}`);
     if (!introspectionObservable) {
       return null;
     }
     return introspectionObservable.pipe(
       map((introspectionData: CustomizableSedDocumentData | null) => {
         if (introspectionData) {
-          dataSource.introspectedData = introspectionData;
+          this.simulationDataSource.introspectedData = introspectionData;
         }
       }),
     );
@@ -242,51 +239,24 @@ export class RunCustomSimulationComponent implements OnInit, OnChanges {
   }
 
   public archiveSedDocSpecsLoaded(sedDocSpecs?: CombineArchiveSedDocSpecs): void {
-    const modelFormats = new Set<string>();
-    const modelSource: string[] = [];
-    const simType: string[] = [];
     const simulationAlgorithms = new Set<string>();
-    let specsContainUnsupportedModel = false;
+    const simulations = new Set<SedSimulation>();
 
-    //  VALIDATE: Confirm that every model and algorithm within the sed doc spec is supported.
+    //  get the sed doc
     sedDocSpecs?.contents.forEach((content: CombineArchiveSedDocSpecsContent, contentIndex: number): void => {
       const sedDoc: SedDocument = content.location.value;
-      //this.loadData(sedDoc);
-      sedDoc.models.forEach((model: SedModel, modelIndex: number): void => {
-        let edamId: string | null = null;
-        for (const modelingFormat of BIOSIMULATIONS_FORMATS) {
-          const sedUrn = modelingFormat?.biosimulationsMetadata?.modelFormatMetadata?.sedUrn;
-          if (!sedUrn || !modelingFormat.id || !model.language.startsWith(sedUrn)) {
-            continue;
-          }
-          edamId = modelingFormat.id;
-        }
-        if (edamId) {
-          modelFormats.add(edamId);
-        } else {
-          specsContainUnsupportedModel = true;
-        }
-        // set model source for project introspection
-        modelSource.push(model.source);
-      });
-      // SET SIMULATION ALGS
-      sedDoc.simulations.forEach((sim: SedSimulation): void => {
-        const kisaoId = sim.algorithm.kisaoId;
-        simulationAlgorithms.add(kisaoId);
-        simType.push(sim._type);
-        // FILL DATA
-      });
+      this.sedDoc = sedDoc;
     });
 
-    this.formGroup.controls.modelSource.setValue(modelSource);
-    this.formGroup.controls.modelFormats.setValue(Array.from(modelFormats));
-    this.formGroup.controls.simulationAlgorithms.setValue(Array.from(simulationAlgorithms));
-    this.formGroup.controls.simType.setValue(simType);
+    // get the simType
+    this.sedDoc.simulations.forEach((sim: SedSimulation): void => {
+      const kisaoId = sim.algorithm.kisaoId;
+      simulationAlgorithms.add(kisaoId);
+      simulations.add(sim);
+      this.simType = sim._type as SimulationType;
+    });
 
-    console.log(`THE model FORMATS set in group: ${this.formGroup.get('modelFormats')?.value}`);
-    console.log(`The model source set in group: ${this.formGroup.get('modelSource')?.value}`);
-    console.log(`THE sims set in group: ${this.formGroup.get('simType')?.value}`);
-
+    const customizeableData = this.simulationService.createCustomizableSedDocumentData(this.sedDoc, this.simType);
     console.log(`SED DOCS LOADED`);
   }
 
