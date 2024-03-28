6af0b0aa0a9a83e271c18318cc644eb20320f6db
codefactor-io <support@codefactor.io>
2024-03-28 20:22:22 +0000

    [CodeFactor] Apply fixes to commit 1a3dacb

--- libs/simulation-project-utils/simulation-project-utils/src/lib/ui/create-project/form-steps/uniform-time-course-simulation.component.ts
+++ libs/simulation-project-utils/simulation-project-utils/src/lib/ui/create-project/form-steps/uniform-time-course-simulation.component.ts
@@ -2,7 +2,6 @@ import { Component, OnChanges, SimpleChanges } from '@angular/core';
 import { UntypedFormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
 import { IFormStepComponent, FormStepData } from '../create-project/forms';
 import {
-  POSITIVE_INTEGER_VALIDATOR,
   NON_NEGATIVE_FLOAT_VALIDATOR,
   UNIFORM_TIME_SPAN_VALIDATOR,
 } from '@biosimulations/shared/ui';
