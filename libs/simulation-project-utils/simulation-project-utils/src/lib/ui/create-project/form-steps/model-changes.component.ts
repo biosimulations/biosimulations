2dd64f85865625a64419bc85c053f851ab0e9257
alexPatrie <alexanderpatrie@gmail.com>
2024-03-29 09:03:53 -0400

    fix: updated config

--- libs/simulation-project-utils/simulation-project-utils/src/lib/ui/create-project/form-steps/model-changes.component.ts
+++ libs/simulation-project-utils/simulation-project-utils/src/lib/ui/create-project/form-steps/model-changes.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component } from '@angular/core';
 import { UntypedFormArray, UntypedFormBuilder, Validators, UntypedFormGroup, AbstractControl } from '@angular/forms';
 import { IFormStepComponent, FormStepData } from '../create-project/forms';
 import { UNIQUE_ATTRIBUTE_VALIDATOR_CREATOR, SEDML_ID_VALIDATOR } from '@biosimulations/shared/ui';
@@ -9,32 +9,14 @@ import { SedModelAttributeChangeTypeEnum, SedModelChange } from '@biosimulations
   templateUrl: './model-changes.component.html',
   styleUrls: ['./form-steps.scss'],
 })
-export class ModelChangesComponent implements IFormStepComponent, OnChanges, OnInit {
+export class ModelChangesComponent implements IFormStepComponent {
   public nextClicked = false;
-  public formArray!: UntypedFormArray;
-  public isReRun = false;
-  @Input() public sharedFormArray?: UntypedFormArray;
+  public formArray: UntypedFormArray;
 
   public constructor(private formBuilder: UntypedFormBuilder) {
-    this.formArray = this.formBuilder.array([], {
+    this.formArray = formBuilder.array([], {
       validators: [UNIQUE_ATTRIBUTE_VALIDATOR_CREATOR('id')],
     });
-    this.addDefaultFields();
-  }
-
-  public ngOnInit() {
-    if (this.sharedFormArray) {
-      this.formArray = this.sharedFormArray;
-      console.log(`Shared form array set!`);
-    }
-  }
-
-  public ngOnChanges(changes: SimpleChanges) {
-    /* TODO: Extract changes from sharedFormArray here */
-  }
-
-  private addDefaultFields(): void {
-    /* Set placeholder fields on init */
     const defaultRowCount = 3;
     for (let i = 0; i < defaultRowCount; i++) {
       this.addModelChangeField();
