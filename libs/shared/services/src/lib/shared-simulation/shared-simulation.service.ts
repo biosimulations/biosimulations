35fefc6bcc9dc3ca664fb73315ca33163795827b
alexPatrie <alexanderpatrie@gmail.com>
2024-04-02 06:52:12 -0400

    chore: removed duplicate and unused from shared service

--- libs/shared/services/src/lib/shared-simulation/shared-simulation.service.ts
+++ libs/shared/services/src/lib/shared-simulation/shared-simulation.service.ts
@@ -1,8 +1,7 @@
-import { AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
 import { Observable, of, BehaviorSubject, combineLatest, throwError, switchMap } from 'rxjs';
-import { catchError, map, concatAll, debounceTime, shareReplay } from 'rxjs/operators';
+import { catchError, map, debounceTime, shareReplay } from 'rxjs/operators';
 import { HttpClient, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
-import { SimulationType, CommonFile } from '@biosimulations/datamodel/common';
+import { CommonFile } from '@biosimulations/datamodel/common';
 import {
   CombineArchiveSedDocSpecs,
   CombineArchiveSedDocSpecsContent,
@@ -10,31 +9,14 @@ import {
   SedModel,
   SedSimulation,
 } from '@biosimulations/combine-api-angular-client';
-import {
-  SedModelChange,
-  SedModelAttributeChangeTypeEnum,
-  SedVariable,
-  SedUniformTimeCourseSimulation,
-  Namespace,
-  SedOutput,
-  SedReportTypeEnum,
-  SedReport,
-  SedDataGenerator,
-  SedDataSet,
-  SedUniformTimeCourseSimulationTypeEnum,
-} from '@biosimulations/combine-api-angular-client';
-import { BIOSIMULATIONS_FORMATS, BIOSIMULATIONS_FORMATS_BY_ID } from '@biosimulations/ontology/extra-sources';
+import { SedUniformTimeCourseSimulation } from '@biosimulations/combine-api-angular-client';
+import { BIOSIMULATIONS_FORMATS } from '@biosimulations/ontology/extra-sources';
 import { environment } from '@biosimulations/shared/environments';
 import { Injectable } from '@angular/core';
-import { forkJoin, Subject } from 'rxjs';
+import { forkJoin } from 'rxjs';
 import { Router } from '@angular/router';
 import { SimulationRunStatus } from '@biosimulations/datamodel/common';
-import {
-  ISimulation,
-  Simulation,
-  isUnknownSimulation,
-  SimulationStatusService,
-} from '../shared-simulation-status/shared-simulation-status.service';
+import { ISimulation, SimulationStatusService } from '../shared-simulation-status/shared-simulation-status.service';
 import { Storage } from '@ionic/storage-angular';
 import { ConfigService } from '@biosimulations/config/angular';
 import { SimulationRun } from '@biosimulations/datamodel/common';
@@ -68,65 +50,6 @@ export interface ReRunQueryParams {
   sedFileUrl?: string;
 }
 
-export interface CustomizableSedDocumentData {
-  modelChanges: SedModelChange[];
-  modelVariables: SedVariable[];
-  uniformTimeCourseSimulation?: SedUniformTimeCourseSimulation;
-  namespaces: Namespace[];
-}
-
-export interface SimMethodData extends FormStepData {
-  simulationType?: SimulationType | string;
-  algorithm?: string;
-  framework?: string;
-}
-
-export interface ModelData extends FormStepData {
-  modelFormat?: string;
-  modelFile?: Blob | File | string;
-  modelUrl?: string;
-  modelChanges?: SedModelChange[];
-  modelLanguage?: string; // urn identifier
-}
-
-export interface CustomSimulationDatasource {
-  simMethodData: SimMethodData;
-  modelData: ModelData;
-  introspectedData?: CustomizableSedDocumentData;
-  reRunParams?: ReRunQueryParams;
-  modelUrl?: string;
-}
-
-// -- SHARED FUNCTIONS
-
-export const UNIQUE_ATTRIBUTE_VALIDATOR_CREATOR = function (attrName: string): ValidatorFn {
-  return function (control: AbstractControl): ValidationErrors | null {
-    const values = control.value;
-
-    const attributes = values.map((value: any): string => {
-      return value?.[attrName];
-    });
-
-    const uniqueValues = new Set<string>(attributes);
-    if (uniqueValues.size === values.length) {
-      return null;
-    }
-
-    const error: ValidationErrors = {};
-    error[attrName + 'Unique'] = true;
-    return error;
-  };
-};
-
-export const SEDML_ID_VALIDATOR: ValidatorFn = function (control: AbstractControl): ValidationErrors | null {
-  const idPattern = /^[a-z_][a-z0-9_]+$/i;
-  const value = control.value;
-  if (value && control.value.match(idPattern)) {
-    return null;
-  }
-  return { validSedmlId: true };
-};
-
 // -- SHARED SIMULATION SERVICE IMPLEMENTATION
 
 @Injectable({
@@ -135,10 +58,6 @@ export const SEDML_ID_VALIDATOR: ValidatorFn = function (control: AbstractContro
 export class SharedSimulationService {
   private key = 'simulations';
   private simulations: ISimulation[] = [];
-  public reRunQueryParams: Subject<ReRunQueryParams> = new Subject();
-  public reRunObservable!: Observable<ReRunQueryParams>;
-  public reRunTriggered = false;
-
   // Memory/HTTP cache
   private simulationsMap$: { [key: string]: BehaviorSubject<ISimulation> } = {};
   private simulationsMapSubject = new BehaviorSubject(this.simulationsMap$);
@@ -262,7 +181,7 @@ export class SharedSimulationService {
                     }
                     edamId = modelingFormat.id;
                   }
-                  queryParams.modelFormat = edamId ? edamId : 'format_2585'; // default is sbml TODO: Change this
+                  queryParams.modelFormat = edamId ? edamId : 'format_2585';
                 });
 
                 sedDoc.simulations.forEach((sim: SedSimulation): void => {
@@ -291,18 +210,6 @@ export class SharedSimulationService {
           }
         });
       });
-  }
-
-  private parseDates(simulations: ISimulation[]) {
-    simulations.forEach((simulation: ISimulation): void => {
-      if (typeof simulation.submitted === 'string') {
-        simulation.submitted = new Date(simulation.submitted);
-      }
-      if (typeof simulation.updated === 'string') {
-        simulation.updated = new Date(simulation.updated);
-      }
-    });
-    return simulations;
   }
 
   /**
@@ -348,19 +255,16 @@ export class SharedSimulationService {
     }
   }
 
-  public storeNewLocalSimulation(simulation: Simulation): void {
-    this.storeSimulations([simulation]);
-    this.addSimulation(simulation);
-  }
-
-  public storeExistingExternalSimulations(simulations: ISimulation[]): void {
-    simulations = this.parseDates(simulations);
-    simulations.forEach((simulation) => {
-      simulation.submittedLocally = false;
-      this.addSimulation(simulation);
-      this.storeSimulations([simulation]);
+  private parseDates(simulations: ISimulation[]) {
+    simulations.forEach((simulation: ISimulation): void => {
+      if (typeof simulation.submitted === 'string') {
+        simulation.submitted = new Date(simulation.submitted);
+      }
+      if (typeof simulation.updated === 'string') {
+        simulation.updated = new Date(simulation.updated);
+      }
     });
-    this.storeSimulations(simulations);
+    return simulations;
   }
 
   /**
@@ -396,33 +300,6 @@ export class SharedSimulationService {
     for (const sim of this.simulations) {
       this.updateSimulation(sim.id);
     }
-  }
-
-  /**
-   * Delete a simulation
-   */
-  public removeSimulation(id: string): void {
-    const simulation: ISimulation = this.simulationsMap[id];
-    const iSimulation = this.simulations.indexOf(simulation);
-    this.simulations.splice(iSimulation, 1);
-    delete this.simulationsMap[id];
-    delete this.simulationsMap$[id];
-    this.simulationsMapSubject.next(this.simulationsMap$);
-
-    this.storeSimulations([]);
-  }
-
-  /**
-   * Delete all simulations
-   */
-  public removeSimulations(): void {
-    while (this.simulations.length) {
-      const simulation: ISimulation = this.simulations.pop() as ISimulation;
-      delete this.simulationsMap[simulation.id];
-      delete this.simulationsMap$[simulation.id];
-    }
-    this.simulationsMapSubject.next(this.simulationsMap$);
-    this.storeSimulations([]);
   }
 
   public getSimulations(): Observable<ISimulation[]> {
@@ -514,54 +391,6 @@ export class SharedSimulationService {
     return this.simulationsMap$[uuid].asObservable().pipe(shareReplay(1));
   }
 
-  /**
-   * Add a simulation to the http cache
-   * @param simulation
-   */
-  private addSimulation(simulation: ISimulation): boolean {
-    if (!(simulation.id in this.simulationsMap$)) {
-      const simSubject = new BehaviorSubject(simulation);
-      this.simulationsMap$[simulation.id] = simSubject;
-      this.simulationsMapSubject.next(this.simulationsMap$);
-      this.updateSimulation(simulation.id);
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  /**
-   * @author Bilal
-   * @param uuid The id of the simulation
-   * If we have the simulations in cache(a map of behavior subjects), return it, and trigger an update
-   * If not, then get it via http, store it to cache, trigger an update (to start polling), and return
-   * the simulator from cache. In both cases we want to return from cache. This is because the cache contains
-   * behavior subjects already configured to poll the api. The recieving method can simply pipe or subscribe
-   * to have the latest data.
-   */
-  public getSimulation(uuid: string): Observable<ISimulation> {
-    if (uuid in this.simulationsMap$) {
-      return this.getSimulationFromCache(uuid);
-    } else {
-      const sim = this.getSimulationHttp(uuid).pipe(
-        map((value: ISimulation) => {
-          if (isUnknownSimulation(value)) {
-            return of(value);
-          } else {
-            // LOCAL Storage
-            this.storeSimulations([value]);
-            this.addSimulation(value);
-            return this.getSimulationFromCache(uuid);
-          }
-        }),
-        concatAll(),
-        shareReplay(1),
-      );
-
-      return sim;
-    }
-  }
-
   public getSpecsOfSedDocsInCombineArchive(
     fileOrUrl: File | string,
   ): Observable<CombineArchiveSedDocSpecs | undefined> {
@@ -582,253 +411,4 @@ export class SharedSimulationService {
       shareReplay(1),
     );
   }
-
-  public introspectNewProject(
-    http: HttpClient,
-    modelData: FormStepData,
-    simMethodData: FormStepData,
-    errorHandler: (modelUrl: string) => void,
-  ): Observable<CustomizableSedDocumentData | null> | null {
-    const formData = CreateNewProjectFormData(modelData, simMethodData);
-    if (!formData) {
-      return null;
-    }
-
-    console.log(`THE SIM METHOD DATA FROM UTILS PROJECT INTROSPECTION: ${Object.keys(simMethodData)}`);
-    console.log(`The framework and type: ${simMethodData.framework}, ${simMethodData.simulationType}`);
-    console.log(`THE MODELDATA FROM UTILS PROJECT INTROSPECTION: ${Object.keys(modelData)}`);
-    Object.keys(modelData).forEach((p) => {
-      console.log(`THE MODEL DATA KEY VALUE: ${modelData[p]}`);
-    });
-    // console.log(`THE FILE: ${modelData.modelFile?.name}`);
-    const modelUrl = modelData?.modelUrl as string;
-    console.log(`THE MODEL URL BEING USED: ${modelUrl}`);
-    const endpoints = new Endpoints();
-    const introspectionEndpoint = endpoints.getModelIntrospectionEndpoint(false);
-    const introspectionObservable = PostNewProjectSedDocument(
-      http,
-      introspectionEndpoint,
-      formData,
-      modelUrl,
-      errorHandler,
-    );
-    return introspectionObservable.pipe(map(CreateNewProjectArchiveDataOperator(simMethodData)));
-  }
-
-  public getIntrospectionData(
-    modelFile: CommonFile,
-    modelLanguage: string,
-    simulationType: string,
-    modelingFramework: string,
-    kisaoId: string,
-    errorHandler?: any,
-  ): Observable<SedDocument | null> {
-    /*const modelUrl = modelFile.url as string;
-    const modelForm: ModelData = {
-      modelFormat: modelLanguage,
-      modelFile: modelUrl,
-      modelUrl: modelUrl
-    }
-    const simForm: SimMethodData = {
-      simulationType: simulationType,
-      framework: modelingFramework,
-      algorithm: kisaoId
-    }
-    return this.introspectNewProject(
-      this.httpClient,
-      modelForm,
-      simForm,
-      errorHandler
-    );*/
-    const formData = new FormData();
-    const modelUrl = modelFile.url as string;
-    formData.append('modelFile', modelUrl);
-    formData.append('modelLanguage', modelLanguage);
-    formData.append('simulationType', simulationType);
-    formData.append('modelingFramework', modelingFramework);
-    formData.append('simulationAlgorithm', kisaoId);
-    formData.append('modelUrl', modelUrl);
-
-    const introspectionEndpoint = this.endpoints.getModelIntrospectionEndpoint(false);
-    return this.httpClient
-      .post<SedDocument>(introspectionEndpoint, formData, { headers: { Accept: 'application/json' } })
-      .pipe(
-        catchError((error: HttpErrorResponse) => {
-          console.error('Failed to get introspection data', error);
-          return of(null); // Adjust based on how you want to handle errors
-        }),
-      );
-  }
-
-  public createCustomizableSedDocumentData(
-    sedDoc: SedDocument,
-    simulationType: SimulationType,
-  ): CustomizableSedDocumentData {
-    const namespaces: Namespace[] = [];
-    const modelChanges = GatherModelChanges(sedDoc, namespaces);
-    const modelVariables = GatherModelVariables(sedDoc, namespaces);
-    const uniformTimeCourseSimulation = GatherTimeCourseData(sedDoc, simulationType);
-    namespaces.sort((a, b): number => {
-      return (a.prefix || '').localeCompare(b.prefix || '', undefined, {
-        numeric: true,
-      });
-    });
-    return {
-      modelChanges: modelChanges,
-      modelVariables: modelVariables,
-      uniformTimeCourseSimulation: uniformTimeCourseSimulation,
-      namespaces: namespaces,
-    };
-  }
-}
-
-// -- PRIVATE INTROSPECTION FUNCTIONS
-
-function CreateNewProjectFormData(modelData: FormStepData, simMethodData: FormStepData): FormData | null {
-  const modelFormat = modelData?.modelFormat as string;
-  const modelFile = modelData?.modelFile as Blob;
-  const modelUrl = modelData?.modelUrl as string;
-  const frameworkId = simMethodData?.framework as string;
-  const simulationType = simMethodData?.simulationType as SimulationType;
-  const algorithmId = simMethodData?.algorithm as string;
-  if (!modelFormat || (!modelUrl && !modelFile) || !frameworkId || !simulationType || !algorithmId) {
-    return null;
-  }
-  const formData = new FormData();
-  if (modelFile) {
-    formData.append('modelFile', modelFile);
-  } else {
-    formData.append('modelUrl', modelUrl);
-  }
-  const formatData = BIOSIMULATIONS_FORMATS_BY_ID[modelFormat];
-  const modelLanguage = formatData?.biosimulationsMetadata?.modelFormatMetadata?.sedUrn;
-  if (modelLanguage) {
-    formData.append('modelLanguage', modelLanguage);
-  }
-  formData.append('modelingFramework', frameworkId);
-  formData.append('simulationType', simulationType);
-  formData.append('simulationAlgorithm', algorithmId);
-  return formData;
-}
-
-function PostNewProjectSedDocument(
-  http: HttpClient,
-  postEndpoint: string,
-  formData: FormData,
-  modelUrl: string,
-  errorHandler: (modelUrl: string) => void,
-): Observable<SedDocument | null> {
-  return http.post<SedDocument>(postEndpoint, formData).pipe(
-    catchError((error: HttpErrorResponse): Observable<null> => {
-      if (!environment.production) {
-        console.error(error);
-      }
-      errorHandler(modelUrl);
-      return of<null>(null);
-    }),
-  );
-}
-
-function CreateNewProjectArchiveDataOperator(
-  simMethodData: FormStepData,
-): (doc: SedDocument | null) => CustomizableSedDocumentData | null {
-  const simulationType = simMethodData?.simulationType as SimulationType;
-  return (sedDoc: SedDocument | null) => {
-    return sedDoc ? CreateCustomizableSedDocumentData(sedDoc, simulationType) : null;
-  };
-}
-
-function CreateCustomizableSedDocumentData(
-  sedDoc: SedDocument,
-  simulationType: SimulationType,
-): CustomizableSedDocumentData {
-  const namespaces: Namespace[] = [];
-  const modelChanges = GatherModelChanges(sedDoc, namespaces);
-  const modelVariables = GatherModelVariables(sedDoc, namespaces);
-  const uniformTimeCourseSimulation = GatherTimeCourseData(sedDoc, simulationType);
-  namespaces.sort((a, b): number => {
-    return (a.prefix || '').localeCompare(b.prefix || '', undefined, {
-      numeric: true,
-    });
-  });
-  return {
-    modelChanges: modelChanges,
-    modelVariables: modelVariables,
-    uniformTimeCourseSimulation: uniformTimeCourseSimulation,
-    namespaces: namespaces,
-  };
-}
-
-function GatherModelVariables(sedDoc: SedDocument, namespaces: Namespace[]): SedVariable[] {
-  const sedReports = sedDoc.outputs?.filter((output: SedOutput): boolean => {
-    return output._type === SedReportTypeEnum.SedReport;
-  }) as SedReport[];
-  const firstReport = sedReports?.[0];
-  if (!firstReport || !firstReport.dataSets) {
-    return [];
-  }
-  const modelVariables: SedVariable[] = [];
-  const dataGeneratorsMap: { [id: string]: SedDataGenerator } = {};
-  sedDoc.dataGenerators?.forEach((dataGenerator: SedDataGenerator): void => {
-    dataGeneratorsMap[dataGenerator.id] = dataGenerator;
-  });
-  firstReport.dataSets.forEach((dataSet: SedDataSet): void => {
-    const modelVar: SedVariable = dataGeneratorsMap[dataSet.dataGenerator].variables[0];
-    if (!modelVar) {
-      return;
-    }
-    modelVariables.push(modelVar);
-    AddUniqueNamespaces(modelVar.target?.namespaces, namespaces);
-  });
-  return modelVariables;
-}
-
-function GatherModelChanges(sedDoc: SedDocument, namespaces: Namespace[]): SedModelChange[] {
-  const changes = sedDoc?.models?.[0]?.changes;
-  if (!changes) {
-    return [];
-  }
-  const changeValues: SedModelChange[] = [];
-  changes.forEach((change: SedModelChange): void => {
-    // TODO: extend to other types of changes
-    if (change._type !== SedModelAttributeChangeTypeEnum.SedModelAttributeChange) {
-      return;
-    }
-    changeValues.push(change);
-    AddUniqueNamespaces(change.target?.namespaces, namespaces);
-  });
-  changeValues.sort((a, b): number => {
-    const aId = a.id || '';
-    const bId = b.id || '';
-    return aId.localeCompare(bId, undefined, { numeric: true });
-  });
-  return changeValues;
-}
-
-function GatherTimeCourseData(
-  sedDoc: SedDocument,
-  simType: SimulationType,
-): SedUniformTimeCourseSimulation | undefined {
-  const simulation = sedDoc?.simulations?.[0];
-  const selectedType = simType !== SimulationType.SedUniformTimeCourseSimulation;
-  const docType = simulation?._type === SedUniformTimeCourseSimulationTypeEnum.SedUniformTimeCourseSimulation;
-  if (!selectedType || !docType) {
-    return undefined;
-  }
-  return simulation as SedUniformTimeCourseSimulation;
-}
-
-function AddUniqueNamespaces(newNamespaces: Namespace[] | undefined, existingNamespaces: Namespace[]): void {
-  if (!newNamespaces) {
-    return;
-  }
-  newNamespaces.forEach((newNamespace: Namespace): void => {
-    const alreadyAdded =
-      existingNamespaces.find((namespace: Namespace): boolean => {
-        return newNamespace.prefix === namespace.prefix;
-      }) !== undefined;
-    if (!alreadyAdded) {
-      existingNamespaces.push(newNamespace);
-    }
-  });
 }
