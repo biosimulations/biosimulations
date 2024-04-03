6e5c933158646d5243a628cd87b2cd0e51494390
alexPatrie <alexanderpatrie@gmail.com>
2024-04-03 10:57:19 -0400

    fix: imported matcard module in test for platform overview

--- apps/platform/src/app/components/simulations/view/overview/overview.component.spec.ts
+++ apps/platform/src/app/components/simulations/view/overview/overview.component.spec.ts
@@ -5,6 +5,9 @@ import { ConfigService } from '@biosimulations/config/angular';
 import { SimulationService } from '../../../../services/simulation/simulation.service';
 import { Storage } from '@ionic/storage-angular';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+// Import the Angular Material modules
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('OverviewComponent', () => {
   let component: OverviewComponent;
@@ -13,8 +16,19 @@ describe('OverviewComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [OverviewComponent],
-      imports: [BiosimulationsIconsModule, HttpClientTestingModule],
-      providers: [ConfigService, SimulationService, Storage],
+      imports: [
+        BiosimulationsIconsModule,
+        HttpClientTestingModule,
+        // Add the Angular Material modules here
+        MatCardModule,
+        MatSnackBarModule,
+      ],
+      providers: [
+        ConfigService,
+        SimulationService,
+        Storage,
+        // If you have any service that needs to be mocked, add it here
+      ],
     }).compileComponents();
   }));
 
