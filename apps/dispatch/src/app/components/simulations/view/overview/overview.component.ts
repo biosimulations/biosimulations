c107f6e2528a36a898d41be0aa26f17cfac9472e
alex patrie <alexanderpatrie@gmail.com>
2024-04-02 22:27:48 -0400

    fix: refactor

--- apps/dispatch/src/app/components/simulations/view/overview/overview.component.ts
+++ apps/dispatch/src/app/components/simulations/view/overview/overview.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FormattedSimulation } from '../view.model';
-import { SimulationService } from '../../../../services/simulation/simulation.service';
+import { SharedSimulationService } from '@biosimulations/shared/services';
+import { MatSnackBar, MatSnackBarRef, TextOnlySnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'biosimulations-simulation-run-overview',
@@ -8,14 +9,29 @@ import { SimulationService } from '../../../../services/simulation/simulation.se
   styleUrls: ['./overview.component.scss'],
 })
 export class OverviewComponent {
-  @Input()
-  public simulation!: FormattedSimulation;
+  @Input() public simulation!: FormattedSimulation;
+  @Input() public hasSbml!: boolean;
 
-  public constructor(private simulationService: SimulationService) {
+  public constructor(private sharedSimulationService: SharedSimulationService, public snackBar: MatSnackBar) {
     /* Constructor is empty */
   }
 
   public rerunProject(id: string): void {
-    this.simulationService.rerunSimulationProject(id);
+    this.sharedSimulationService.rerunProject(id);
+  }
+
+  public rerunCustomProject(id: string): void {
+    this.sharedSimulationService.rerunCustomProject(id);
+  }
+
+  public rerunNotSupported(): MatSnackBarRef<TextOnlySnackBar> {
+    const msg =
+      'This feature is only currently available for models encoded in BNGL, CellML, SBML, SBML-fbc, ' +
+      'SBML-qual, and Smoldyn.';
+    return this.snackBar.open(msg, 'Ok', {
+      duration: 15000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
   }
 }
