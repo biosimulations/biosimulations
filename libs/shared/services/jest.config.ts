278371196b58dd4b316c1a3fa5e00be247415e9d
alexPatrie <alexanderpatrie@gmail.com>
2024-04-04 10:28:47 -0400

    chore: moved jest config attributes

--- libs/shared/services/jest.config.ts
+++ libs/shared/services/jest.config.ts
@@ -1,11 +1,36 @@
 /* eslint-disable */
+// export default {
+//   globals: {},
+//   transform: {
+//     '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+//   },
+//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+//   coverageDirectory: '../../../coverage/libs/shared/services',
+//   displayName: 'shared-services',
+//   preset: '../../../jest.preset.js',
+// };
+
+/* eslint-disable */
 export default {
-  globals: {},
-  transform: {
-    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+      diagnostics: false,
+    },
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+  moduleNameMapper: {
+    '^@core/(.*)$': '<rootDir>/src/app/core/$1',
+    '^@state/(.*)$': '<rootDir>/src/app/state/$1',
+    // Map more paths as necessary
+  },
+  transformIgnorePatterns: ['node_modules/(?!@angular)'],
   coverageDirectory: '../../../coverage/libs/shared/services',
   displayName: 'shared-services',
-  preset: '../../../jest.preset.js',
 };
