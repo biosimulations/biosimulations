c63f52f79e6ebe44d5afedc2b8270717dd65e603
alexPatrie <alexanderpatrie@gmail.com>
2024-04-04 09:04:42 -0400

    fix: updated spec file for shared sim service

--- libs/shared/services/src/lib/shared-simulation/shared-simulation.service.spec.ts
+++ libs/shared/services/src/lib/shared-simulation/shared-simulation.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Drivers } from '@ionic/storage';
+import { Storage } from '@ionic/storage-angular';
+import { SharedSimulationService } from '@biosimulations/shared/services';
+import { ConfigService } from '@biosimulations/config/angular';
 
-import { SharedSimulationService } from './shared-simulation.service';
-
-describe('SharedSimulationService', () => {
+describe('SimulationService', () => {
   let service: SharedSimulationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler, ConfigService, Storage],
+      imports: [
+        IonicStorageModule.forRoot({
+          driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage],
+        }),
+      ],
+    });
     service = TestBed.inject(SharedSimulationService);
   });
 
