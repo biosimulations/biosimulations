f671733e6cd449ee07918c4b7a8343b399d29285
alexPatrie <alexanderpatrie@gmail.com>
2024-04-05 13:07:17 -0400

    fix: updated simulation shared service to include proper providers

--- libs/shared/services/src/lib/shared-simulation/shared-simulation.service.spec.ts
+++ libs/shared/services/src/lib/shared-simulation/shared-simulation.service.spec.ts
@@ -1,33 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+
+import { IonicStorageModule, Storage } from '@ionic/storage-angular';
+import { HttpClient } from '@angular/common/http';
+import { Drivers } from '@ionic/storage';
+
+import { SimulationRunService } from '@biosimulations/angular-api-client';
 import { SharedSimulationService } from '@biosimulations/shared/services';
 import { ConfigService } from '@biosimulations/config/angular';
-import { Storage } from '@ionic/storage-angular';
-import { HttpClient } from '@angular/common/http';
-import { SimulationRunService } from '@biosimulations/angular-api-client';
 
 describe('SharedSimulationStatusService', () => {
   let simService: SharedSimulationService;
-  let configService: ConfigService;
-  let storage: Storage;
-  let http: HttpClient;
-  let simRunService: SimulationRunService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [SharedSimulationService, ConfigService],
+      providers: [SharedSimulationService, ConfigService, SimulationRunService, HttpClient, Storage],
+      imports: [
+        IonicStorageModule.forRoot({
+          driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage],
+        }),
+      ],
     });
     simService = TestBed.inject(SharedSimulationService);
-    configService = TestBed.inject(ConfigService);
-    storage = TestBed.inject(Storage);
-    http = TestBed.inject(HttpClient);
-    simRunService = TestBed.inject(SimulationRunService);
   });
 
   it('should be created', () => {
     expect(simService).toBeTruthy();
-    expect(configService).toBeTruthy();
-    expect(storage).toBeTruthy();
-    expect(http).toBeTruthy();
-    expect(simRunService).toBeTruthy();
   });
 });
