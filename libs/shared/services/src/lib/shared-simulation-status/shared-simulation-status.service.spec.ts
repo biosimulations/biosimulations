c523cc87f4d77d3bcba4bf7eab81645571e5a5d9
alexPatrie <alexanderpatrie@gmail.com>
2024-04-05 10:59:01 -0400

    fix: added configService provider to shared status service test bed

--- libs/shared/services/src/lib/shared-simulation-status/shared-simulation-status.service.spec.ts
+++ libs/shared/services/src/lib/shared-simulation-status/shared-simulation-status.service.spec.ts
@@ -1,16 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SimulationStatusService } from '@biosimulations/shared/services';
+import { ConfigService } from '@biosimulations/config/angular';
 
 describe('SharedSimulationStatusService', () => {
-  let service: SimulationStatusService;
+  let statusService: SimulationStatusService;
+  let configService: ConfigService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SimulationStatusService);
+    TestBed.configureTestingModule({
+      providers: [SimulationStatusService, ConfigService],
+    });
+    statusService = TestBed.inject(SimulationStatusService);
+    configService = TestBed.inject(ConfigService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(statusService).toBeTruthy();
+    expect(configService).toBeTruthy();
   });
 });
