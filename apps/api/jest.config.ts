c3b12bb1d6059a4b3b026279f8bd16f68218a27d
alexPatrie <alexanderpatrie@gmail.com>
2024-04-09 06:37:32 -0400

    fix: updated ts spec config to include ecma script module

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,10 +1,12 @@
 /* eslint-disable */
 export default {
-  globals: {
-    useESM: true,
-  },
   coverageDirectory: '../../coverage/apps/api',
   testEnvironment: 'node',
   displayName: 'api',
   preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
 };
