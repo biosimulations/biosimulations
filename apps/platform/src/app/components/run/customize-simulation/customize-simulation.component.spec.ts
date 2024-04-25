840dcfd666c4754886e1d55c40c432bc7ece9599
alexPatrie <alexanderpatrie@gmail.com>
2024-04-24 23:36:32 -0400

    fix: added objects to custom spec

--- apps/platform/src/app/components/run/customize-simulation/customize-simulation.component.spec.ts
+++ apps/platform/src/app/components/run/customize-simulation/customize-simulation.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CustomizeSimulationComponent } from './customize-simulation.component';
 import { ConfigService } from '@biosimulations/config/angular';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Drivers } from '@ionic/storage';
+import { IonicStorageModule, Storage } from '@ionic/storage-angular';
 
 describe('CustomizeSimulationComponent', () => {
   let component: CustomizeSimulationComponent;
@@ -10,8 +12,13 @@ describe('CustomizeSimulationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CustomizeSimulationComponent],
-      providers: [ConfigService],
-      imports: [HttpClientTestingModule],
+      providers: [ConfigService, Storage],
+      imports: [
+        HttpClientTestingModule,
+        IonicStorageModule.forRoot({
+          driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage],
+        }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CustomizeSimulationComponent);
