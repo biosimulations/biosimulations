384fc0515caf18ecea9805850e694d4340908d9a
alexPatrie <alexanderpatrie@gmail.com>
2024-04-24 23:55:31 -0400

    fix: added providers remaining to customize simulation

--- apps/platform/src/app/components/run/customize-simulation/customize-simulation.component.spec.ts
+++ apps/platform/src/app/components/run/customize-simulation/customize-simulation.component.spec.ts
@@ -1,10 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CustomizeSimulationComponent } from './customize-simulation.component';
 import { ConfigService } from '@biosimulations/config/angular';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Drivers } from '@ionic/storage';
 import { IonicStorageModule, Storage } from '@ionic/storage-angular';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedUiModule } from '@biosimulations/shared/ui';
+import { BiosimulationsIconsModule } from '@biosimulations/shared/icons';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MaterialFileInputModule } from '@biosimulations/material-file-input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('CustomizeSimulationComponent', () => {
   let component: CustomizeSimulationComponent;
@@ -13,10 +20,17 @@ describe('CustomizeSimulationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CustomizeSimulationComponent],
-      providers: [ConfigService, Storage],
+      providers: [ConfigService, Storage, HttpClient, HttpHandler],
       imports: [
-        MatSnackBar,
-        HttpClientTestingModule,
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        SharedUiModule,
+        BiosimulationsIconsModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MaterialFileInputModule,
+        NoopAnimationsModule,
         IonicStorageModule.forRoot({
           driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage],
         }),
