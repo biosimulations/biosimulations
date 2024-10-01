c39c842e5502f2254f6b6975837110a3b5f7dacb
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-10-01 16:45:24 -0400

    chore: removed projex content directory because it is unused

--- apps/projex/src/app/components/utils/utils.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SimulationProjectUtilsModule } from '@biosimulations/simulation-project-utils';
-import { UtilsRoutingModule } from './utils-routing.module';
-
-@NgModule({
-  imports: [CommonModule, SimulationProjectUtilsModule, UtilsRoutingModule],
-})
-export class UtilsModule {}
