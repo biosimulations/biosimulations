bf14c2f01885e6d2c065f9c0b30553b7fd396f73
jcschaff <schaff.jim@gmail.com>
2024-10-20 23:28:48 -0400

    chore: hardcode endpoints in environment files same for frontend/backend

--- libs/shared/environments/src/lib/environment.prod.ts
+++ libs/shared/environments/src/lib/environment.prod.ts
@@ -1,8 +1,23 @@
 import { environmentType } from './environment.type';
 import { VERSION } from './version';
+import { endpointType } from './endpoint.type';
+
 export const environment: environmentType = {
   production: true,
   env: 'prod',
   baseUrl: 'https://api.biosimulations.org',
   version: VERSION,
+};
+export const endpoints: endpointType = {
+  api: 'https://api.biosimulations.org',
+  simulators_api: 'https://api.biosimulators.org',
+  combine_api: 'https://combine.api.biosimulations.org',
+  simdata_api: 'https://simdata.api.biosimulations.org',
+  simulators_app: 'https://biosimulators.org',
+  platform_app: 'https://biosimulators.org',
+  dispatch_app: 'https://run.biosimulations.org',
+  external_api: 'https://api.biosimulations.org',
+  external_simulators_api: 'https://api.biosimulators.org',
+  external_combine_api: 'https://combine.api.biosimulations.org',
+  external_simdata_api: 'https://simdata.api.biosimulations.org',
 };
--- libs/shared/environments/src/lib/environment.ts
+++ libs/shared/environments/src/lib/environment.ts
@@ -1,4 +1,5 @@
 import { environmentType } from './environment.type';
 /* Change the name of the file to environment.type.ts where type is the name of the environment you wish to load */
-import { environment as currentEnvironment } from './environment.dev';
+import { environment as currentEnvironment, endpoints as currentEndpoints } from './environment.dev';
 export const environment: environmentType = currentEnvironment;
+export const endpoints = currentEndpoints;
